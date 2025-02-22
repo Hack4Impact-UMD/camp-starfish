b82a2b26a444538b22313d0fe61007a22bcf5d44
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-02-22 17:44:36 -0500

    added assets, colors, and fonts from style guide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,45 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const lato = localFont({
+  src: [
+    { path: "../../Lato-Black.ttf", weight: "900", style: "normal" },
+    { path: "../../Lato-BlackItalic.ttf", weight: "900", style: "italic" },
+    { path: "../../Lato-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../../Lato-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "../../Lato-Light.ttf", weight: "300", style: "normal" },
+    { path: "../../Lato-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "../../Lato-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../../Lato-RegularItalic.ttf", weight: "400", style: "italic" },
+    { path: "../../Lato-Thin.ttf", weight: "100", style: "normal" },
+    { path: "../../Lato-ThinItalic.ttf", weight: "100", style: "italic" },
+  ],
+  variable: "--font-lato",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const newSpirit = localFont({
+  src: [
+    { path: "../../NewSpirit-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../../NewSpirit-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "../../NewSpirit-Light.ttf", weight: "300", style: "normal" },
+    { path: "../../NewSpirit-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "../../NewSpirit-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../../NewSpirit-MediumItalic.ttf", weight: "500", style: "italic" },
+    { path: "../../NewSpirit-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../../NewSpirit-RegularItalic.ttf", weight: "400", style: "italic" },
+    { path: "../../NewSpirit-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../../NewSpirit-SemiBoldItalic.ttf", weight: "600", style: "italic" },
+  ],
+  variable: "--font-new-spirit"
+})
+
+const besteam = localFont({
+  src: "../../public/fonts/Besteam.ttf",
+  weight: "400",
+  style: "regular",
+  variable: "--font-besteam"
+})
 
 export const metadata: Metadata = {
   title: "Camp Starfish",
@@ -25,7 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lato.variable} ${newSpirit.variable} ${besteam.variable} antialiased`}
       >
         {children}
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        camp: {
+          primary: "#002D45",
+          secondary: "#FFDE59",
+          tert: {
+            green: "#07B862",
+            blue: "#00BDCE",
+            orange: "F4831F"
+          }
+        },
+        pattern: {
+          primary_bg: 'rgba(255, 255, 255, 0.07)',
+          secondary_bg: 'rgba(255, 255, 255, 0.40)',
+          white_bg: 'rgba(96, 96, 96, 0.07)'
+        }
       },
     },
   },
