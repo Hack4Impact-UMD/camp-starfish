081ddbb1505492bb0d5bf22be82c9a9415ae11c8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-02-22 12:31:11 -0500

    set up Firebase

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "camp-starfish.firebaseapp.com",
+  projectId: "camp-starfish",
+  storageBucket: "camp-starfish.firebasestorage.app",
+  messagingSenderId: "353349843655",
+  appId: "1:353349843655:web:659825cf31b9dcf3e0dce2",
+  measurementId: "G-S3K5R4T5K8",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app); 
