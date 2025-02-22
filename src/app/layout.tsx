5924623da9b84ba52b59dbe9b51e4451fcd1a801
Benjamin-Nicolas <benwesi@terpmail.umd.edu>
2025-02-22 18:49:14 -0500

    added loading animations
    Added the different loading animation levels.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,32 +4,32 @@ import localFont from "next/font/local";
 
 const lato = localFont({
   src: [
-    { path: "../../Lato-Black.ttf", weight: "900", style: "normal" },
-    { path: "../../Lato-BlackItalic.ttf", weight: "900", style: "italic" },
-    { path: "../../Lato-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../../Lato-BoldItalic.ttf", weight: "700", style: "italic" },
-    { path: "../../Lato-Light.ttf", weight: "300", style: "normal" },
-    { path: "../../Lato-LightItalic.ttf", weight: "300", style: "italic" },
-    { path: "../../Lato-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../../Lato-RegularItalic.ttf", weight: "400", style: "italic" },
-    { path: "../../Lato-Thin.ttf", weight: "100", style: "normal" },
-    { path: "../../Lato-ThinItalic.ttf", weight: "100", style: "italic" },
+    { path: "../../public/fonts/Lato/Lato-Black.ttf", weight: "900", style: "normal" },
+    { path: "../../public/fonts/Lato/Lato-BlackItalic.ttf", weight: "900", style: "italic" },
+    { path: "../../public/fonts/Lato/Lato-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../../public/fonts/Lato/Lato-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "../../public/fonts/Lato/Lato-Light.ttf", weight: "300", style: "normal" },
+    { path: "../../public/fonts/Lato/Lato-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "../../public/fonts/Lato/Lato-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../../public/fonts/Lato/Lato-RegularItalic.ttf", weight: "400", style: "italic" },
+    { path: "../../public/fonts/Lato/Lato-Thin.ttf", weight: "100", style: "normal" },
+    { path: "../../public/fonts/Lato/Lato-ThinItalic.ttf", weight: "100", style: "italic" },
   ],
   variable: "--font-lato",
 });
 
 const newSpirit = localFont({
   src: [
-    { path: "../../NewSpirit-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../../NewSpirit-BoldItalic.ttf", weight: "700", style: "italic" },
-    { path: "../../NewSpirit-Light.ttf", weight: "300", style: "normal" },
-    { path: "../../NewSpirit-LightItalic.ttf", weight: "300", style: "italic" },
-    { path: "../../NewSpirit-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../../NewSpirit-MediumItalic.ttf", weight: "500", style: "italic" },
-    { path: "../../NewSpirit-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../../NewSpirit-RegularItalic.ttf", weight: "400", style: "italic" },
-    { path: "../../NewSpirit-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../../NewSpirit-SemiBoldItalic.ttf", weight: "600", style: "italic" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-Bold.ttf", weight: "700", style: "normal" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-BoldItalic.ttf", weight: "700", style: "italic" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-Light.ttf", weight: "300", style: "normal" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-LightItalic.ttf", weight: "300", style: "italic" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-Medium.ttf", weight: "500", style: "normal" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-MediumItalic.ttf", weight: "500", style: "italic" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-Regular.ttf", weight: "400", style: "normal" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-RegularItalic.ttf", weight: "400", style: "italic" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "../../public/fonts/NewSpirit/NewSpirit-SemiBoldItalic.ttf", weight: "600", style: "italic" },
   ],
   variable: "--font-new-spirit"
 })
