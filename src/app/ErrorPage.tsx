98451f1d46492655333c88c54d93d4228c563726
Joel Chemmanur <joelchem@outlook.com>
2025-02-24 14:03:45 -0500

    added error page with basic text

---
+++ src/app/ErrorPage.tsx
@@ -0,0 +1,6 @@
+export default function ErrorPage() {
+    return <div>
+        <h1>Error</h1>
+        <p>It seems we can’t find what you’re looking for.</p>
+    </div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ErrorPage from "./ErrorPage";
+
 export default function Home() {
-  return <></>
+  return <><ErrorPage/></>
 }
