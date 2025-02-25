0ccab6c4bd8b3f823beaee3d3e8bdfc40f9d7cc6
Rivan Parikh <rivan.parikh@gmail.com>
2025-02-25 14:54:01 -0500

    converted css to tailwind css

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import Link from 'next/link'; // Use Next.js Link instead of react-router-dom
 import darkBgLogo from '../assets/logos/darkBgLogo.png';
 import profile from '../assets/logos/Profile.png';
-import './Navbar.css';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="container">
+    <nav className="w-full h-[112px] bg-[#002D45] px-32 flex justify-between items-center relative">
       {/* Logo on the left */}
-      <div className="left-section">
+      <div className="flex items-center">
         <Link href="/">
-          <img className="logo" src={darkBgLogo.src} alt="Camp Starfish Logo" />
+          <img className="w-[100.94px] h-[72px] cursor-pointer" src={darkBgLogo.src} alt="Camp Starfish Logo" />
         </Link>
       </div>
 
       {/* Centered Text Container */}
-      <div className="text-container">
+      <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-20 text-white text-[20px] font-bold leading-6 font-lato">
         {Array.from({ length: 4 }, (_, index) => (
           <span key={index}>Example Item</span>
         ))}
@@ -24,7 +23,7 @@ const Navbar: React.FC = () => {
       {/* Profile Picture on the right */}
       <div>
         <Link href="/profile">
-        <img className="profile" src={profile.src} alt="Profile" />
+          <img className="w-[72px] h-[72px]" src={profile.src} alt="Profile" />
         </Link>
       </div>
     </nav>
