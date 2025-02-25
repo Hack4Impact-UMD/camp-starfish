fa90887cbe390d2bf50dc1ad4261eb4b70a8cf35
Joel Chemmanur <joelchem@outlook.com>
2025-02-24 23:08:18 -0500

    styling changes

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,19 +1,32 @@
-'use client'
+"use client";
 
-import { redirect } from "next/navigation"
-
+import { redirect } from "next/navigation";
 
 export default function Error({
-    error,
-  }: {
-    error: Error & { digest?: string }
-  }) {
-   
-    return (
-      <div className="flex items-center justify-center h-screen flex-col bg-white">
-        <h1 className="text-black text-6xl font-semibold mb-4" style={{fontFamily: "var(--font-new-spirit)"}}>{error.name}</h1>
-        <p className="text-black mb-16 text-center max-w-4xl" style={{fontFamily: "var(--font-lato)"}}>{error.message}</p>
-        <button className="bg-camp-tert-green px-24 py-3 font-bold rounded-full text-white" onClick={() => redirect("/")}>GO BACK</button>
-      </div>
-    )
-  }
+  error,
+}: {
+  error: Error & { digest?: string };
+}) {
+  return (
+    <div className="flex items-center justify-center h-screen flex-col bg-white">
+      <h1
+        className="text-black text-6xl font-semibold mb-4"
+        style={{ fontFamily: "var(--font-new-spirit)" }}
+      >
+        {error.name}
+      </h1>
+      <p
+        className="text-black mb-16 text-center max-w-4xl"
+        style={{ fontFamily: "var(--font-lato)" }}
+      >
+        {error.message}
+      </p>
+      <button
+        className="bg-camp-tert-green px-24 py-3 font-bold rounded-full text-white"
+        onClick={() => redirect("/")}
+      >
+        GO BACK
+      </button>
+    </div>
+  );
+}
