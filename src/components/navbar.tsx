a8bc184d3e8de1b2e5bda974a338de04f9012679
Rivan Parikh <rivan.parikh@gmail.com>
2025-02-25 16:43:49 -0500

    used imported color, fixed absolute styling, and made profile pic smaller

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import Link from 'next/link'; // Use Next.js Link instead of react-router-dom
-import darkBgLogo from '../assets/logos/darkBgLogo.png';
-import profile from '../assets/logos/Profile.png';
+import React from "react";
+import Link from "next/link"; // Use Next.js Link instead of react-router-dom
+import darkBgLogo from "../assets/logos/darkBgLogo.png";
+import profile from "../assets/logos/Profile.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="w-full h-[112px] bg-[#002D45] px-32 flex justify-between items-center relative">
+    <nav className="w-full h-[112px] bg-camp-primary px-32 flex items-center justify-between">
       {/* Logo on the left */}
-      <div className="flex items-center">
-        <Link href="/">
-          <img className="w-[100.94px] h-[72px] cursor-pointer" src={darkBgLogo.src} alt="Camp Starfish Logo" />
-        </Link>
-      </div>
+      <Link href="/">
+        <img className="w-[100.94px] h-[72px] cursor-pointer" src={darkBgLogo.src} alt="Camp Starfish Logo" />
+      </Link>
 
       {/* Centered Text Container */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 flex gap-20 text-white text-[20px] font-bold leading-6 font-lato">
-        {Array.from({ length: 4 }, (_, index) => (
-          <span key={index}>Example Item</span>
+      <div className="flex gap-20 text-white text-[20px] font-bold leading-6 font-lato">
+        {["Example Item 1", "Example Item 2", "Example Item 3", "Example Item 4"].map((item, index) => (
+          <span key={index} className="cursor-pointer">
+            {item}
+          </span>
         ))}
       </div>
 
       {/* Profile Picture on the right */}
-      <div>
-        <Link href="/profile">
-          <img className="w-[72px] h-[72px]" src={profile.src} alt="Profile" />
-        </Link>
-      </div>
+      <Link href="/profile">
+        <img className="w-[48px] h-[48px] cursor-pointer" src={profile.src} alt="Profile" />
+      </Link>
     </nav>
   );
 };
