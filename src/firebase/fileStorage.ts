aeaf26f019d00b244ed28e5baa4ab2e2ce132b87
Teky1 <officialteky@gmail.com>
2025-02-28 18:58:00 -0500

    function structures

---
+++ src/firebase/fileStorage.ts
@@ -0,0 +1,27 @@
+import { getStorage, ref } from "firebase/storage";
+
+const storage = getStorage();
+
+export function uploadImage(img: File, path: string) {
+
+
+
+}
+
+export function downloadImage(path: string) {
+
+
+
+}
+
+export function uploadImages(imgs: File[], paths: string[]) {
+
+    
+
+}
+
+export function downloadImages(paths: string[]) {
+
+
+
+}
