a2f04c54c72ecc505519e2607a6f83d8fb8434a6
Rivan Parikh <rivan.parikh@gmail.com>
2025-02-28 01:06:57 -0500

    added some potential authentication code

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,14 @@ import darkBgLogo from "../assets/logos/darkBgLogo.png";
 import profile from "../assets/logos/Profile.png";
 
 const Navbar: React.FC = () => {
+
+  const navLinks = [
+    { name: "Programs", href: "/programs", roles: ["Counselor", "Admin"] },
+    { name: "Campers", href: "/campers", roles: ["Counselor", "Admin", "Parent"] },
+    { name: "Albums", href: "/albums", roles: ["Counselor", "Admin", "Photographer", "Parent "] }, 
+
+  ];
+
   return (
     <nav className="w-full h-[112px] bg-camp-primary px-32 flex items-center justify-between">
       {/* Logo on the left */}
@@ -20,11 +28,32 @@ const Navbar: React.FC = () => {
         ))}
       </div>
 
-      {/* Profile Picture on the right */}
-      <Link href="/profile">
-        <img className="w-[48px] h-[48px] cursor-pointer" src={profile.src} alt="Profile" />
-      </Link>
-    </nav>
+      {/*uncomment below once auth is set up*/}
+
+      {/* Navigation Links (Conditional Rendering Based on Role)
+      {user?.isAuthenticated && (
+        <div className="flex gap-20 text-white text-[20px] font-bold leading-6 font-lato">
+          {navLinks
+            .filter((link) => link.roles.includes(user.role))
+            .map((link) => (
+              <Link key={link.href} href={link.href} className="cursor-pointer">
+                {link.name}
+              </Link>
+            ))}
+        </div>
+      )}
+      */}
+
+        <Link href="/profile">
+          <img className="w-[48px] h-[48px] cursor-pointer" src={profile.src} alt="Profile" />
+        </Link>
+      {/* Profile Picture on the right
+      {user?.isAuthenticated && (
+        <Link href="/profile">
+          <img className="w-[48px] h-[48px] cursor-pointer" src={profile.src} alt="Profile" />
+        </Link>
+      )} */}
+      </nav>
   );
 };
 
