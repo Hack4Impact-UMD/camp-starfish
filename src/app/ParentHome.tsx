16b155c354cd96955b9cbaa49f0061c490f2beee
Esha Vigneswaran <eshavigneswaran@gmail.com>
2025-02-28 23:05:06 -0500

    Started the staff home page

---
+++ src/app/ParentHome.tsx
@@ -0,0 +1,33 @@
+
+export default function ParentHome() {
+  return(
+    <div className = "p-20">
+      <div className = "mb-[100px]">
+        <h1 className = "text-[85px] font-semibold font-new-spirit">Welcome, staffName!</h1>
+        <p className = "text-[20px]">You can schedule activities, upload and tag pictures</p>
+        <p className = "text-[20px]">You can schedule activities, upload and tag pictures</p>
+        <p className = "text-[20px]">You can schedule activities, upload and tag pictures</p>
+      </div>
+
+      <div className = "flex flex-col justify-between">
+        <div className = "mb-[50px]">
+          <h3 className = "text-[32px] font-black font-new-spirit">ALBUMS</h3>
+          <p className = "text-[20px]">You can schedule activities, upload and tag pictures</p>
+        </div>
+
+        <div className = "mb-[50px]">
+          <h3 className = "text-[32px] font-black">PROGRAMS</h3>
+          <p className = "text-[20px]">You can schedule activities, upload and tag pictures</p>
+        </div>
+
+        <div>
+          <h3 className = "text-[32px] font-black">CAMPERS</h3>
+          <p className = "text-[20px]">You can schedule activities, upload and tag pictures</p>
+        </div>
+        
+        
+      </div>
+    </div>
+  )
+  
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import ParentHome from "./ParentHome"
 export default function Home() {
-  return <></>
+  return(
+    <>
+      <ParentHome />
+    </>
+
+  )
 }
