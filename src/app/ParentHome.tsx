2b93cf486f9a231f6d81ad7875c391ad8d41d859
Esha Vigneswaran <eshavigneswaran@gmail.com>
2025-03-04 17:14:19 -0500

    Slight mod to left margin for left div container

--- src/app/ParentHome.tsx
+++ src/app/ParentHome.tsx
@@ -6,7 +6,7 @@ export default function ParentHome() {
     return (
         <div className="flex flex-col lg:flex-row text-[20px] items-center justify-center min-h-screen text-black bg-white px-6">
             {/* Left Half */}
-            <div className="flex flex-col items-center lg:items-start justify-center w-full lg:w-1/2 h-full p-6 lg:p-12 lg:ml-[120px] gap-8 text-center lg:text-left">
+            <div className="flex flex-col items-center lg:items-start justify-center w-full lg:w-1/2 h-full p-6 lg:p-12 lg:ml-[60px] gap-8 text-center lg:text-left">
                 <h1 className="text-[40px] lg:text-[80px] font-semibold font-newSpirit">Welcome, parentName!</h1>
                 <p>You can view and download your camper’s photos now</p>
                 <button
