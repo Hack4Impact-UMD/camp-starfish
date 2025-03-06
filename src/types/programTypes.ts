03530455e041d7564751f44f67a39607ca251806
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-03-05 23:56:05 -0500

    created types for program structure; blocks have not been typed

--- src/types/programTypes.ts
+++ src/types/programTypes.ts
@@ -1,19 +1,39 @@
 export interface Program {
-  bundles: Bundle[];
-
-}
-
-export interface Bundle {
-  blocks: Block[];
-}
-
-export interface Block {
-  activities: Activity[];
-}
-
-export interface Activity {
   name: string;
-  category: ActivityCategory;
+  startDate: string; // ISO-8601
+  endDate: string; // ISO-8601
+  schedule: ProgramSection[];
 }
 
-export type ActivityCategory = "Arts & Crafts" | "Athletics" | "Learning Center" | "Activate" | "Discovery" | "Martial Arts" | "Boating" | "Drama" | 
+export type ProgramSection = CommonSection | SchedulingSection;
+
+export interface CommonSection {
+  name: string;
+  startDate: string; // ISO-8601
+  endDate: string; // ISO-8601
+}
+
+export type SchedulingSectionType = "BUNDLE" | "BUNK-JAMBO" | "NON-BUNK-JAMBO";
+export interface SchedulingSection extends CommonSection {
+  type: SchedulingSectionType;
+  blocks: { [blockName: string]: Block };
+}
+
+export type Block = ActivityBlock | FreeplayBlock;
+
+export interface ActivityBlock {
+
+}
+
+export interface FreeplayBlock {
+
+}
+
+export type ActivityCategory = "Arts & Crafts" | "Athletics" | "Learning Center" | "Activate" | "Discovery" | "Martial Arts" | "Boating" | "Drama" // get full list from Lydia
+
+export interface Bunk {
+  bunkId: number;
+  staffId: number;
+  camperIds: number[];
+}
+
