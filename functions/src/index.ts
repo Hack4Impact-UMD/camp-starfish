b41fe080ca0261c00a3e35b56a731a905803630c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-03-09 14:27:00 -0400

    initialized Firebase project with auth, firestore, storage, functions, and hosting services

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onRequest((request, response) => {
+  response.set("Access-Control-Allow-Origin", "*");
+  logger.info("Hello logs take 2!", {structuredData: true});
+  response.send({ data: "Hello from Firebase!"});
+});
