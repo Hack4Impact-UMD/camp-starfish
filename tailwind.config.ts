48e2bb2b9733a4e0ffa55eed9ed8c2ae2c97dff3
Benjamin-Nicolas <benwesi@terpmail.umd.edu>
2025-03-10 19:32:48 -0400

    updated "Welcome" color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,8 @@ export default {
           tert: {
             green: "#07B862",
             blue: "#00BDCE",
-            orange: "F4831F"
+            orange: "F4831F",
+            dark_grey: "#333333"
           },
           white: "#FFFFFF"
         },
