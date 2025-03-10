102c3825341efc11d95fe5372a66be22fa10a57a
Gelila Kebede <gkebede1@terpmail.umd.edu>
2025-03-10 19:58:31 -0400

    CRUD ops for programs

---
+++ src/data/firestore/programs.ts
@@ -0,0 +1,65 @@
+import { db } from "@/config/firebase"; 
+import { Program } from "@/types/programTypes";
+import {
+    doc,
+    collection,
+    Transaction,
+} from "firebase/firestore";
+
+const PROGRAM_COLLECTION = "programs";
+
+export async function getProgramById(transaction: Transaction, id: string): Promise<Program> {
+    try {
+        const programRef = doc(db, PROGRAM_COLLECTION, id);
+        const programDoc = await transaction.get(programRef);
+
+        if (!programDoc.exists()) {
+            throw new Error("Program not found");
+        }
+
+        return programDoc.data() as Program;
+    } catch (error) {
+        throw new Error(`Failed to get program: ${error}`);
+    }
+};
+
+//TODO: check if program already exists
+export async function createProgram(transaction: Transaction, program: Program): Promise<string> {
+    try {
+        const programRef = doc(collection(db, PROGRAM_COLLECTION));
+        transaction.set(programRef, program);
+        return programRef.id;
+    } catch (error) {
+        throw new Error(`Failed to create program: ${error}`);
+    }
+};
+
+export async function updateProgram(transaction: Transaction, id: string, updates: Partial<Program>): Promise<void> {
+    try {
+        const programRef = doc(db, PROGRAM_COLLECTION, id);
+        const programDoc = await transaction.get(programRef);
+
+        if (!programDoc.exists()) {
+            throw new Error("Program not found");
+        }
+
+        transaction.update(programRef, updates);
+    } catch (error) {
+        throw new Error(`Failed to update program: ${error}`);
+    }
+};
+
+export async function deleteProgram(transaction: Transaction, id: string): Promise<void> {
+    try {
+        const programRef = doc(db, PROGRAM_COLLECTION, id);
+        const programDoc = await transaction.get(programRef);
+
+        if (!programDoc.exists()) {
+            throw new Error("Program not found");
+        }
+
+        transaction.delete(programRef);
+    } catch (error) {
+        throw new Error(`Failed to delete program: ${error}`);
+    }
+}
