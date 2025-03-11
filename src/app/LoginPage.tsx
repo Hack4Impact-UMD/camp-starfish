983772c40f6ca476b699a22de39d6e9a2c394b1c
Gelila Kebede <gkebede1@terpmail.umd.edu>
2025-03-11 14:51:23 -0400

    fix sizing when under navbar

--- src/app/LoginPage.tsx
+++ src/app/LoginPage.tsx
@@ -7,9 +7,9 @@ import BackgroundPattern from "@/components/BackgroundPattern";
 
 export default function LoginPage() {
   return (
-    <div className="relative min-h-full flex items-center justify-center bg-camp-primary">
-      <div className="absolute inset-0 w-full h-full">
-        <BackgroundPattern fill="#FFFFFF" opacity={0.1} />
+    <div className="relative min-h-full w-full flex items-center justify-center bg-camp-primary overflow-hidden">
+      <div className="absolute inset-0 h-full">
+        <BackgroundPattern fill="#FFFFFF" opacity={0.07} />
       </div>
 
       <div className="bg-[#F1F1F1] m-5 py-8 px-5 rounded-2xl w-5/6 max-w-[424px] min-h-[300px] flex flex-col items-center text-center shadow-lg relative">
--- src/components/BackgroundPattern.tsx
+++ src/components/BackgroundPattern.tsx
@@ -11,11 +11,11 @@ export default function BackgroundPattern({
     <svg
       xmlns="http://www.w3.org/2000/svg"
       id="Layer_1"
-      width="100%"
-      height="100%"
       viewBox="0 0 4951.83 4081.35"
       preserveAspectRatio="xMidYMid slice"
       fill="none"
+      width="100%"
+      height="100%"
     >
       <defs>
         <pattern
