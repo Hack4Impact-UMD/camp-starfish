e1998b33507f375406cf0bb359674e69ad802183
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-03-11 11:17:33 -0400

    formatting

--- src/data/storage/fileOperations.ts
+++ src/data/storage/fileOperations.ts
@@ -2,32 +2,24 @@ import { storage } from "@/config/firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export async function uploadImage(img: File, path: string) {
-
-    let uploadRef = ref(storage, path);
-    await uploadBytes(uploadRef, img);
-
+  let uploadRef = ref(storage, path);
+  await uploadBytes(uploadRef, img);
 }
 
 export async function downloadImage(path: string) {
-
-    let downloadRef = ref(storage, path);
-    return await getDownloadURL(downloadRef);
-
+  let downloadRef = ref(storage, path);
+  return await getDownloadURL(downloadRef);
 }
 
 export async function uploadImages(imgs: File[], paths: string[]) {
-    
-    let uploadPromises = imgs.map((img, i) => {
-        uploadImage(img, paths[i]);
-    });
+  let uploadPromises = imgs.map((img, i) => {
+    uploadImage(img, paths[i]);
+  });
 
-    await Promise.all(uploadPromises);
-
+  await Promise.all(uploadPromises);
 }
 
 export async function downloadImages(paths: string[]) {
-
-    let downloadPromises = paths.map((path) => downloadImage(path));
-
-    return await Promise.all(downloadPromises);
+  let downloadPromises = paths.map((path) => downloadImage(path));
+  return await Promise.all(downloadPromises);
 }
