daf27945accd54a55f384599c53c8eba4fad9433
cc13985 <claire13985@gmail.com>
2025-03-11 01:17:05 -0400

    updated colors with new hex codes

--- src/components/GalleryCardOne.tsx
+++ src/components/GalleryCardOne.tsx
@@ -10,13 +10,13 @@ type GalleryCardProps = {
 
 export default function GalleryCardOne({ title, description }: GalleryCardProps) {
     return (
-      <div className = "flex flex-col justify-center items-center px-[36px] py-[40px] w-[370px] gap-[8px] rounded-[8px] shadow-[0px_0px_16px_-2px_rgba(0,0,0,0.30)]">
-        <span className ="flex flex-row gap-[8px]">
-          <h3 className = "font-lato text-[32px] font-[900] text-camp-primary">{title}</h3>
-          <img src = {albumIcon.src}></img>
+      <div className="flex flex-col justify-center items-center px-[36px] py-[40px] w-[370px] gap-[8px] rounded-[8px] shadow-[0px_0px_16px_-2px_rgba(0,0,0,0.30)]">
+        <span className="flex flex-row gap-[8px]">
+          <h3 className="font-lato text-[32px] font-black text-camp-primary">{title}</h3>
+          <img src={albumIcon.src}></img>
         </span>
-        <p className = "text-center text-[20px] font-[400] text-[#4A4A4A]">{description}</p>
-        <img className="mt-[20px] mb-[20px]" src = {StaticImageCard.src}></img>
+        <p className="text-center text-[20px] font-[400] text-modalSecondaryTitle">{description}</p>
+        <img className="mt-[20px] mb-[20px]" src={StaticImageCard.src}></img>
         <button className="rounded-[40px] w-full py-[16px] text-[20px] text-white font-[700] bg-camp-tert-green">VIEW {title}</button>      
       </div>
     )
