95fdcc4c0ccfca677eddd591c0da7d0f93091c3d
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-03-10 23:01:29 -0400

    implemented auth for conditional rendering in Navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,63 +1,69 @@
+"use client";
+
 import React from "react";
-import Link from "next/link"; // Use Next.js Link instead of react-router-dom
+import Link from "next/link";
 import darkBgLogo from "../assets/logos/darkBgLogo.png";
 import profile from "../assets/logos/Profile.png";
+import { useAuth } from "@/auth/useAuth";
+import { Role } from "@/types/personTypes";
+
+const navbarLinks: { name: string; href: string; roles: Role[] }[] = [
+  { name: "Programs", href: "/programs", roles: ["STAFF", "ADMIN"] },
+  {
+    name: "Campers",
+    href: "/campers",
+    roles: ["STAFF", "ADMIN", "PARENT"],
+  },
+  {
+    name: "Albums",
+    href: "/albums",
+    roles: ["STAFF", "ADMIN", "PHOTOGRAPHER", "PARENT"],
+  },
+];
 
 const Navbar: React.FC = () => {
-
-  const navLinks = [
-    { name: "Programs", href: "/programs", roles: ["Counselor", "Admin"] },
-    { name: "Campers", href: "/campers", roles: ["Counselor", "Admin", "Parent"] },
-    { name: "Albums", href: "/albums", roles: ["Counselor", "Admin", "Photographer", "Parent "] }, 
-
-  ];
+  const auth = useAuth();
+  const role: Role = auth.token?.claims.role as Role;
 
   return (
     <nav className="w-full h-[112px] bg-camp-primary px-32 flex items-center justify-between gap-20">
       {/* Logo on the left */}
       <div className="flex-none">
         <Link href="/">
-          <img className="w-[100.94px] h-[72px] flex-none cursor-pointer" src={darkBgLogo.src} alt="Camp Starfish Logo" />
+          <img
+            className="w-[100.94px] h-[72px] flex-none cursor-pointer"
+            src={darkBgLogo.src}
+            alt="Camp Starfish Logo"
+          />
         </Link>
       </div>
 
       {/* Centered Text Container */}
-      <div className="flex gap-20 text-white text-[20px] font-bold leading-6 font-lato shrink">
-        {["Example Item 1", "Example Item 2", "Example Item 3", "Example Item 4"].map((item, index) => (
-          <span key={index} className="cursor-pointer shrink">
-            {item}
-          </span>
-        ))}
-      </div>
-
-      {/*uncomment below once auth is set up*/}
-
-      {/* Navigation Links (Conditional Rendering Based on Role)
-      {user?.isAuthenticated && (
-        <div className="flex gap-20 text-white text-[20px] font-bold leading-6 font-lato">
-          {navLinks
-            .filter((link) => link.roles.includes(user.role))
-            .map((link) => (
-              <Link key={link.href} href={link.href} className="cursor-pointer">
-                {link.name}
+      {auth.token && (
+        <div className="flex gap-20 text-white text-[20px] font-bold leading-6 font-lato shrink">
+          {navbarLinks
+            .filter((item) => item.roles.includes(role))
+            .map((item, index) => (
+              <Link key={index} href={item.href} className="cursor-pointer">
+                <span className="cursor-pointer shrink font-lato font-bold">
+                  {item.name}
+                </span>
               </Link>
             ))}
         </div>
       )}
-      */}
 
-      <div className="flex-none">
+      {/* Profile Icon on the right */}
+      {auth.token && <div className="flex-none">
         <Link href="/profile">
-          <img className="w-[62px] h-[62px] flex-none cursor-pointer" src={profile.src} alt="Profile" />
+          <img
+            className="w-[62px] h-[62px] flex-none cursor-pointer"
+            src={profile.src}
+            alt="Profile"
+          />
         </Link>
-      </div>
-      {/* Profile Picture on the right
-      {user?.isAuthenticated && (
-        <Link href="/profile">
-          <img className="w-[48px] h-[48px] cursor-pointer" src={profile.src} alt="Profile" />
-        </Link>
-      )} */}
-      </nav>
+      </div>}
+    </nav>
   );
 };
 
