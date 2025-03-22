4adf93bf24c8c79fd710a164fcf5ff89a3c6ba66
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-03-22 16:28:03 -0400

    changed return type of schedules.ts functions from ProgramSectionp[] to SchedulingSection[]

--- src/data/firestore/schedules.ts
+++ src/data/firestore/schedules.ts
@@ -1,30 +1,29 @@
 import { db } from "@/config/firebase";
-import { ProgramSection } from "@/types/programTypes";
+import { ProgramSection, SchedulingSection } from "@/types/programTypes";
 import {
     query,
     where,
     getDocs,
-    collection,
-    Transaction
+    collection
 } from "firebase/firestore";
 
-export async function getCamperSchedule(camperId: number, programId: string): Promise<ProgramSection[]> {
+export async function getCamperSchedule(camperId: number, programId: string): Promise<SchedulingSection[]> {
     try {
         const camperScheduleRef = collection(db, `programs/${programId}/camperSchedules`);
         const q = query(camperScheduleRef, where("campminderId", "==", camperId));
         const querySnapshot = await getDocs(q);
-        return querySnapshot.docs.map((doc) => doc.data() as ProgramSection);
+        return querySnapshot.docs.map((doc) => doc.data() as SchedulingSection);
     } catch (error: any) {
         throw new Error(`Failed to get camper schedule: ${error.code}`);
     }
 }
 
-export async function getEmployeeSchedule(employeeId: number | string, programId: string): Promise<ProgramSection[]> {
+export async function getEmployeeSchedule(employeeId: number | string, programId: string): Promise<SchedulingSection[]> {
     try {
         const employeeScheduleRef = collection(db, `programs/${programId}/employeeSchedules`);
         const q = query(employeeScheduleRef, where("campminderId", "==", employeeId));
         const querySnapshot = await getDocs(q);
-        return querySnapshot.docs.map((doc) => doc.data() as ProgramSection);
+        return querySnapshot.docs.map((doc) => doc.data() as SchedulingSection);
     } catch (error: any) {
         throw new Error(`Failed to get employee schedule: ${error.code}`);
     }
