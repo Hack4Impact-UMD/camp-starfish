f265ab964e1539b3f5a8357e9f9d37d67466d70c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-03-21 23:35:49 -0400

    added redirects to EmployeeHomePage

--- src/app/EmployeeHomePage.tsx
+++ src/app/EmployeeHomePage.tsx
@@ -16,11 +16,11 @@ export default function EmployeeHomePage() {
       {/* Content Section */}
       <div className="flex flex-wrap justify-center items-center gap-[84px]">
         {/* Albums */}
-        <GalleryCardOne title="ALBUMS" description="Manage photos from past and ongoing programs"/>
+        <GalleryCardOne title="ALBUMS" href="/albums" description="Manage photos from past and ongoing programs"/>
         {/* Programs */}
-        <GalleryCardOne title="PROGRAMS" description="Use the activity scheduler to organize campers and staff"/>
+        <GalleryCardOne title="PROGRAMS" href="/programs" description="Use the activity scheduler to organize campers and staff"/>
         { /* Campers */}
-        <GalleryCardOne title="CAMPERS" description="Access the cohort list and each camper’s details"/>
+        <GalleryCardOne title="CAMPERS" href="/campers" description="Access the cohort list and each camper’s details"/>
       </div>
     </div>
   )
--- src/components/GalleryCardOne.tsx
+++ src/components/GalleryCardOne.tsx
@@ -1,16 +1,18 @@
 "use client";
+
+import { redirect } from "next/navigation";
 import PolaroidPhotos1 from "../assets/images/PolaroidPhotos1.png";
 import albumIcon from "@/assets/icons/albumIcon.svg";
 
-
 type GalleryCardProps = {
   title: string;
+  href: string;
   description: string;
 };
 
-export default function GalleryCardOne({ title, description }: GalleryCardProps) {
+export default function GalleryCardOne({ title, href, description }: GalleryCardProps) {
     return (
-      <div className="flex flex-col justify-center items-center px-[36px] py-[40px] w-[370px] gap-[8px] rounded-[8px] shadow-[0px_0px_16px_-2px_rgba(0,0,0,0.30)]">
+      <div className="flex flex-col justify-center items-center px-[36px] py-[40px] w-[370px] gap-[8px] rounded-[8px] shadow-[0px_0px_16px_-2px_rgba(0,0,0,0.30)]" onClick={() => redirect(href)}>
         <span className="flex flex-row gap-[8px]">
           <h3 className="font-lato text-[32px] font-black text-camp-primary">{title}</h3>
           <img src={albumIcon.src}></img>
