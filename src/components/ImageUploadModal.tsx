8ae8b53c16bea6fafaa97f996cb12b7ed31e3a63
Joel Chemmanur <joelchem@outlook.com>
2025-04-06 04:42:29 -0400

    basic accept/reject icons

--- src/components/ImageUploadModal.tsx
+++ src/components/ImageUploadModal.tsx
@@ -8,20 +8,22 @@ import {
   DialogTrigger,
 } from "@radix-ui/react-dialog";
 import submitIconUrl from "@/assets/icons/submitIcon.svg";
+import crossIconUrl from "@/assets/icons/xIconPrimary.svg";
+import alertIconUrl from "@/assets/icons/alert.svg";
+import uploadedIconUrl from "@/assets/icons/xIconPrimary.svg";
+import fileLoadIcon from "@/assets/icons/fileLoadSuccessIcon.svg";
 import { useDropzone } from "react-dropzone";
-import { useState } from "react";
+import { JSX, useState } from "react";
 
-const MAX_FILE_SIZE = 5*1024*1024; // 5 MB
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
 
 type ImageUploadModalProps = {
   children: React.ReactNode;
   albumID: string;
 };
 
-export default function ImageUploadModal({ children }: ImageUploadModalProps) {
 
-  // TODO: use onDrop which takes in accepted files on each new drop
-  // TODO; also use onRejectedDrop or something that gets the new rejected files on each drop
+export default function ImageUploadModal({ children }: ImageUploadModalProps) {
 
   let { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     accept: {
@@ -30,11 +32,38 @@ export default function ImageUploadModal({ children }: ImageUploadModalProps) {
     },
     maxSize: MAX_FILE_SIZE,
     onDrop: (accepted, rejected) => {
-      console.log(accepted);
-      console.log(rejected);
-      setStagedFiles(last => last.concat(accepted));
-    }
+      console.log(accepted, rejected)
+      setStagedFiles((last) => last.concat(accepted));
+      setFailedFiles((last) => last.concat(rejected.map(e => e.file)));
+    },
   });
+
+  function FileComponent({ file, accepted }: { file: File; accepted: boolean }) {
+    return (
+      <div
+        className="w-[35rem] my-1 py-3 px-3 bg-camp-background-formField text-camp-text-headingBody rounded-md flex flex-row items-center justify-between"
+        key={file.name}
+      >
+        <span className="text-camp-text-headingBody text-sm">{file.name}</span>
+        <div className="mr-1">
+          <img
+            src={accepted ? fileLoadIcon.src : alertIconUrl.src}
+            className="w-6 h-6 inline-block"
+          ></img>
+          <img 
+            src={crossIconUrl.src}
+            onClickCapture={() => (
+              accepted ? 
+              setStagedFiles(last => last.filter(e => e != file)) 
+              : setFailedFiles(last => last.filter(e => e != file))
+            )}
+             className="w-5 h-5 inline-block cursor-pointer p-1 ml-4"></img>
+        </div>
+      </div>
+    );
+  }
+
+  
 
   let [stagedFiles, setStagedFiles] = useState<File[]>([]);
   let [failedFiles, setFailedFiles] = useState<File[]>([]);
@@ -49,20 +78,26 @@ export default function ImageUploadModal({ children }: ImageUploadModalProps) {
             <DialogTitle className="text-2xl font-semibold text-camp-white font-lato">
               Upload Photos
             </DialogTitle>
-            <DialogClose className="font-bold px-2">
-              X {/* TODO: change to icon */}
-            </DialogClose>
           </div>
 
           <div className="bg-white p-5">
             <div {...getRootProps({ className: "dropzone" })}>
               <input {...getInputProps()} />
-              {stagedFiles.length > 0 ? (
+              {stagedFiles.length+failedFiles.length > 0 ? (
                 <div className="h-[20rem] overflow-scroll">
+                  {failedFiles.map((file) => (
+                    <FileComponent
+                    key={file.name}
+                    file={file}
+                    accepted={false}
+                  />
+                  ))}
                   {stagedFiles.map((file) => (
-                    <div className="block w-[35rem] my-1 py-3 px-3 bg-camp-background-formField text-camp-text-headingBody rounded-md" key={file.name}>
-                      <span className="text-camp-text-headingBody text-sm ">{file.name}</span>
-                    </div>
+                    <FileComponent
+                      key={file.name}
+                      file={file}
+                      accepted={true}
+                    />
                   ))}
                 </div>
               ) : (
@@ -85,18 +120,26 @@ export default function ImageUploadModal({ children }: ImageUploadModalProps) {
                   </span>
                 </div>
               )}
-              
             </div>
             <div>
-                <button className="bg-camp-buttons-neutral text-bold font-lato text-camp-buttons-buttonTextLight mt-4 px-8 py-2 rounded-full">
+              <DialogClose asChild>
+                <button 
+                onClick={() => {
+                  setStagedFiles([]);
+                  setFailedFiles([]);
+                }}
+                className="bg-camp-buttons-neutral text-bold font-lato text-camp-buttons-buttonTextLight mt-4 px-8 py-2 rounded-full">
                   Cancel
                 </button>
-                {stagedFiles.length > 0 ? (
-                  <button className="bg-camp-tert-green text-bold font-lato text-camp-buttons-buttonTextDark ml-2 mt-4 px-8 py-2 rounded-full">
-                    Upload {stagedFiles.length} Photo{(stagedFiles.length > 1 ? 's' : '')}
-                  </button>
-                ) : null}
-              </div>
+              </DialogClose>
+
+              {stagedFiles.length > 0 ? (
+                <button className="bg-camp-tert-green text-bold font-lato text-camp-buttons-buttonTextDark ml-2 mt-4 px-8 py-2 rounded-full">
+                  Upload {stagedFiles.length} Photo
+                  {stagedFiles.length > 1 ? "s" : ""}
+                </button>
+              ) : null}
+            </div>
           </div>
         </DialogContent>
       </DialogPortal>
