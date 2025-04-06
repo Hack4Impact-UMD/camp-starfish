910b484b6ee05f5c108504e8d6a4848b5ad2fb58
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-04-05 22:10:42 -0400

    first iteration of programTypes

--- src/types/programTypes.ts
+++ src/types/programTypes.ts
@@ -1,11 +1,11 @@
-export interface Program {
+export interface Session {
   name: string;
   startDate: string; // ISO-8601
   endDate: string; // ISO-8601
-  schedule: ProgramSection[];
+  schedule: SessionSection[];
 }
 
-export type ProgramSection = CommonSection | SchedulingSection;
+export type SessionSection = CommonSection | SchedulingSection;
 
 export interface CommonSection {
   name: string;
@@ -16,24 +16,64 @@ export interface CommonSection {
 export type SchedulingSectionType = "BUNDLE" | "BUNK-JAMBO" | "NON-BUNK-JAMBO";
 export interface SchedulingSection extends CommonSection {
   type: SchedulingSectionType;
-  blocks: { [blockName: string]: Block };
+  freeplays: { [freeplayId: string]: Freeplay };
+  blocks: { [blockId: string]: Activity[] };
 }
 
-export type Block = ActivityBlock | FreeplayBlock;
+export type ActivityCategory =
+  | "ACT" // Activate!
+  | "A&C" // Arts & Crafts
+  | "ATH" // Athletics
+  | "BOAT"  // Boating
+  | "CHAL"  // Challenge
+  | "DNC"  // Dance
+  | "DRA"  // Drama
+  | "DISC"  // Discovery
+  | "LC"  // Learning Center
+  | "MUS"  // Music
+  | "OUT"  // Outdoor Cooking
+  | "SMA"  // Small Animals
+  | "XPL"  // Xplore!
+  | "OCP"  // Teens
+  | "WF";  // Waterfront
 
-export interface ActivityBlock {
-
+export interface Activity {
+  name: string;
+  description: string;
+  category?: ActivityCategory; // included only in Bundle activities
+  ageGroup?: AgeGroup; // included only in Bundle activities
+  assignments: {
+    campers: number[];
+    staff: number[];
+    admin: number[];
+  }
 }
 
-export interface FreeplayBlock {
-
+export interface ActivityAssignments {
+  admin: number[];
 }
-
-export type ActivityCategory = "Arts & Crafts" | "Athletics" | "Learning Center" | "Activate" | "Discovery" | "Martial Arts" | "Boating" | "Drama" // get full list from Lydia
 
 export interface Bunk {
-  bunkId: number;
-  staffId: number;
+  bunkNum: number;
+  staffIds: number[];
   camperIds: number[];
 }
 
+export interface Freeplay {
+  posts: Record<Post, number[]> // Admin & Staff only
+  buddies: Record<number, number[]>; // Staff assigned to 1-2 campers each
+}
+
+export type Post =
+  | "Wishy Washy"
+  | "Lily Pads"
+  | "Book Nook"
+  | "Camp Store"
+  | "Blacktop"
+  | "Waterfront"
+  | "Fort Starfish"
+  | "Teens Lounge"
+  | "Truckstop"
+  | "Field";
+
+export type AgeGroup = 'NAV' | 'OCP';
