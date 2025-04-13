74ba2e74bf5c87b4c13e5ae7e2464b9f7a79097b
Benjamin-Nicolas <benwesi@terpmail.umd.edu>
2025-04-13 01:00:22 -0400

    Add vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
+      <h1 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Camp Starfish - Vite Version</h1>
+      <p>This is a simplified version running with Vite.</p>
+    </div>
+  )
+}
+
+export default App
