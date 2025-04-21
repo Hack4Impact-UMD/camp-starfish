c5da4e7421f3a82c9c49b0b6d91c600d3e3c4b79
Joel Chemmanur <joelchem@outlook.com>
2025-04-21 10:39:59 -0400

    organized larger code chunks into components

--- src/components/FileUploadModal.tsx
+++ src/components/FileUploadModal.tsx
@@ -34,8 +34,7 @@ export default function FileUploadModal({
   acceptedFileExtensions,
   maxFileSize,
 }: FileUploadModalProps) {
-
-  let [files, setFiles] = useState<{file: File, state: FileStatus}[]>([]);
+  let [files, setFiles] = useState<{ file: File; state: FileStatus }[]>([]);
   let [uploadState, setUploadState] = useState<UploadState>("none");
 
   const mimeTypes: string[] = [
@@ -55,11 +54,11 @@ export default function FileUploadModal({
     accept: inputAccept,
     maxSize: maxFileSize * 1024 * 1024,
     onDrop: async (accepted: File[], rejected: FileRejection[]) => {
-      setFiles((last) => last.concat(
-        accepted.map((f) => ({file: f, state: "success"}))
-      ).concat(
-        rejected.map((f) => ({file: f.file, state: "failure"}))
-      ));
+      setFiles((last) =>
+        last
+          .concat(accepted.map((f) => ({ file: f, state: "success" })))
+          .concat(rejected.map((f) => ({ file: f.file, state: "failure" })))
+      );
     },
   });
 
@@ -94,7 +93,105 @@ export default function FileUploadModal({
   }
 
   function InitialUploadView() {
-    
+    return (
+      <div className="border-4 border-dashed border-camp-tert-orange rounded-lg text-center px-32 py-12">
+        <span className="block font-lato text-camp-text-subheading font-bold text-lg">
+          Supported file formats:{" "}
+          {acceptedFileExtensions.map((type: string) => type).join(", ")} (Max{" "}
+          {maxFileSize}MB)
+        </span>
+        <img
+          src={submitIcon.src}
+          className="w-12 h-12 text-center block mx-auto m-4"
+        ></img>
+        <span className="block font-lato text-camp-text-subheading font-bold text-lg m-2">
+          Drag and drop files
+        </span>
+        <span className="block font-lato text-camp-text-subheading font-bold text-lg m-2">
+          OR
+        </span>
+        <span className=" cursor-pointer block font-lato text-camp-text-link font-bold text-lg m-2">
+          Select from device
+        </span>
+      </div>
+    );
+  }
+
+  function FinishedUploadView() {
+    return (
+      <div className="mx-6 my-4">
+        <img
+          src={uploadState == "success" ? uploadGreenIcon.src : alertIcon.src}
+          className="w-6 h-6 text-center block mx-auto m-4"
+        ></img>
+        <span className="block text-center text-camp-primary font-bold font-lato text-xl">
+          Upload {uploadState == "success" ? "successful" : "failed"}!
+        </span>
+        <span className="text-center text-camp-text-modalSecondaryTitle block m-4 text-sm">
+          {files.filter((e) => e.state == "success").length} files{" "}
+          {uploadState == "success" ? "uploaded." : "failed to upload."}
+        </span>
+        <div className="text-center">
+          <DialogClose asChild>
+            <button className="bg-camp-buttons-neutral text-bold font-lato text-camp-buttons-buttonTextLight px-12 py-2 rounded-full">
+              Close
+            </button>
+          </DialogClose>
+
+          <button className="bg-camp-primary text-bold font-lato text-camp-buttons-buttonTextDark ml-4 px-12 py-2 rounded-full">
+            View
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  function UploadedFilesView() {
+    return (
+      <div className="h-[20rem] overflow-scroll">
+        {files
+          .filter((e) => e.state == "success")
+          .map((fileState) => (
+            <FileComponent
+              key={fileState.file.name}
+              file={fileState.file}
+              accepted={true}
+            />
+          ))}
+      </div>
+    );
+  }
+
+  function BottomButtonsComponent() {
+    return (
+      <>
+        <DialogClose asChild>
+          <button className="bg-camp-buttons-neutral text-bold font-lato text-camp-buttons-buttonTextLight mt-4 px-8 py-2 rounded-full">
+            Cancel
+          </button>
+        </DialogClose>
+
+        <button
+          hidden={files.filter((e) => e.state == "success").length == 0}
+          onClick={async () => {
+            try {
+              onUpload(
+                files.filter((e) => e.state == "success").map((x) => x.file)
+              );
+              setUploadState("success");
+            } catch (err: unknown) {
+              setUploadState("fail");
+              console.error(err);
+              return;
+            }
+          }}
+          className="bg-camp-tert-green text-bold font-lato text-camp-buttons-buttonTextDark ml-2 mt-4 px-8 py-2 rounded-full"
+        >
+          Upload {files.filter((e) => e.state == "success").length} File
+          {files.filter((e) => e.state == "success").length > 1 ? "s" : ""}
+        </button>
+      </>
+    );
   }
 
   return (
@@ -122,99 +219,23 @@ export default function FileUploadModal({
 
           <div className="bg-white p-5">
             {uploadState != "none" ? (
-              <div className="mx-6 my-4">
-                <img
-                  src={
-                    uploadState == "success"
-                      ? uploadGreenIcon.src
-                      : alertIcon.src
-                  }
-                  className="w-6 h-6 text-center block mx-auto m-4"
-                ></img>
-                <span className="block text-center text-camp-primary font-bold font-lato text-xl">
-                  Upload {uploadState == "success" ? "successful" : "failed"}!
-                </span>
-                <span className="text-center text-camp-text-modalSecondaryTitle block m-4 text-sm">
-                  {files.filter(e => e.state == "success").length} files{" "}
-                  {uploadState == "success" ? "uploaded." : "failed to upload."}
-                </span>
-                <div className="text-center">
-                  <DialogClose asChild>
-                    <button className="bg-camp-buttons-neutral text-bold font-lato text-camp-buttons-buttonTextLight px-12 py-2 rounded-full">
-                      Close
-                    </button>
-                  </DialogClose>
-
-                  <button className="bg-camp-primary text-bold font-lato text-camp-buttons-buttonTextDark ml-4 px-12 py-2 rounded-full">
-                    View
-                  </button>
-                </div>
-              </div>
+              <FinishedUploadView />
             ) : (
               <div {...getRootProps({ className: "dropzone" })}>
                 <input {...getInputProps()} />
-                {files.filter(e => e.state == "success").length > 0 ? (
-                  <div className="h-[20rem] overflow-scroll">
-                    {files.filter(e => e.state == "success").map((fileState) => (
-                      <FileComponent
-                        key={fileState.file.name}
-                        file={fileState.file}
-                        accepted={true}
-                      />
-                    ))}
-                  </div>
+                {files.filter((e) => e.state == "success").length > 0 ? (
+                  <>
+                    <UploadedFilesView />
+                    <BottomButtonsComponent />
+                  </>
                 ) : (
-                  <div className="border-4 border-dashed border-camp-tert-orange rounded-lg text-center px-32 py-12">
-                    <span className="block font-lato text-camp-text-subheading font-bold text-lg">
-                      Supported file formats:{" "}
-                      {acceptedFileExtensions
-                        .map((type: string) => type)
-                        .join(", ")}{" "}
-                      (Max {maxFileSize}MB)
-                    </span>
-                    <img
-                      src={submitIcon.src}
-                      className="w-12 h-12 text-center block mx-auto m-4"
-                    ></img>
-                    <span className="block font-lato text-camp-text-subheading font-bold text-lg m-2">
-                      Drag and drop files
-                    </span>
-                    <span className="block font-lato text-camp-text-subheading font-bold text-lg m-2">
-                      OR
-                    </span>
-                    <span className=" cursor-pointer block font-lato text-camp-text-link font-bold text-lg m-2">
-                      Select from device
-                    </span>
-                  </div>
+                  <>
+                    <InitialUploadView />
+                    <BottomButtonsComponent />
+                  </>
                 )}
               </div>
             )}
-
-            <div hidden={uploadState != "none"}>
-              <DialogClose asChild>
-                <button className="bg-camp-buttons-neutral text-bold font-lato text-camp-buttons-buttonTextLight mt-4 px-8 py-2 rounded-full">
-                  Cancel
-                </button>
-              </DialogClose>
-
-              <button
-                hidden={files.filter(e => e.state == "success").length == 0}
-                onClick={async () => {
-                  try {
-                    onUpload(files.filter(e => e.state == "success").map(x => x.file));
-                    setUploadState("success");
-                  } catch (err: unknown) {
-                    setUploadState("fail");
-                    console.error(err);
-                    return;
-                  }
-                }}
-                className="bg-camp-tert-green text-bold font-lato text-camp-buttons-buttonTextDark ml-2 mt-4 px-8 py-2 rounded-full"
-              >
-                Upload {files.filter(e => e.state == "success").length} File
-                {files.filter(e => e.state == "success").length > 1 ? "s" : ""}
-              </button>
-            </div>
           </div>
         </DialogContent>
       </DialogPortal>
