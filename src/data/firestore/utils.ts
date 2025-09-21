568a1a3d07c29a5484c27dfe4e0e0db62cba5d7d
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-21 13:46:31 -0400

    mapped out Firestore collections

--- src/data/firestore/utils.ts
+++ src/data/firestore/utils.ts
@@ -2,7 +2,25 @@ export enum Collection {
   CAMPERS = "campers",
   PARENTS = "parents",
   EMPLOYEES = "employees",
+
   ALBUMS = "albums",
-  IMAGES = "images",
   SESSIONS = "sessions",
+  PROGRAM_AREAS = "program_areas",
+  POSTS = "posts",
+}
+
+export enum AlbumsSubcollection {
+  IMAGES = "images",
+}
+
+export enum SessionsSubcollection {
+  ATTENDEES = "attendees",
+  MASTER_SCHEDULES = "master_schedules",
+  BUNKS = "bunks",
+}
+
+export enum MasterSchedulesSubcollection {
+  BUNDLES = "bundles",
+  JAMBOREES = "jamborees",
+  FREEPLAYS = "freeplays",
 }
