db7e03b32d6c57c49681dcc4c468319c544c6f97
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-24 08:10:47 -0400

    added Apps Script service

---
+++ src/auth/types/authTypes.ts
@@ -0,0 +1,15 @@
+import { ParsedToken } from "firebase/auth";
+import { UserRole } from "@/types/personTypes";
+
+export interface GoogleTokens {
+  refreshToken: string;
+  accessToken: string;
+  expirationTime: string;
+}
+
+export interface CustomClaims {
+  googleTokens?: GoogleTokens;
+  role?: UserRole;
+}
+
+export type DecodedIdTokenWithCustomClaims = ParsedToken & CustomClaims;
---
+++ src/data/appsScriptService.ts
@@ -0,0 +1,35 @@
+import { DecodedIdTokenWithCustomClaims } from "@/auth/types/authTypes";
+import { auth } from "@/config/firebase";
+
+async function callAppsScript(functionName: string, parameters?: any[]): Promise<any> {
+  const claims = ((await auth.currentUser?.getIdTokenResult())?.claims as DecodedIdTokenWithCustomClaims);
+  if (!auth.currentUser) {
+    throw new Error("You must be logged in to access this feature.");
+  } else if (!claims.role || claims.role !== 'ADMIN') {
+    throw new Error("You do not have permission to access this feature.");
+  } else if (!claims.googleTokens?.accessToken) {
+    // TODO: Redirect to OAuth flow
+    throw new Error("Invalid access token. Please authorize Google to access this feature.")
+  }
+
+  const response = await fetch(
+    `https://script.googleapis.com/v1/scripts/${process.env.NEXT_PUBLIC_GOOGLE_APPS_SCRIPT_DEPLOYMENT_ID}:run`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        function: functionName,
+        parameters,
+      }),
+      headers: {
+        Authorization: `Bearer ${claims.googleTokens.accessToken}`,
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    }
+  );
+  const data = await response.json();
+  if (data.error) {
+    throw new Error("An unexpected error occurred. Please try again later.");
+  }
+  return data.response.result;
+}
