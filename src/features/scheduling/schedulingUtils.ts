4a19b432d03354de427ef48d23d85426dda134ec
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-09-26 16:30:04 -0400

    fixed conflict detection logic (flipped booleans)

--- src/features/scheduling/schedulingUtils.ts
+++ src/features/scheduling/schedulingUtils.ts
@@ -2,7 +2,7 @@ import { AttendeeID } from "@/types/sessionTypes";
 
 export function doesConflictExist(attendee: AttendeeID, otherAttendeeIds: number[]) {
   if (attendee.role === "CAMPER") {
-    return attendee.nonoList.every((id) => !otherAttendeeIds.includes(id))
+    return attendee.nonoList.some((id) => otherAttendeeIds.includes(id))
   }
-  return attendee.nonoList.every((id) => !otherAttendeeIds.includes(id)) && attendee.yesyesList.every((id) => !otherAttendeeIds.includes(id));
+  return attendee.nonoList.some((id) => otherAttendeeIds.includes(id)) && attendee.yesyesList.some((id) => otherAttendeeIds.includes(id));
 }
