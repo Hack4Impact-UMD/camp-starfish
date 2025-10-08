e04babfb97dd3c18cad63fbb7b2afa647453afff
Alexis Tungcod <alexisgt0015@gmail.com>
2025-10-08 00:18:17 -0400

    updates to GAS preference sheet

--- apps-script/src/code.ts
+++ apps-script/src/code.ts
@@ -1,36 +1,137 @@
-//importing types to use for data inputs and use in sheet
-import { Camper } from "../../src/types/personTypes";
-import { Bunk } from "../../src/types/sessionTypes";
-// google app script that creates spreadsheets for jamborees and bundles
+// google apps script that creates preference spreadsheets for jamborees and bundles
+//importing necessary types
+import { CamperAttendeeID } from "../../src/types/sessionTypes";
+import { 
+  BundleBlockActivities, 
+  BunkJamboreeBlockActivities, 
+  NonBunkJamboreeBlockActivities 
+} from "../../src/types/sessionTypes";
 
-function createJambooreeSheet(bunkJambo: Camper [], nonBunkJambo: Bunk [] ){
-    //two sheets for each bundle letter (A, B, C, D)
-    //two sheets should have the same design/layout
-    //creating NAV sheet
-    const navSheet = SpreadsheetApp.create("NAV Sheet");
-    //creating OCP sheet
-    const ocpSheet = SpreadsheetApp.create("OCP Sheet");
+/* attentde types type
+non-bunk: use camper IDs 
+bunk: bunk numbers 
+*/
+
+type AttendeeList = CamperAttendeeID[] | number[];
+
+// Type for block activities - array of activities with name property
+type BlockActivities = Array<{ name: string; [key: string]: any }>;
+
+// type for block activities map - maps block letters to lists of activities
+type BlockActivitiesMap = {
+  [blockId: string]: BlockActivities;
+};
+
+// color blocks to use for different blocks on the sheet for design
+const BLOCK_COLORS: { [key: string]: string } = {
+  'A': '#FF0000', 
+  'B': '#FFA500', 
+  'C': '#FFFF00', 
+  'D': '#00FF00', 
+};
+
+/* both NAV and OCP sheets should have the same dimensions and format
+*/
+
+//creating actual sheet
+function createPreferenceSheet(
+  attendees: AttendeeList, //list of attendees (either campers or bunks)
+  blockActivities: BlockActivitiesMap, //activities for the given block
+  sheetName: string //what the input for the sheet should be
+): string {
+  // create the main spreadsheet 
+  const spreadsheet = SpreadsheetApp.create(sheetName);
+  const sheet = spreadsheet.getActiveSheet();
+  
+  //starting index for columns
+  let colIndex = 1;
+  const blockIds = Object.keys(blockActivities).sort();
+  const totalAttendees = attendees.length;
+  
+  // setting up first column and then iterating through
+  const bunkList = typeof attendees[0] === 'number';
+  sheet.getRange(1, colIndex).setValue(bunkList ? "Bunk" : "Camper");
+  colIndex++;
+  
+  // headers for blocks and activities
+  for (const blockId of blockIds) {
+    const activities = blockActivities[blockId];
+    const startCol = colIndex;
     
-    //getting the dimensions of the sheets
-    /* get number of navs, get number of bunks, get number of ocps */
-    //want to add make rows = number of campers in the jamboree + 1 for block letter + 1 for activity number
-    //want to make columns = number of activitys * number of blocks + 1 for bunk + 1 for camper numbers
+    //entering information for headers for each block and activities
+    for (let i = 0; i < activities.length; i++) {
+      const activity = activities[i];
+      sheet.getRange(1, colIndex).setValue(`${blockId} - ${activity.name}`);
+      colIndex++;
+    }
+    
+    // applying sheet background colors
+    const endCol = colIndex - 1;
+    const blockColor = BLOCK_COLORS[blockId];
+    const totalRows = totalAttendees + 1;
+    sheet.getRange(1, startCol, totalRows, endCol - startCol + 1).setBackground(blockColor);
+  }
+  
+  //keep track of number of current column
+  const currentCol = colIndex - 1;
+  
+  // fill in attendee data starting in the second column going down the rows
+  for (let i = 0; i < attendees.length; i++) {
+    const currentAttendee = attendees[i];
+    //checking if the attendee is a bunk whole (bunk) or campers indidually (non-bunk)
+    if (typeof currentAttendee === 'number') {
+      // setting the rows for bunks
+      sheet.getRange(i + 2, 1).setValue(currentAttendee);
+    } 
+    //setting the row for each cmaper
+    else if (typeof currentAttendee === 'object' && 'name' in currentAttendee) {
+      const currentCamper = currentAttendee as CamperAttendeeID;
+      sheet.getRange(i + 2, 1).setValue(`${currentCamper.name.firstName} ${currentCamper.name.lastName}`);
+    }
+  }
+  
+  // formatting sheet
+  sheet.setFrozenRows(1);
+  sheet.setFrozenColumns(1);
+  sheet.getRange(1, 1, 1, currentCol).setHorizontalAlignment('center');
+  sheet.autoResizeColumns(1, currentCol);
+  
+  return spreadsheet.getId();
+}
 
-    //inputting data
-    /* 1. go through each list and place them into sheet (if they are in the right age group)
-       2. check preference for each activity (1-5 in that order) and place that number in the column then continue to next activity
-     */
+//BUNDLE SHEET CREATION
+function createBundleSheet(
+  campers: CamperAttendeeID[], //list of campers attending
+  blockActivities: { [blockId: string]: BundleBlockActivities }, //activities for the given block
+  bundleLetter: string //bundle letter
+): string {
+  return createPreferenceSheet(
+    campers,
+    blockActivities,
+    `Bundle ${bundleLetter} Schedule`
+  );
+}
 
-    //setting colors for different bundles
-        /* set range with given columns then color */
+//BUNK JAMBOREE SHEET CREATION
+function createBunkJamboreeSheet(
+  bunkNumbers: number[], //list of bunk numbers attending
+  blockActivities: { [blockId: string]: BunkJamboreeBlockActivities } //activities for the given block
+): string {
+  return createPreferenceSheet(
+    bunkNumbers,
+    blockActivities,
+    "Bunk Jamboree Schedule"
+  );
+}
 
-    //block a color = red
-
-    //block b color = orange
-
-    //block c color = yellow
-
-    //block d color = green
-
-    //each block has 5 columns (5 activities)
+//NON-BUNK JAMBOREE SHEET CREATION
+function createNonBunkJamboreeSheet(
+  campers: CamperAttendeeID[], //campers attending
+  blockActivities: { [blockId: string]: NonBunkJamboreeBlockActivities } //activities for the given block
+): string {
+  return createPreferenceSheet(
+    campers,
+    blockActivities,
+    "Non-Bunk Jamboree Schedule"
+  );
 }
