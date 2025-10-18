e0ea9d1ee5b9ed76c84ca5965dc847964c0e1721
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-10-17 22:43:23 -0400

    add colors to Mantine theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,11 @@ import AuthProvider from "@/auth/AuthProvider";
 import Footer from "../components/Footer";
 
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import {
+  MantineColorsTuple,
+  MantineProvider,
+  MantineThemeOverride,
+} from "@mantine/core";
 
 const lato = localFont({
   src: [
@@ -127,6 +131,70 @@ const besteam = localFont({
   variable: "--font-besteam",
 });
 
+const mantineTheme: MantineThemeOverride = {
+  colors: {
+    neutral: [
+      ...Array<string>(2).fill('#FFFFFF'),
+      '#FAFAFB',
+      '#DEE1E3',
+      '#C0C6C9',
+      '#3B4E57',
+      '#2F424C',
+      ...Array<string>(3).fill('#1D323D')
+    ] as unknown as MantineColorsTuple,
+    primary: [
+      ...Array<string>(4).fill("#E6EAEC"),
+      "#2B5165",
+      "#002D45",
+      ...Array<string>(4).fill("#001B2A"),
+    ] as unknown as MantineColorsTuple,
+    "secondary-orange": [
+      ...Array<string>(4).fill("#FACCA3"),
+      "#F4831F",
+      "#AB5C16",
+      ...Array<string>(4).fill("#955013"),
+    ] as unknown as MantineColorsTuple,
+    "secondary-green": [
+      ...Array<string>(4).fill("#99E2BF"),
+      "#07B862",
+      "#058145",
+      ...Array<string>(4).fill("#04703C"),
+    ] as unknown as MantineColorsTuple,
+    "accent-yellow": [
+      ...Array<string>(3).fill("#FFEC9F"),
+      "#FFE475",
+      "#FFDE59",
+      "#B39B3E",
+      ...Array<string>(4).fill("#9C8736"),
+    ] as unknown as MantineColorsTuple,
+    "accent-blue": [
+      ...Array<string>(3).fill("#6BD5E3"),
+      "#2BC2D6",
+      "#00B6CE",
+      "#007F90",
+      ...Array<string>(4).fill("#006F7E"),
+    ] as unknown as MantineColorsTuple,
+    success: [
+      ...Array<string>(5).fill("#1E8E3E"),
+      "#15632B",
+      ...Array<string>(4).fill("#125726"),
+    ] as unknown as MantineColorsTuple,
+    error: [
+      ...Array<string>(5).fill("#D32F2F"),
+      "#942121",
+      ...Array<string>(4).fill("#811D1D"),
+    ] as unknown as MantineColorsTuple,
+    warning: [
+      ...Array<string>(5).fill("#EFAF00"),
+      "#A77A00",
+      ...Array<string>(4).fill("#926B00"),
+    ] as unknown as MantineColorsTuple,
+    link: Array<string>(10).fill('#1A80D8') as unknown as MantineColorsTuple,
+  },
+  primaryShade: 4,
+  primaryColor: 'primary',
+};
+
 export const metadata: Metadata = {
   title: "Camp Starfish",
   description: "Camp Starfish's photo portal and scheduling application",
@@ -142,7 +210,7 @@ export default function RootLayout({
       <body
         className={`${lato.variable} ${newSpirit.variable} ${besteam.variable} antialiased w-full min-h-screen flex flex-col`}
       >
-        <MantineProvider>
+        <MantineProvider theme={mantineTheme}>
           <AuthProvider>
             <>
               <div className="w-full">
