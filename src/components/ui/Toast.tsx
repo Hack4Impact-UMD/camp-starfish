e81944ef37110fbc8675284cec06a97c0dc5ffdf
Alexis Tungcod <alexisgt0015@gmail.com>
2025-10-23 23:18:45 -0400

    mantime imports

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { ToastContainer, toast } from 'react-toastify';
-import { useToaster } from 'react-hot-toast/headless';
 
 
 export type ToastVariant = "success" | "error";
--- src/features/notifications/NotificationsProvider.tsx
+++ src/features/notifications/NotificationsProvider.tsx
@@ -4,6 +4,24 @@ import React, { createContext, useCallback, useContext, useMemo, useRef, useStat
 import { createPortal } from "react-dom";
 import toast, { Toaster } from 'react-hot-toast';
 import type { NotificationId, NotificationItem, NotificationsContextValue } from "./types";
+import { Notifications } from "@mantine/notifications";
 
+// alias functions
+import {
+  cleanNotifications, // notifications.clean
+  cleanNotificationsQueue, // notifications.cleanQueue
+  hideNotification, // notifications.hide
+  showNotification, // notifications.show
+  updateNotification, // notifications.update
+  updateNotificationsState, // notifications.updateState
+} from '@mantine/notifications';
+
+export default function NotificationsProviderWrapper({  }) {
+  return (
+    <>
+      <Notifications position="top-center" />
+    </>
+  );
+}
 
 
