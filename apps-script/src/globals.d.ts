9c7240541fc26c82e74d64d1f0f652c13f5b81a3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-11-01 20:04:43 -0400

    add Moment.js to Apps Script dev environment

--- apps-script/src/globals.d.ts
+++ apps-script/src/globals.d.ts
@@ -1,7 +1,15 @@
-declare global {
-  // declare all Apps Script functions here
-  // ex. var functionName(param1: string, param2: number): boolean
+import type momentType from "moment";
 
+declare global {
+  namespace Moment {
+    const moment: typeof momentType;
+  }
+  var moment: typeof momentType
+
+  // preferencesSheets.ts
+  var createPreferencesSpreadsheet: (sessionName: string) => string
+  // declare all Apps Script functions here
+  // ex. var functionName(param1: string, param2: nu
   // after the function implementation in a separate file, add the following line
   // globalThis.functionName = functionName;
 }
