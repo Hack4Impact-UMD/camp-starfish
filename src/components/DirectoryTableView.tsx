aa79020fdf55b00838482f018813db9f63866bf5
Psangs123 <39105705+FearlessTiger@users.noreply.github.com>
2025-11-06 15:59:14 -0500

    created column defs to the overall directory table

--- src/components/DirectoryTableView.tsx
+++ src/components/DirectoryTableView.tsx
@@ -4,30 +4,73 @@ import {
   useMantineReactTable,
 } from "mantine-react-table";
 import { useMemo } from "react";
-import { Person } from "@/types/personTypes";
+import { Attendee, AttendeeID } from "@/types/sessionTypes";
 
-const data: Person[] = [
+const data: AttendeeID[] = [
   {
-    name: { firstName: "John", lastName: "Doe" },
+    name: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    role: "CAMPER",
+    sessionId: "session1",
     gender: "Male",
-  },
-  {
-    name: { firstName: "Parth", lastName: "Sangani" },
-    gender: "Male",
+    dateOfBirth: "01-01-2025",
+    nonoList: [1, 2, 3],
+    ageGroup: "NAV",
+    level: 1,
+    bunk: 1,
+    id: 1,
+    swimOptOut: false,
   },
 ];
 
 export const DirectoryTableView = () => {
-  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+  const columns = useMemo<MRT_ColumnDef<AttendeeID>[]>(
     () => [
       {
-        accessorFn: (row) => `${row.name.firstName} ${row.name.lastName}`,
-        id: "fullname",
-        header: "Name",
+        accessorFn: (row) => `${row.name.firstName}`,
+        id: "firstName",
+        header: "FIRST NAME",
+      },
+      {
+        accessorFn: (row) => `${row.name.lastName}`,
+        id: "lastName",
+        header: "LAST NAME",
+      },
+      {
+        accessorKey: "ageGroup",
+        id: "ageGroup",
+        header: "AGE GROUP",
+      },
+      {
+        accessorKey: "bunk",
+        id: "bunk",
+        header: "BUNK",
+      },
+      {
+        accessorKey: "health",
+        id: "health",
+        header: "HEALTH",
       },
       {
         accessorKey: "gender",
         header: "Gender",
+      },
+      {
+        accessorKey: "nonoList",
+        id: "nonoList",
+        header: "NO-NO LIST",
+      },
+      {
+        accessorKey: "yesyesList",
+        id: "yesyesList",
+        header: "YES-YES LIST",
+      },
+      {
+        accessorKey: "dateOfBirth",
+        id: "dateOfBirth",
+        header: "DATE OF BIRTH",
       },
     ],
     []
