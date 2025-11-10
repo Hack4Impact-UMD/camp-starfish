fbe92ac154bb1a7cbcb532e3ef57a11d8cf3a174
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-11-09 23:19:25 -0500

    move NotificationIcon component to separate file

--- src/features/notifications/Notification.tsx
+++ src/features/notifications/Notification.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import {
   Notification as MantineNotification,
   NotificationProps as MantineNotificationProps,
-  ThemeIcon,
 } from "@mantine/core";
-import Image from "next/image";
-import successIcon from "@/assets/icons/notificationSuccess.svg";
-import errorIcon from "@/assets/icons/notificationError.svg";
+import NotificationIcon from "./NotificationIcon";
 
 export type NotificationVariant = "success" | "error";
 
@@ -15,49 +12,12 @@ interface NotificationProps extends MantineNotificationProps {
   variant: NotificationVariant;
 }
 
-const DefaultIcon = ({ variant }: { variant: NotificationVariant }) => {
-  const isSuccess = variant === "success";
-  return (
-    <ThemeIcon
-      color={isSuccess ? "success" : "error"}
-      variant="filled"
-      radius="xl"
-      size={28}
-      style={{ border: "none" }}
-    >
-      <div
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          overflow: "hidden",
-          borderRadius: "50%",
-        }}
-      >
-        <Image
-          src={isSuccess ? successIcon : errorIcon}
-          alt={isSuccess ? "Success" : "Error"}
-          fill
-          style={{
-            objectFit: "contain",
-            display: "block",
-            transform: "scale(1.55)",
-            transformOrigin: "center",
-          }}
-          sizes="28px"
-        />
-      </div>
-    </ThemeIcon>
-  );
-};
-
 export default function Notification(props: NotificationProps) {
-  const { variant, message, onClose, ...rest } = props;
+  const { variant, message, ...rest } = props;
   return (
     <MantineNotification
       withCloseButton
-      onClose={onClose}
-      icon={<DefaultIcon variant={variant} />}
+      icon={<NotificationIcon variant={variant} />}
       closeButtonProps={{ size: "xl", iconSize: 30 }}
       {...rest}
     >
