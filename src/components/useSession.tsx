e1274cf5395668ff319e86843f5dedf267117009
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-11-19 20:19:51 -0500

    refactor useSession hook

--- src/components/useSession.tsx
+++ src/components/useSession.tsx
@@ -1,25 +1,9 @@
-import { db } from "@/config/firebase";
-import { collection, doc, getDoc, setDoc } from "firebase/firestore";
-import { Session } from "@/types/sessionTypes";
 import { useQuery } from "@tanstack/react-query";
-import { getSessionDates } from "@/data/firestore/sessions";
+import { getSessionById } from "@/data/firestore/sessions";
 
-function useSession(id: string) {
-  const sessionsRef = collection(db, "sessions");
-  const {
-    data: session,
-    isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["sessionData", id],
-    queryFn: getSessionDates,
+export default function useSession(sessionId: string) {
+  return useQuery({
+    queryKey: ["session", sessionId],
+    queryFn: () => getSessionById(sessionId),
   });
-
-  return {
-    session,
-    isLoading,
-    error,
-  };
 }
-
-export default useSession;
