47f460c8b6b727b7ef4c17e6747d0264341dd853
Esha Vigneswaran <eshavigneswaran@gmail.com>
2025-11-28 11:58:41 -0500

    Added notifs to successful session creations and for errors

--- src/hooks/sessions/useCreateSessions.ts
+++ src/hooks/sessions/useCreateSessions.ts
@@ -1,17 +1,23 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { setSession } from '@/data/firestore/sessions';
 import { Session } from '../../types/sessionTypes';
+import useNotifications from '@/features/notifications/useNotifications';
 
 export function useCreateSession() {
   const queryClient = useQueryClient();
+  const { success, error } = useNotifications();
 
   return useMutation({
-    mutationFn: async (data: Session) => setSession(data), 
+    mutationFn: async (data: Session) => setSession(data),
+
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['sessions'] });
+      success("Session created successfully!");
     },
-    onError: (error) => {
-      console.error('Error creating session:', error);
+
+    onError: (err: any) => {
+      console.error("Error creating session:", err);
+      error(err?.message || "Failed to create session.");
     },
   });
 }
