58644d08624f01ff9c103abbbf30975e6914f1f7
Varun Rao <31515224+DurjaMan27@users.noreply.github.com>
2025-12-01 08:47:12 -0500

    feat(sessions): create & implement CreateSessionModal (#57)
    - Implemented CreateSessionModal
    - Made it use Mantine's Modals Manager
    - Integrated component into SessionsPage
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    * **New Features**
    * Added a modal interface for creating new sessions with date range
    picker functionality, requiring a session name and valid start/end date
    selection.
    
    * **Style**
    * Enhanced theming for date picker and text input components with
    improved styling and customization.
    
    <sub>✏️ Tip: You can customize this high-level summary in your review
    settings.</sub>
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Esha Vigneswaran <eshavigneswaran@gmail.com>
    Co-authored-by: Nitin Kanchinadam <nitin.kanchinadam@gmail.com>

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -109,7 +109,31 @@ export const theme: MantineThemeOverride = {
   },
   defaultRadius: "xl",
   cursorType: "pointer",
+
   components: {
+    DatePicker: {
+      defaultProps: {
+        firstDayOfWeek: 0,
+
+      },
+      classNames: {
+        calendarHeader: "mb-[20px]",
+        calendarHeaderLevel: "!text-[14px] !font-bold text-primary-5",
+        weekday: "!text-neutral-5 !font-[600] !text-[14px]  border-none",
+
+        day: `
+          !text-[14px] h-[38px] !text-primary-6 !p-[1px]
+          hover:bg-primary-0
+          data-[selected]:bg-link-0 data-[selected]:text-white
+          data-[selected]:hover:bg-primary-6
+          data-[in-range]:bg-[rgba(34,139,230,0.2)]
+          data-[first-in-range]:!bg-link-0 data-[first-in-range]:!text-white data-[first-in-range]:!rounded-sm 
+          data-[last-in-range]:!bg-link-0 data-[last-in-range]:!text-white data-[last-in-range]:!rounded-sm
+        `,
+        root: "!border !border-primary-5 !rounded-lg !p-2",
+
+      }
+    },
     Notification: {
       styles: (theme: MantineTheme, props: NotificationProps) => {
         const { variant = 'success' } = props;
@@ -150,6 +174,9 @@ export const theme: MantineThemeOverride = {
       },
     },
     TextInput: {
+      classNames: {
+        input: "bg-primary-0 rounded-md border-none px-3 py-2 text-sm text-neutral-5 placeholder:text-neutral-5 "
+      },
       defaultProps: {
         radius: "md",
         size: "sm",
@@ -233,12 +260,12 @@ export const theme: MantineThemeOverride = {
               borderColor: 'var(--mantine-color-blue-filled)',
             }
           },
-          icon: { 
+          icon: {
             display: 'none' // Hide default white dot for solid bullets
           }
         },
       },
     },
-  
+
   },
 };
