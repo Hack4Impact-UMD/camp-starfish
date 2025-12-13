40c38ab5d36cf6674a668c792c81a0886ecc090f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-12-13 17:15:15 -0500

    combine similar functions & create corresponding hooks

---
+++ src/features/albumSessionLinking/linkAlbumAndSession.ts
@@ -0,0 +1,34 @@
+import { db } from "@/config/firebase";
+import { getAlbumById, updateAlbum } from "@/data/firestore/albums";
+import { getSessionById, updateSession } from "@/data/firestore/sessions";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { runTransaction, Transaction } from "firebase/firestore";
+
+export default async function linkAlbumAndSession(albumId: string, sessionId: string) {
+  await runTransaction(db, async (transaction: Transaction) => {
+    const session = await getSessionById(sessionId, transaction);
+    const album = await getAlbumById(albumId, transaction);
+
+    if (session.albumId === albumId && album.sessionId === sessionId) {
+      return;
+    } else if (session.albumId) {
+      throw Error(`Session ${sessionId} is already linked to album ${session.albumId}`);
+    } else if (album.sessionId) {
+      throw Error(`Album ${albumId} is already linked to session ${album.sessionId}`);
+    }
+
+    await updateSession(sessionId, { albumId }, transaction);
+    await updateAlbum(albumId, { sessionId }, transaction);
+  });
+}
+
+interface UseLinkAlbumAndSessionVars {
+  albumId: string;
+  sessionId: string;
+}
+
+export function useLinkAlbumAndSession() {
+  return useMutation({
+    mutationFn: ({ albumId, sessionId }: UseLinkAlbumAndSessionVars) => linkAlbumAndSession(albumId, sessionId)
+  })
+}
---
+++ src/features/albumSessionLinking/unlinkAlbumAndSession.ts
@@ -0,0 +1,30 @@
+import { db } from "@/config/firebase";
+import { getAlbumById, updateAlbum } from "@/data/firestore/albums";
+import { getSessionById, updateSession } from "@/data/firestore/sessions";
+import { useMutation } from "@tanstack/react-query";
+import { runTransaction, Transaction } from "firebase/firestore";
+
+type UseUnlinkAlbumAndSessionVars = {
+  albumId: string;
+  sessionId: string;
+}
+
+export default async function unlinkAlbumAndSession(albumId: string, sessionId: string) {
+  await runTransaction(db, async (transaction: Transaction) => {
+    const session = await getSessionById(sessionId, transaction);
+    const album = await getAlbumById(albumId, transaction);
+
+    if (session.albumId !== albumId || album.sessionId !== sessionId) {
+      throw Error(`Session ${sessionId} and Album ${albumId} are not linked to each other`);
+    }
+
+    await updateSession(sessionId, { albumId: undefined }, transaction);
+    await updateAlbum(albumId, { sessionId: undefined }, transaction);
+  });
+}
+
+export function useUnlinkAlbumAndSession() {
+  return useMutation({
+    mutationFn: ({ albumId, sessionId }: UseUnlinkAlbumAndSessionVars) => unlinkAlbumAndSession(albumId, sessionId)
+  })
+}
