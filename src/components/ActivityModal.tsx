79330b5f97dcc903a69086c089e42a5101f253bb
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-12-17 18:50:37 -0500

    update ActivityModal styling

--- src/components/ActivityModal.tsx
+++ src/components/ActivityModal.tsx
@@ -20,6 +20,7 @@ import {
 } from "@/features/scheduling/generation/schedulingUtils";
 import { getActivityName } from "@/utils/activityUtils";
 import { getFullName } from "@/utils/personUtils";
+import { Grid, Stack } from "@mantine/core";
 
 interface ActivityModalProps {
   session: SessionID;
@@ -33,101 +34,78 @@ interface ActivityModalProps {
 
 export default function ActivityModal(props: ActivityModalProps) {
   const { session, section, blockId, activity, campers, staff, admins } = props;
-  const attendeesById = getAttendeesById([...campers, ...staff, ...admins]);
+
+  const { staffNames, camperNames } = useMemo(() => {
+    const attendeesById = getAttendeesById([...campers, ...staff, ...admins]);
+    const assignments = activity.assignments;
+    const adminNames = assignments.adminIds.map((adminId) =>
+      getFullName(attendeesById[adminId])
+    );
+    if (isIndividualAssignments(assignments)) {
+      return {
+        staffNames: [
+          ...assignments.staffIds.map((staffId) =>
+            getFullName(attendeesById[staffId])
+          ),
+          ...adminNames,
+        ],
+        camperNames: assignments.camperIds.map((camperId) =>
+          getFullName(attendeesById[camperId])
+        ),
+      };
+    }
+
+    return {
+      staffNames: assignments.bunkNums.flatMap((bunkNum) =>
+        staff
+          .filter((staff) => staff.bunk === bunkNum)
+          .map((staff) => getFullName(staff))
+      ),
+      camperNames: assignments.bunkNums.flatMap((bunkNum) =>
+        campers
+          .filter((camper) => camper.bunk === bunkNum)
+          .map((camper) => getFullName(camper))
+      ),
+    };
+  }, [campers, staff, admins, activity]);
 
   return (
-    <div className="grid grid-cols-2 justify-center items-center w-full m-0 p-0">
-      {/* Content */}
-      <div className="col-span-2 flex flex-col gap-0 w-full">
-        {/* First Row: Section and Date */}
-        <div className="flex flex-col justify-center items-center bg-[#D6EAF8] rounded-none border-black border-y-2 py-2">
-          <h2 className="text-2xl font-bold mb-2">{section.name}</h2>
-          <p className="text-lg text-camp-text-secondary">
-            {`${moment(section.startDate).format("dddd M/D")} - ${moment(
-              section.endDate
-            ).format("dddd M/D")}`}
-          </p>
-        </div>
-
-        {/* Second Row: Block and Activity */}
-        <div className="flex flex-col justify-center items-center bg-white rounded-none border-black border-b-2 py-2">
-          <h2 className="text-xl font-bold">
-            BLOCK {blockId.toUpperCase()} - {getActivityName(activity)}
-          </h2>
-        </div>
-        <div className="flex flex-col justify-center items-center bg-white rounded-none border-black border-b-2 py-2">
-          <p className="text-md text-center">{activity.description}</p>
-        </div>
-
-        {/* Third Row: Staff and Campers Grid */}
-        <div className="flex flex-row">
-          {/* Staff Column */}
-          <div className="flex flex-col justify-center items-center bg-white border-black border-r-2 w-1/2">
-            <h4 className="text-lg font-bold mb-2 border-black border-b-2 w-full h-full text-center align-middle">
-              Staff
-            </h4>
-            <div className="space-y-3">
-              {isIndividualAssignments(activity.assignments)
-                ? activity.assignments.staffIds.map((staffId) => (
-                    <div
-                      key={staffId}
-                      className="border border-gray-300 rounded p-3 text-center bg-white"
-                    >
-                      {getFullName(attendeesById[staffId])}
-                    </div>
-                  ))
-                : activity.assignments.bunkNums.map((bunkNum) => (
-                    <div
-                      key={bunkNum}
-                      className="border border-gray-300 rounded p-3 text-center bg-white"
-                    >
-                      {`Bunk ${bunkNum}`}
-                    </div>
-                  ))}
-
-              {activity.assignments.adminIds &&
-                activity.assignments.adminIds.map((adminId) => (
-                  <div
-                    key={adminId}
-                    className="border border-gray-300 rounded p-3 text-center bg-white"
-                  >
-                    {getFullName(attendeesById[adminId])}
-                  </div>
-                ))}
-            </div>
+    <div className="grid grid-cols-2">
+      <div className="col-span-2 flex flex-col justify-center items-center bg-[#D6EAF8] rounded-none border-black border-y-2 py-2 w-full">
+        <h2 className="text-2xl font-bold mb-2">{section.name}</h2>
+        <p className="text-lg">
+          {`${moment(section.startDate).format("dddd M/D")} - ${moment(
+            section.endDate
+          ).format("dddd M/D")}`}
+        </p>
+      </div>
+      <div className="col-span-2 flex flex-col justify-center items-center bg-white rounded-none border-black border-b-2 py-2 w-full">
+        <h2 className="text-xl font-bold">
+          BLOCK {blockId.toUpperCase()} - {getActivityName(activity)}
+        </h2>
+      </div>
+      <div className="col-span-2 flex flex-col justify-center items-center bg-white rounded-none border-black border-b-2 py-2 w-full">
+        <p className="text-md text-center">{activity.description}</p>
+      </div>
+      <p className="text-lg font-bold mb-2 border-black border-b-2 border-r-2 w-full h-full text-center align-middle">
+        Staff
+      </p>
+      <p className="text-lg font-bold mb-2 border-black border-b-2 w-full h-full text-center align-middle">
+        Campers
+      </p>
+      <div className="space-y-3 border-black border-r-2">
+        {staffNames.map((staffName) => (
+          <div key={staffName} className="p-3 text-center bg-white">
+            {staffName}
           </div>
-
-          {/* Campers Column */}
-          <div className="flex flex-col justify-center items-center bg-white border-black w-1/2">
-            <h4 className="text-lg font-bold mb-2 border-black border-b-2 w-full h-full text-center align-middle">
-              Campers
-            </h4>
-            <div className="space-y-3">
-              {"camperIds" in activity.assignments &&
-                (activity.assignments as IndividualAssignments).camperIds?.map(
-                  (camperId) => (
-                    <div
-                      key={camperId}
-                      className="border border-gray-300 rounded p-3 text-center bg-white"
-                    >
-                      {getFullName(attendeesById[camperId])}
-                    </div>
-                  )
-                )}
-              {"bunkNums" in activity.assignments &&
-                (activity.assignments as BunkAssignments).bunkNums?.map(
-                  (bunkNum) => (
-                    <div
-                      key={bunkNum}
-                      className="border border-gray-300 rounded p-3 text-center bg-white"
-                    >
-                      Bunk {bunkNum}
-                    </div>
-                  )
-                )}
-            </div>
+        ))}
+      </div>
+      <div className="space-y-3">
+        {camperNames.map((camperName) => (
+          <div key={camperName} className="p-3 text-center bg-white">
+            {camperName}
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
