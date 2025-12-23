bf8d6844f64d32dbe43009adf6253c4700d83e3a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2025-12-23 09:08:21 -0400

    delete leftover prop

--- src/components/NightShiftTable.tsx
+++ src/components/NightShiftTable.tsx
@@ -170,7 +170,6 @@ export default function NightScheduleTable(props: NightScheduleTableProps) {
             </div>
           </div>
         ),
-        accessorFn: 
       },
       {
         accessorKey: "position",
