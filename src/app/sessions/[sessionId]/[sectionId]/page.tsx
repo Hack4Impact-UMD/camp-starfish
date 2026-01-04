e2c84c4347cfc3e1299b3bc697eeb79416922fe7
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-01-03 22:45:00 -0500

    fix params typing

--- src/app/sessions/[sessionId]/[sectionId]/page.tsx
+++ src/app/sessions/[sessionId]/[sectionId]/page.tsx
@@ -5,7 +5,7 @@ interface SectionRouteParams {
   sectionId: string;
 }
 
-export default async function SectionRoute(props: { params: SectionRouteParams }) {
+export default async function SectionRoute(props: { params: Promise<SectionRouteParams> }) {
   const { sessionId, sectionId } = await props.params;
   return <SectionPage sessionId={sessionId} sectionId={sectionId} />
 }
