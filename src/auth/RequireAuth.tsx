326e472db394f24e0a82892b2f7a84c6d0d38052
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-01-07 21:31:42 -0500

    add fallbackComponent for when all AuthCases evaluate to false

--- src/auth/RequireAuth.tsx
+++ src/auth/RequireAuth.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Error from "@/app/error";
-
 interface AuthCase {
   authFn: () => boolean;
   component: React.ReactNode;
@@ -9,24 +7,15 @@ interface AuthCase {
 
 interface RequireAuthProps {
   authCases: AuthCase[];
+  fallbackComponent: React.ReactNode;
 }
 
 export default function RequireAuth(props: RequireAuthProps) {
-  const { authCases } = props;
-  // Iterate through auth cases and return the first matching component
+  const { authCases, fallbackComponent } = props;
   for (const authCase of authCases) {
     if (authCase.authFn()) {
-      return <>{authCase.component}</>;
+      return authCase.component;
     }
   }
-
-  // Default error page when all functions evaluate to false
-  const accessError: Error & { digest?: string } = {
-    name: "Access Denied",
-    message: "You do not have permission to view this page.",
-  };
-  
-  return (
-    <Error error={accessError} />
-  );
+  return fallbackComponent;  
 }
