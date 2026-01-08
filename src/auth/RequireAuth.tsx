16a4bb1758ab93cd8b64d97b1570324d040f1aed
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-01-07 22:48:34 -0500

    make fallbackComponent optional

--- src/auth/RequireAuth.tsx
+++ src/auth/RequireAuth.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import ErrorPage from "@/app/error";
+
 interface AuthCase {
   authFn: () => boolean;
   component: React.ReactNode;
@@ -7,15 +9,18 @@ interface AuthCase {
 
 interface RequireAuthProps {
   authCases: AuthCase[];
-  fallbackComponent: React.ReactNode;
+  fallbackComponent?: React.ReactNode;
 }
 
 export default function RequireAuth(props: RequireAuthProps) {
-  const { authCases, fallbackComponent } = props;
+  const {
+    authCases,
+    fallbackComponent = <ErrorPage error={Error("You do not have permission to access this page.")} />,
+  } = props;
   for (const authCase of authCases) {
     if (authCase.authFn()) {
       return authCase.component;
     }
   }
-  return fallbackComponent;  
+  return fallbackComponent;
 }
