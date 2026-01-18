dd8207957cfc0609b58663f70d8061d8487b3593
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-01-18 14:59:48 -0500

    refactor user types except admin & staff

--- src/typesNew/userTypes.ts
+++ src/typesNew/userTypes.ts
@@ -1,17 +1,37 @@
 import { Moment } from "moment";
 
 interface BaseUser {
-  id: string;
+  id: number;
+  uid: string;
+  email: string;
   name: {
     firstName: string;
     middleName?: string;
     lastName: string;
   };
   role: Role;
-  gender: "Male" | "Female" | "Other";
+  gender: Gender;
   dateOfBirth: Moment;
-  
+
 }
 
 type Role = "CAMPER" | "PARENT" | "STAFF" | "PHOTOGRAPHER" | "ADMIN";
 type Gender = "Male" | "Female" | "Other";
+type PhotoPermissions = "PUBLIC" | "PRIVATE";
+
+
+interface Camper extends BaseUser {
+  role: "CAMPER";
+  photoPermissions: PhotoPermissions;
+  parentIds: number[];
+  nonoListIds: number[];
+}
+
+interface Parent extends BaseUser {
+  role: "PARENT";
+  camperIds: number[];
+}
+
+interface Photographer extends BaseUser {
+  role: "PHOTOGRAPHER";
+}
