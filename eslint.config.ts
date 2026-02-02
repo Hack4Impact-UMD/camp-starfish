c4c5966ebff14234f5513a7cfc4cf019a8ef04a8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-01 21:11:17 -0500

    remove unused variables

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 import importPlugin from "eslint-plugin-import";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 const eslintConfig = [
   ...[...nextVitals, ...nextTs].map(config => ({
