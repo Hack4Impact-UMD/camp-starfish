3857a189f1079fdc3e9d29ac077a7b4d860970d7
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-02 19:14:18 -0500

    fix linter for functions & apps-script packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,35 +1,42 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import importPlugin from "eslint-plugin-import";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = [
+  {
+    files: ["./**/*.{ts,tsx}",],
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      "import": importPlugin
+    },
+    languageOptions: {
+      parser: tsParser,
+      sourceType: "module",
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ["tsconfig.json"],
+        },
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { ignoreRestSiblings: true },
+      ],
+      "import/no-unresolved": 'error',
+    },
+  },
   ...[...nextVitals, ...nextTs].map(config => ({
     ...config,
     files: [
       "./src/**/*.{ts,tsx}",
     ],
   })),
-  {
-    files: [
-      "./src/**/*.{ts,tsx}",
-      "./functions/src/**/*.{ts,tsx}",
-      "./apps-script/src/**/*.{ts,tsx}",
-    ],
-    rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        { ignoreRestSiblings: true },
-      ],
-    },
-  },
-  {
-    files: [
-      "./src/**/*.{ts,tsx}",
-      "./functions/src/**/*.{ts,tsx}",
-    ],
-    rules: {
-      "import/no-unresolved": 'error',
-    }
-  },
 ]
 
 export default eslintConfig;
