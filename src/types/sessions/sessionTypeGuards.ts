588749c5a7d42e520a1fb92f82b3d1f80b2f9f4e
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-04 23:45:13 -0500

    fix type guard

--- src/types/sessions/sessionTypeGuards.ts
+++ src/types/sessions/sessionTypeGuards.ts
@@ -1,6 +1,6 @@
-import { AdminAttendee, Attendee, CamperAttendee, CommonSection, SchedulingSection, SchedulingSectionType, Section, StaffAttendee } from "./sessionTypes";
+import { AdminAttendee, Attendee, CamperAttendee, CommonSection, SchedulingSection, SchedulingSectionType, Section, SectionType, StaffAttendee } from "./sessionTypes";
 
-export function isSchedulingSectionType(sectionType: SchedulingSectionType): sectionType is SchedulingSectionType { return sectionType === "BUNDLE" || sectionType === "BUNK-JAMBO" || sectionType === "NON-BUNK-JAMBO"; };
+export function isSchedulingSectionType(sectionType: SectionType): sectionType is SchedulingSectionType { return sectionType === "BUNDLE" || sectionType === "BUNK-JAMBO" || sectionType === "NON-BUNK-JAMBO"; };
 
 export function isCommonSection(section: Section): section is CommonSection { return section.type === "COMMON"; };
 export function isSchedulingSection(section: Section): section is SchedulingSection { return section.type === "BUNDLE" || section.type === "BUNK-JAMBO" || section.type === "NON-BUNK-JAMBO"; };
