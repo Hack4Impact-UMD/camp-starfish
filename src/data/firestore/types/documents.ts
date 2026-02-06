c1fe847b68cbfd2f8d0767102e70caa886ffa92a
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-05 23:21:27 -0500

    fix typing of documents based on discriminated unions

--- src/data/firestore/types/documents.ts
+++ src/data/firestore/types/documents.ts
@@ -2,16 +2,17 @@ import { Album, AlbumItem } from "@/types/albums/albumTypes";
 import { ProgramArea, SectionSchedule } from "@/types/scheduling/schedulingTypes";
 import { Attendee, Bunk, Freeplay, NightSchedule, Post, Section, Session } from "@/types/sessions/sessionTypes";
 import { User } from "@/types/users/userTypes";
+import { DistributiveOmit } from "@/utils/types/typeUtils";
 
 export type AlbumDoc = Omit<Album, "id">;
 export type AlbumItemDoc = Omit<AlbumItem, "id" | "albumId">;
 
-export type UserDoc = Omit<User, "id">;
+export type UserDoc = DistributiveOmit<User, "id">;
 
 export type SessionDoc = Omit<Session, "id">;
-export type AttendeeDoc = Omit<Attendee, "sessionId" | "attendeeId">;
-export type SectionDoc = Omit<Section, "id" | "sessionId">;
-export type SectionScheduleDoc = Omit<SectionSchedule, "id" | "sessionId">;
+export type AttendeeDoc = DistributiveOmit<Attendee, "sessionId" | "attendeeId">;
+export type SectionDoc = DistributiveOmit<Section, "id" | "sessionId">;
+export type SectionScheduleDoc = DistributiveOmit<SectionSchedule, "id" | "sessionId">;
 export type BunkDoc = Omit<Bunk, "bunkNum" | "sessionId">;
 export type NightScheduleDoc = Omit<NightSchedule, "date" | "sessionId">;
 export type FreeplayDoc = Omit<Freeplay, "date" | "sessionId">;
