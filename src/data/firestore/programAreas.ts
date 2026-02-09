57ad00c9cf97f7943e1e0545d51ff0e0aa386bb3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-02-08 19:02:46 -0500

    create basic queries & hooks for program areas

---
+++ src/data/firestore/programAreas.ts
@@ -0,0 +1,47 @@
+import { db } from "@/config/firebase";
+import { ProgramArea } from "@/types/scheduling/schedulingTypes";
+import { ProgramAreaDoc } from "./types/documents";
+import {
+  doc,
+  Transaction,
+  WriteBatch,
+  QueryDocumentSnapshot,
+  FirestoreDataConverter,
+  WithFieldValue,
+  DocumentReference,
+  collection,
+  where,
+  query,
+  Query,
+  UpdateData
+} from "firebase/firestore";
+import { setDoc, getDoc, updateDoc, executeQuery, deleteDoc } from "./firestoreClientOperations";
+import { Collection } from "./types/collections";
+
+const programAreaFirestoreConverter: FirestoreDataConverter<ProgramArea, ProgramAreaDoc> = {
+  toFirestore: (programArea: WithFieldValue<ProgramArea>) => {
+    const { id, ...dto } = programArea;
+    return dto as WithFieldValue<ProgramArea>;
+  },
+  fromFirestore: (snapshot: QueryDocumentSnapshot<ProgramAreaDoc, ProgramAreaDoc>): ProgramArea => ({ id: snapshot.ref.id, ...snapshot.data() })
+};
+
+export async function getProgramAreaById(id: string, transaction?: Transaction): Promise<ProgramArea> {
+  return await getDoc<ProgramArea, ProgramAreaDoc>(doc(db, Collection.PROGRAM_AREAS, id) as DocumentReference<ProgramArea, ProgramAreaDoc>, programAreaFirestoreConverter, transaction);
+};
+
+export async function getProgramAreasByIds(ids: string[]): Promise<ProgramArea[]> {
+  return await executeQuery<ProgramArea, ProgramAreaDoc>(query(collection(db, Collection.PROGRAM_AREAS), where("id", "in", ids)) as Query<ProgramArea, ProgramAreaDoc>, programAreaFirestoreConverter);
+}
+
+export async function setProgramArea(id: string, programArea: ProgramAreaDoc, instance?: Transaction | WriteBatch): Promise<void> {
+  await setDoc<ProgramArea, ProgramAreaDoc>(doc(db, Collection.PROGRAM_AREAS, id) as DocumentReference<ProgramArea, ProgramAreaDoc>, { id, ...programArea }, programAreaFirestoreConverter, instance);
+}
+
+export async function updateProgramArea(id: string, updates: UpdateData<ProgramAreaDoc>, instance?: Transaction | WriteBatch): Promise<void> {
+  await updateDoc<ProgramArea, ProgramAreaDoc>(doc(db, Collection.PROGRAM_AREAS, id) as DocumentReference<ProgramArea, ProgramAreaDoc>, updates, programAreaFirestoreConverter, instance);
+}
+
+export async function deleteProgramArea(id: string, instance?: Transaction | WriteBatch): Promise<void> {
+  await deleteDoc<ProgramArea, ProgramAreaDoc>(doc(db, Collection.PROGRAM_AREAS, id) as DocumentReference<ProgramArea, ProgramAreaDoc>, programAreaFirestoreConverter, instance);
+}
---
+++ src/hooks/programAreas/useProgramAreas.ts
@@ -0,0 +1,14 @@
+import { getProgramAreasByIds } from "@/data/firestore/programAreas";
+import { useQuery, useQueryClient, UseQueryOptions } from "@tanstack/react-query";
+
+export default function useProgramAreas(ids: string[]) {
+  const queryClient = useQueryClient();
+  return useQuery({
+    queryKey: ['programAreas', ids],
+    queryFn: async () => {
+      const programAreas = await getProgramAreasByIds(ids);
+      programAreas.forEach(programArea => queryClient.setQueryData(['programAreas', programArea.id], programArea));
+    },
+    enabled: !!ids.length
+  })
+}
