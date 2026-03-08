194a4906318ed9569e7d0f00786b04891ee72836
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-03-08 14:36:02 -0400

    add default shades to generate-theme-override script

--- scripts/generate-theme-override.ts
+++ scripts/generate-theme-override.ts
@@ -4,12 +4,14 @@ import { appendFileSync } from "fs";
 
 const mergedTheme = mergeThemeOverrides(DEFAULT_THEME, theme);
 const colors = mergedTheme.colors || {};
+const primaryShade = typeof mergedTheme.primaryShade === 'number' ? mergedTheme.primaryShade : mergedTheme.primaryShade?.light ?? 5;
 let css = "@theme {\n";
 for (const [color, shades] of Object.entries(colors)) {
   if (!shades) continue;
   for (let i = 0; i < shades.length; i++) {
     css += `--color-${color}-${i}: ${shades[i]};\n`
   }
+  css += `--color-${color}: ${shades[primaryShade]};\n`
 }
 css += "}\n";
 appendFileSync("./src/styles/theme.css", css);
