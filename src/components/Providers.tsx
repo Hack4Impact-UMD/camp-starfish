80ea6ff9fa3e1c4ccf0f42eae5c0f03776df64ad
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-03-07 22:58:08 -0500

    remove "withCssVariables" prop

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -17,7 +17,7 @@ export default function Providers(props: ProvidersProps) {
   const { children } = props;
   return (
     <QueryClientProvider client={queryClient}>
-      <MantineProvider theme={theme} withCssVariables>
+      <MantineProvider theme={theme}>
         <Notifications />
         <ModalsProvider>
           <AuthProvider>{children}</AuthProvider>
