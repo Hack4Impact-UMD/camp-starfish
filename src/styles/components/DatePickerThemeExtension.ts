010c24bc4540a53502b0a9a7f3976d4030b209b2
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-03-14 15:08:32 -0400

    remove rounding on days in DatePIcker

--- src/styles/components/DatePickerThemeExtension.ts
+++ src/styles/components/DatePickerThemeExtension.ts
@@ -5,7 +5,7 @@ const DatePickerThemeExtension = DatePicker.extend({
     datePickerRoot: 'border-2 border-blue-9',
     calendarHeaderLevel: "text-sm font-bold text-blue-9 font-Lato",
     weekday: "text-neutral-5 font-semibold font-Lato text-sm border-none",
-    day: "text-sm h-9 text-blue-9 font-normal font-Lato p-2 not-data-in-range:hover:rounded-sm data-disabled:text-neutral-4 data-outside:text-neutral-5 data-selected:bg-neutral-8 data-selected:text-neutral-0 data-first-in-range:rounded-s-sm data-last-in-range:rounded-e-sm",
+    day: "text-sm h-9 text-blue-9 font-normal font-Lato p-2 rounded-none data-disabled:text-neutral-4 data-outside:text-neutral-5 data-selected:bg-neutral-8 data-selected:text-neutral-0",
   },
   defaultProps: {
     firstDayOfWeek: 0,
