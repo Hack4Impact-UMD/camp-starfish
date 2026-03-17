df95d1286945fc828ac4b21f0956567091317138
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-03-17 19:35:52 -0400

    style Modal header in theme extension

--- src/styles/components/ModalThemeExtension.ts
+++ src/styles/components/ModalThemeExtension.ts
@@ -4,7 +4,8 @@ const ModalThemeExtension = Modal.extend({
   classNames: {
     content: 'rounded-none',
     header: 'rounded-none bg-blue-8',
-    title: 'text-white text-2xl'
+    title: 'text-white text-2xl uppercase font-Lato font-black',
+    close: 'text-white hover:bg-blue-6'
   }
 });
 
