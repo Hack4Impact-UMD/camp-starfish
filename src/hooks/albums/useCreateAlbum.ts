b50d766cb175edfae46e712d04eda4b1c061a62c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-03-17 19:34:07 -0400

    create album mutation hooks

---
+++ src/hooks/albums/useCreateAlbum.ts
@@ -0,0 +1,13 @@
+import { setAlbum } from "@/data/firestore/albums";
+import { AlbumDoc } from "@/data/firestore/types/documents";
+import { useMutation } from "@tanstack/react-query";
+
+interface UseCreateAlbumParams {
+  album: AlbumDoc;
+}
+
+export default function useCreateAlbum() {
+  return useMutation({
+    mutationFn: (params: UseCreateAlbumParams) => setAlbum(params.album)
+  })
+}
