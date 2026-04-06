825a6e841eeeec63f890093c96cad90ebc1e0fc5
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-05 20:13:44 -0400

    implement basic Dropzone functionality for album thumbnail

--- src/components/EditAlbumModal.tsx
+++ src/components/EditAlbumModal.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { modals } from "@mantine/modals";
 import useAlbumById from "@/hooks/albums/useAlbumById";
-import { Button, FileInput, Text, TextInput } from "@mantine/core";
+import { Button, Image, Indicator, Text, TextInput } from "@mantine/core";
 import { Dropzone, FileWithPath } from "@mantine/dropzone";
 import LoadingPage from "@/app/loading";
 import ErrorPage from "@/app/error";
-import { MdImage } from "react-icons/md";
+import { MdClose, MdImage } from "react-icons/md";
 
 interface EditAlbumModalProps {
   albumId?: string;
@@ -20,29 +20,59 @@ export default function EditAlbumModal(props: EditAlbumModalProps) {
   const [albumName, setAlbumName] = useState<string>(
     albumQuery.data?.name || "",
   );
+  const [albumThumbnail, setAlbumThumbnail] = useState<File | null>(null);
+  const [albumThumbnailURL, setAlbumThumbnailURL] = useState<string | null>(
+    null,
+  );
+
+  useEffect(() => {
+    if (!albumThumbnail) {
+      setAlbumThumbnailURL(null);
+      return;
+    }
+    const albumThumbnailURL = URL.createObjectURL(albumThumbnail);
+    setAlbumThumbnailURL(albumThumbnailURL);
+    return () => URL.revokeObjectURL(albumThumbnailURL);
+  }, [albumThumbnail]);
 
   if (albumQuery.isLoading) return <LoadingPage />;
   else if (albumQuery.isError) return <ErrorPage error={albumQuery.error} />;
 
   return (
     <div className="flex flex-col items-center w-full h-full">
-      <Dropzone
-        multiple={false}
-        classNames={{
-          root: 'flex justify-center items-center w-2/3 aspect-square m-md',
-          inner: 'w-full h-full'
-        }}
-      >
-        <div className="flex flex-col justify-center items-center w-full h-full bg-blue-0 rounded-sm">
-          <MdImage className="text-neutral-4 w-10 h-10" size={40} />
-          <Text classNames={{ root: 'text-neutral-5' }}>Upload album thumbnail</Text>
-        </div>
-      </Dropzone>
+      {albumThumbnail && albumThumbnailURL ? (
+        <Indicator
+          classNames={{
+            root: 'flex justify-center items-center w-2/3 h-full m-md bg-blue-0 aspect-square bg-contain border',
+            indicator: 'cursor-pointer'
+          }}
+          label={<MdClose onClick={() => setAlbumThumbnail(null)} />}
+          size={20}
+        >
+          <Image src={albumThumbnailURL} alt={albumThumbnail.name} width={10} height={10} className="w-full h-full object-contain" />
+        </Indicator>
+      ) : (
+        <Dropzone
+          onDrop={(files: FileWithPath[]) => setAlbumThumbnail(files[0])}
+          multiple={false}
+          classNames={{
+            root: "flex justify-center items-center w-2/3 h-full aspect-square m-md",
+            inner: "w-full h-full",
+          }}
+        >
+          <div className="flex flex-col justify-center items-center w-full h-full p-2 bg-blue-0">
+            <MdImage className="text-neutral-4 w-10 h-10" size={40} />
+            <Text classNames={{ root: "text-neutral-5" }}>
+              Upload album thumbnail
+            </Text>
+          </div>
+        </Dropzone>
+      )}
 
       <TextInput
         label="Album Title"
         onChange={(e) => setAlbumName(e.target.value)}
-        classNames={{ root: 'w-4/5' }}
+        classNames={{ root: "w-4/5" }}
         value={albumName}
       />
 
