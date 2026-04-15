e2bf58f024e568f88d4c5b7bfafe82807861da40
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-14 21:33:29 -0400

    create NestedFieldPath type for getting all possible field paths of a JS object using dot notation

--- src/utils/types/typeUtils.ts
+++ src/utils/types/typeUtils.ts
@@ -1,2 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
+
+export type NestedFieldPath<T, Prefix extends string = ""> =
+  T extends object
+  ? {
+    [K in keyof T & string]:
+    | `${Prefix}${K}`
+    | NestedFieldPath<T[K], `${Prefix}${K}.`>;
+  }[keyof T & string]
+  : never;
