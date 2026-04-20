5b68f9051a1ac6f4c62a6dd8eca41cbfd459f859
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-19 23:51:06 -0400

    convert useAlbums to an infinite query that accepts QueryOptions

--- src/hooks/albums/useAlbums.ts
+++ src/hooks/albums/useAlbums.ts
@@ -1,15 +1,25 @@
 import { getAlbumDocs } from "@/data/firestore/albums";
+import { QueryOptions } from "@/data/firestore/firestoreClientOperations";
+import { AlbumDoc } from "@/data/firestore/types/documents";
 import { Album } from "@/types/albums/albumTypes";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
+import { QueryDocumentSnapshot } from "firebase/firestore";
 
-export default function useAlbums() {
+export default function useAlbums(queryOptions: QueryOptions<AlbumDoc>) {
   const queryClient = useQueryClient();
-  return useQuery({
-    queryKey: ['albums'],
-    queryFn: async () => {
-      const albums = await getAlbumDocs();
-      albums.forEach((album: Album) => queryClient.setQueryData(['albums', album.id], album));
-      return albums;
+  return useInfiniteQuery({
+    queryKey: ['albums', queryOptions],
+    queryFn: async ({ pageParam }) => {
+      const albumsPage = await getAlbumDocs({
+        ...queryOptions,
+        startAfter: pageParam,
+        startAt: undefined
+      });
+      albumsPage.docs.forEach((album: Album) => queryClient.setQueryData(['albums', album.id], album))
+      return albumsPage;
     },
-  })
+    initialPageParam: undefined as QueryDocumentSnapshot<AlbumDoc, AlbumDoc> | undefined,
+    getNextPageParam: (lastPage) => lastPage.lastSnapshot,
+    getPreviousPageParam: (firstPage) => firstPage.firstSnapshot
+  });
 }
