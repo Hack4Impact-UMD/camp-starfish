2ad51c345916e590ba7c5542807ba76ce80a469b
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-20 17:51:51 -0400

    allow forward & backward pagination in useAlbums query

--- src/hooks/albums/useAlbums.ts
+++ src/hooks/albums/useAlbums.ts
@@ -5,21 +5,31 @@ import { Album } from "@/types/albums/albumTypes";
 import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
 import { QueryDocumentSnapshot } from "firebase/firestore";
 
+interface TanstackQueryFirestorePageParam {
+  direction: 'previous' | 'next';
+  snapshot: QueryDocumentSnapshot<AlbumDoc, AlbumDoc>;
+}
+
 export default function useAlbums(queryOptions: QueryOptions<AlbumDoc>) {
   const queryClient = useQueryClient();
   return useInfiniteQuery({
     queryKey: ['albums', queryOptions],
     queryFn: async ({ pageParam }) => {
-      const albumsPage = await getAlbumDocs({
-        ...queryOptions,
-        startAfter: pageParam,
-        startAt: undefined
-      });
+      if (pageParam) {
+        if (pageParam.direction === 'previous') {
+          queryOptions.startAfter = pageParam.snapshot;
+          queryOptions.startAt = undefined;
+        } else {
+          queryOptions.endBefore = pageParam.snapshot;
+          queryOptions.endAt = undefined;
+        }
+      }
+      const albumsPage = await getAlbumDocs(queryOptions);
       albumsPage.docs.forEach((album: Album) => queryClient.setQueryData(['albums', album.id], album))
       return albumsPage;
     },
-    initialPageParam: undefined as QueryDocumentSnapshot<AlbumDoc, AlbumDoc> | undefined,
-    getNextPageParam: (lastPage) => lastPage.lastSnapshot,
-    getPreviousPageParam: (firstPage) => firstPage.firstSnapshot
+    initialPageParam: undefined as TanstackQueryFirestorePageParam | undefined,
+    getPreviousPageParam: (firstPage) => firstPage.firstSnapshot ? ({ direction: 'previous' as const, snapshot: firstPage.firstSnapshot }) : undefined,
+    getNextPageParam: (lastPage) => lastPage.lastSnapshot ? ({ direction: 'next' as const, snapshot: lastPage.lastSnapshot }) : undefined,
   });
 }
