58c96f85213019560aa67d9294ddbb383b97c3c4
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-20 22:47:39 -0400

    update AlbumCard text

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -3,13 +3,27 @@ import useAlbum from "@/hooks/albums/useAlbum";
 import ErrorPage from "@/app/error";
 import LoadingPage from "@/app/loading";
 import { useRouter } from "next/navigation";
+import { Album } from "@/types/albums/albumTypes";
 interface AlbumCardProps {
   albumId: string;
 }
 
+function getAlbumCardText(album: Album) {
+  const { startDate, endDate, numItems } = album;
+  if (!startDate || !endDate) {
+    return `${numItems} items`;
+  } else if (startDate.isSame(endDate, 'year')) {
+    if (startDate.isSame(endDate, 'month')) {
+      return `${startDate.format("MMMM YYYY")} • ${numItems} items`;
+    }
+    return `${startDate.format("MMMM")} - ${endDate.format("MMMM YYYY")} • ${numItems} items`;
+  }
+  return `${startDate.format("MMMM YYYY")} - ${endDate.format("MMMM YYYY")} • ${numItems} items`;
+}
+
 export default function AlbumCard(props: AlbumCardProps) {
   const { albumId } = props;
-  const { albumQuery } = useAlbum(albumId, { albumThumbnailURL: true });
+  const { albumQuery, thumbnailQuery } = useAlbum(albumId, { albumThumbnailURL: true });
 
   const router = useRouter();
 
@@ -19,24 +33,22 @@ export default function AlbumCard(props: AlbumCardProps) {
     return <LoadingPage></LoadingPage>;
   }
 
-  const { name, startDate, endDate, numItems, id } = albumQuery.data;
+  const album = albumQuery.data;
   return (
     <div
       className="bg-neutral-0 hover:bg-neutral-2 border border-neutral-3 shadow-sm hover:shadow-lg duration-300 p-4 cursor-pointer"
-      onDoubleClick={() => router.push(`/albums/${id}`)}
+      onDoubleClick={() => router.push(`/albums/${album.id}`)}
     >
       <Image
-        src={null}
-        alt={name}
+        src={thumbnailQuery.data}
+        alt={album.name}
         className="w-full h-48 object-contain"
         width={200}
         height={48}
       />
       <div className="mt-2">
-        <Title order={3}>{name}</Title>
-        <Text>
-          {startDate} - {endDate} • {numItems} photos
-        </Text>
+        <Title order={3}>{album.name}</Title>
+        <Text>{getAlbumCardText(albumQuery.data)}</Text>
       </div>
     </div>
   );
