cf470f211bf5a53f20dd499b23e1b77483191374
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-20 21:56:06 -0400

    rename variables

--- src/hooks/albums/useAlbum.ts
+++ src/hooks/albums/useAlbum.ts
@@ -4,9 +4,8 @@ import useAlbumThumbnailURL from "./useAlbumThumbnailURL";
 export default function useAlbum(albumId: string, options: { albumThumbnailURL: boolean }) {
   const albumDocQuery = useAlbumDoc(albumId);
   const albumThumbnailUrlQuery = useAlbumThumbnailURL(albumId, options.albumThumbnailURL && albumDocQuery.isSuccess && albumDocQuery.data.hasThumbnail);
-
   return {
-    albumDoc: albumDocQuery,
-    albumThumbnailQuery: albumThumbnailUrlQuery
-  }
+    albumQuery: albumDocQuery,
+    thumbnailQuery: albumThumbnailUrlQuery,
+  };
 }
