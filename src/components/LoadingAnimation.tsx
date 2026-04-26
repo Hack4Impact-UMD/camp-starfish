6ff450d9fba2429e08f4422912631dee45813524
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-26 00:43:02 -0400

    format

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -8,39 +8,37 @@ import loadingAnimation75 from "@/assets/loading/loadingAnimation75.svg";
 import loadingAnimation100 from "@/assets/loading/loadingAnimation100.svg";
 
 const svgFiles = [
-    loadingAnimation25,
-    loadingAnimation50,
-    loadingAnimation75,
-    loadingAnimation100,
+  loadingAnimation25,
+  loadingAnimation50,
+  loadingAnimation75,
+  loadingAnimation100,
 ];
 
-
 function LoadingAnimation() {
-    const [animationIdx, setAnimationIdx] = useState(0); // Track current loading animation
+  const [animationIdx, setAnimationIdx] = useState(0); // Track current loading animation
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setAnimationIdx((prev) => (prev < svgFiles.length - 1 ? prev + 1 : 0));
-        }, 400);
-        return () => clearTimeout(timeout);
-    }, [animationIdx, svgFiles.length]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAnimationIdx((prev) => (prev < svgFiles.length - 1 ? prev + 1 : 0));
+    }, 400);
+    return () => clearTimeout(timeout);
+  }, [animationIdx, svgFiles.length]);
 
-    return (
-        <div className="flex flex-col items-center justify-center border-4 border-green-5">
-                    <motion.img
-                        src={svgFiles[animationIdx]?.src} // Load current SVG file
-                        alt={`Loading animation ${animationIdx + 1}`}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }} // Show active animation
-                        transition={{ duration: 0.5 }} // Smooth fading transition
-                        style={{
-                            height: '100%',
-                            width: '100%'
-                        }}
-                    />
-                
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center border-4 border-green-5">
+      <motion.img
+        src={svgFiles[animationIdx]?.src} // Load current SVG file
+        alt={`Loading animation ${animationIdx + 1}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }} // Show active animation
+        transition={{ duration: 0.5 }} // Smooth fading transition
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+      />
+    </div>
+  );
 }
 
 export default LoadingAnimation;
