212381b8dc018f60f9fa0263dd78b0d2b9d67610
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-04-30 22:08:27 -0400

    handle rejected files

--- src/components/FileUploadModal.tsx
+++ src/components/FileUploadModal.tsx
@@ -10,15 +10,24 @@ import {
 import { Accept, FileRejection, useDropzone } from "react-dropzone";
 import { useRef, useState } from "react";
 
-import { lookup,  } from "mime-types";
+import { extension, lookup } from "mime-types";
 import Image from "next/image";
 import { modals } from "@mantine/modals";
-import { MdCheckCircle, MdClose, MdError, MdOutlineFileUpload } from "react-icons/md";
+import {
+  MdCheckCircle,
+  MdClose,
+  MdError,
+  MdOutlineFileUpload,
+} from "react-icons/md";
 import { Dropzone, FileWithPath } from "@mantine/dropzone";
 import { Button, List, Loader, ScrollArea, Text } from "@mantine/core";
-import useCreateAlbumItem, { CreateAlbumItemRequest } from "@/hooks/albumItems/useCreateAlbumItem";
+import useCreateAlbumItem, {
+  CreateAlbumItemRequest,
+} from "@/hooks/albumItems/useCreateAlbumItem";
 import { useMutationState } from "@tanstack/react-query";
 import { request } from "http";
+import useNotifications from "@/features/notifications/useNotifications";
+import { groupBy } from "@/utils/data/groupBy";
 
 type FileUploadModalProps = {
   children: React.ReactNode;
@@ -46,8 +55,7 @@ function FileComponent({
     >
   >;
 }) {
-  return (<></>
-  );
+  return <></>;
 }
 
 function FinishedUploadView({
@@ -121,68 +129,105 @@ interface UploadAlbumItemsModalProps {
 export function UploadAlbumItemsModal(props: UploadAlbumItemsModalProps) {
   const { albumId } = props;
 
-  const [files, setFiles] = useState<File[]>([]);
+  const [acceptedFiles, setAcceptedFiles] = useState<File[]>([]);
   const requestsRef = useRef<CreateAlbumItemRequest[]>([]);
-  const acceptedFileExtensions = ["jpg", "png"];
+  const acceptedMimeTypes = ["image/jpeg", "image/png", "image/heic", "image/gif", "video/mp4", "video/quicktime"];
   const maxFileSizeMB = 5;
 
   const createAlbumItemMutation = useCreateAlbumItem();
 
+  const notifications = useNotifications();
+
   const onDrop = (files: FileWithPath[]) => {
-    console.log(files);
+    console.log(files)
+    setAcceptedFiles((prev) => [...prev, ...files]);
+  };
+
+  const onReject = (fileRejections: FileRejection[]) => {
+    const rejectionMessage = fileRejections.length === 1 ? '1 file was rejected because it does not meet the specified requirements.' : `${fileRejections.length} files were rejected because they do not meet the specified requirements.`
+    notifications.error(rejectionMessage);
   }
 
   const onUpload = () => {
-    const requests: CreateAlbumItemRequest[] = files.map(file => ({ albumId: "000f726f-2023-46c8-bd0b-4518751b494b", albumItem: file, inReview: true }));
+    const requests: CreateAlbumItemRequest[] = acceptedFiles.map((file) => ({
+      albumId: "000f726f-2023-46c8-bd0b-4518751b494b",
+      albumItem: file,
+      inReview: true,
+    }));
     requestsRef.current = requests;
-    requests.forEach(req => createAlbumItemMutation.mutate(req));
-  }
+    requests.forEach((req) => createAlbumItemMutation.mutate(req));
+  };
 
   return (
     <>
-    <Dropzone classNames={{ inner: 'flex flex-col justify-center items-center border-4 border-dashed border-orange-5 rounded-lg my-2 py-2' }} onDrop={onDrop} >
-      <Text className="text-center">{`Supported file formats: ${acceptedFileExtensions.join(", ")} (Max ${maxFileSizeMB}MB)`}</Text>
-      <MdOutlineFileUpload className="text-neutral-4" size={50} />
-      <Text className="text-center">{"Drag and drop files"}</Text>
-      <Text className="text-center">{"OR"}</Text>
-      <Text className="text-center">{"Select from device"}</Text>
-    </Dropzone>
-    <ScrollArea.Autosize classNames={{
-      root: 'max-h-80 my-2',
-      content: 'flex flex-col justify-center self-center items-center gap-2'
-    }}>
-      {requestsRef.current.map(req => (<FileItem key={req.albumItem.name} req={req} />))}
-    </ScrollArea.Autosize>
-    <div className="flex justify-between w-full my-2 gap-2">
-      <Button color="gray" className="text-black">CANCEL</Button>
-      <Button color="green" onClick={onUpload}>UPLOAD</Button>
-    </div>
+      <Dropzone        
+        classNames={{
+          inner:
+            "flex flex-col justify-center items-center border-4 border-dashed border-orange-5 rounded-lg my-2 p-2",
+        }}
+        accept={acceptedMimeTypes}
+        maxSize={maxFileSizeMB * 1024 * 1024}
+        onDrop={onDrop}
+        onReject={onReject}
+      >
+        <Text className="text-center">{`Supported file types: ${acceptedMimeTypes.map(mimeType => extension(mimeType)).join(", ")} (Max ${maxFileSizeMB}MB)`}</Text>
+        <MdOutlineFileUpload className="text-neutral-4" size={50} />
+        <Text className="text-center">{"Drag and drop files"}</Text>
+        <Text className="text-center">{"OR"}</Text>
+        <Text className="text-center">{"Select from device"}</Text>
+      </Dropzone>
+      <ScrollArea.Autosize
+        classNames={{
+          root: "max-h-80 my-2",
+          content:
+            "flex flex-col justify-center self-center items-center gap-2",
+        }}
+      >
+        {acceptedFiles.map((file) => (
+          <FileItem key={file.name} file={file} />
+        ))}
+      </ScrollArea.Autosize>
+      <div className="flex justify-between w-full my-2 gap-2">
+        <Button color="gray" className="text-black">
+          CANCEL
+        </Button>
+        <Button color="green" onClick={onUpload}>
+          UPLOAD
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
 interface FileItemProps {
-  req: CreateAlbumItemRequest;
+  file: File;
 }
 
 function FileItem(props: FileItemProps) {
-  const { req } = props;
+  const { file } = props;
   const mutation = useMutationState({
     filters: {
-      predicate: (mutation) => {console.log('mut', mutation); return mutation.state.variables === req}
+      predicate: (mutation) => {
+        console.log("mut", mutation);
+        return mutation.state.variables?.albumItem === file;
+      },
     },
-    select: (mutation) => mutation
-  })
-  console.log('bruh', mutation)
+    select: (mutation) => mutation,
+  });
+  console.log("bruh", mutation);
 
-  
-  return <div className="flex bg-blue-0 rounded-sm p-2 justify-between gap-4 w-full" key={file.name}>
-        <Text>{file.name}</Text>
-        {/* <div className="flex gap-2"> */}
-          {/* {state.length > 0 && state[state.length - 1].status === "success" ? <MdCheckCircle className="text-success" size={25} /> : state.length > 0 && state[state.length - 1].status === "error" ? <MdError className="text-error" size={25} /> : <Loader />} */}
-          {/* <MdClose className="text-blue hover:bg-blue-1 rounded-lg cursor-pointer" size={25} /> */}
-        {/* </div> */}
-      </div>
+  return (
+    <div
+      className="flex bg-blue-0 rounded-sm p-2 justify-between gap-4 w-full"
+      key={file.name}
+    >
+      <Text>{file.name}</Text>
+      {/* <div className="flex gap-2"> */}
+      {/* {state.length > 0 && state[state.length - 1].status === "success" ? <MdCheckCircle className="text-success" size={25} /> : state.length > 0 && state[state.length - 1].status === "error" ? <MdError className="text-error" size={25} /> : <Loader />} */}
+      {/* <MdClose className="text-blue hover:bg-blue-1 rounded-lg cursor-pointer" size={25} /> */}
+      {/* </div> */}
+    </div>
+  );
 }
 
 export function FileUploadModal({
@@ -198,7 +243,7 @@ export function FileUploadModal({
     ...new Set(
       acceptedFileExtensions
         .map((fileType: string) => lookup(fileType))
-        .filter((mimeType: string | false) => mimeType)
+        .filter((mimeType: string | false) => mimeType),
     ),
   ] as string[];
 
@@ -214,7 +259,7 @@ export function FileUploadModal({
       setFiles((last) =>
         last
           .concat(accepted.map((f) => ({ file: f, state: "success" })))
-          .concat(rejected.map((f) => ({ file: f.file, state: "failure" })))
+          .concat(rejected.map((f) => ({ file: f.file, state: "failure" }))),
       );
     },
   });
@@ -275,7 +320,7 @@ export function FileUploadModal({
                     await onUpload(
                       files
                         .filter((e) => e.state == "success")
-                        .map((x) => x.file)
+                        .map((x) => x.file),
                     );
                     setUploadState("success");
                   } catch {
@@ -311,10 +356,12 @@ export function FileUploadModal({
   );
 }
 
-export default function openFileUploadModal(props: FileUploadModalProps & { title: string; }) {
+export default function openFileUploadModal(
+  props: FileUploadModalProps & { title: string },
+) {
   const { title, ...rest } = props;
   modals.open({
     title: title ?? "Upload Files",
     children: <FileUploadModal {...rest} />,
-  }); 
+  });
 }
