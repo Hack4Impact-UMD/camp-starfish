364a8c69237b173443fe16089547866e41ab5a7c
Claire Chen <80183874+cc13985@users.noreply.github.com>
2026-05-02 10:12:12 -0700

    feat(albums): improve AlbumPage UI/UX & implement album upload/download functionality (#28)
    - Improved AlbumPage UI/UX
      - Integrated with Mantine & Tanstack Query
      - Replaced file icons with Material Design icon components from the `react-icons` library
      - Options pane includes tag multi-select, link to pending page, upload icon, and download icon
    - Implemented album item uploading
      - Through the `UploadAlbumItemsModal`, users can upload album items
      - Each album item's state is handled independently since partial success when uploading a large amount of album items is possible
      - Album items are automatically set as in review or approved based on the uploading user's role
        - Roles without permissions to upload album items see an error page in the modal body
    - Implemented album downloading
      - Users with permission to download album items can do so by clicking the download icon
      - Download functionality takes in Firestore query filters to only allow users to download the album items that they are authorized to view
    
    Co-authored-by: Nitin Kanchinadam <nitin.kanchinadam@gmail.com>

--- src/app/albums/[albumId]/AlbumPage.tsx
+++ src/app/albums/[albumId]/AlbumPage.tsx
@@ -1,103 +1,213 @@
-import React from "react";
-import plusIcon from "@/assets/icons/plusIcon.svg";
-import filterIcon from "@/assets/icons/filterIcon.svg";
-import ImageCard from "@/components/ImageCard";
+import React, { useState } from "react";
+import {
+  MdOutlineFileUpload,
+  MdOutlineFileDownload,
+  MdPendingActions,
+} from "react-icons/md";
+import Link from "next/link";
+import AlbumItemCard from "@/components/AlbumItemCard";
 import CardGallery from "@/components/CardGallery";
-import { AlbumItem } from "@/types/albums/albumTypes";
-import FileUploadModal from "@/components/FileUploadModal";
-import { uploadFiles } from "@/data/storage/storageClientOperations";
-import { v4 as uuidv4 } from "uuid";
-import Image from "next/image";
-import moment from "moment";
+import Tagging from "@/components/Tagging";
+import { Album, AlbumItem } from "@/types/albums/albumTypes";
+import { FirestoreQueryOptions } from "@/data/firestore/types/queries";
+import { AlbumItemDoc } from "@/data/firestore/types/documents";
+import useAlbum from "@/hooks/albums/useAlbum";
+import useAlbumItemsList from "@/hooks/albumItems/useAlbumItemsList";
+import {
+  ActionIcon,
+  Anchor,
+  Breadcrumbs,
+  Indicator,
+  Menu,
+  Title,
+  Tooltip,
+} from "@mantine/core";
+import { MdSort } from "react-icons/md";
+import LoadingPage from "@/app/loading";
+import ErrorPage from "@/app/error";
+import useDownloadAlbum from "@/features/albums/downloading/useDownloadAlbum";
+import openUploadAlbumItemsModal from "@/components/UploadAlbumItemsModal/UploadAlbumItemsModal";
 
-const AlbumPage: React.FC = () => {
-  const dates = [
-    "Mon, June 17",
-    "Tues, June 18",
-    "Wed, June 19",
-    "Thurs, June 20",
-    "Fri, June 21",
-  ];
+const allTags = [
+  { id: "1", name: "Claire C." },
+  { id: "2", name: "Nitin K." },
+  { id: "3", name: "Ben E." },
+  { id: "4", name: "Maia J." },
+  { id: "5", name: "Harshitha J." },
+  { id: "6", name: "Tej S." },
+  { id: "7", name: "Advik D." },
+  { id: "8", name: "Christine N." },
+  { id: "9", name: "Esha V." },
+  { id: "10", name: "Gelila K." },
+  { id: "11", name: "Joel C." },
+  { id: "12", name: "Nishtha D." },
+  { id: "13", name: "Rivan P." },
+  { id: "14", name: "Riya M." },
+  { id: "15", name: "Saharsh M." },
+];
 
-  const images: AlbumItem[] = [];
-  for (let i = 0; i < 10; i++) {
-    images.push({
-      name: "Image " + i,
-      tagIds: {
-        approved: [],
-        inReview: [],
-      },
-      dateTaken: moment(dates[i % 5]),
-      inReview: false,
-      id: i.toString(),
-      albumId: "iug",
-    });
-  }
+const enum AlbumPageSortOption {
+  NEWEST_TO_OLDEST = "Newest → Oldest",
+  OLDEST_TO_NEWEST = "Oldest → Newest",
+  A_TO_Z = "A → Z",
+  Z_TO_A = "Z → A",
+}
 
-  const albumId = "album-1";
-
-  const title = "Unknown Album";
-  const session = "No Session";
-
-  async function uploadImages(images: File[]) {
-    await uploadFiles(images.map(image => ({
-      file: image,
-      path: `albums/${albumId}/${uuidv4()}`
-    })));
-  }
-
-  return (
-    <div className="w-full min-h-full bg-gray-100">
-      <div className="container mx-auto px-4 py-6">
-        {/* Header */}
-        <div className="flex items-center justify-between">
-          <h1 className="text-4xl font-lato font-bold text-camp-primary">
-            ALBUMS {">>"} {title} {">>"} {session}
-          </h1>
-          <div className="flex items-center gap-4">
-            <input
-              type="text"
-              placeholder="Search Tags..."
-              className="px-10 py-2 text-sm border text-black border-gray-500 rounded-full shadow-sm focus:outline-hidden focus:ring-2 focus:ring-camp-primary"
-            />
-            <Image
-              className="w-[72px] h-[72px] flex-none cursor-pointer"
-              src={filterIcon.src}
-              alt="Filter"
-              width={48}
-              height={48}
-            />
-            <FileUploadModal
-              onUpload={uploadImages}
-              acceptedFileExtensions={[".jpg", ".png"]}
-              maxFileSize={5}
-            >
-              <Image
-                className="w-[72px] h-[72px] flex-none cursor-pointer"
-                src={plusIcon.src}
-                alt="Plus"
-                width={48}
-                height={48}
-              />
-            </FileUploadModal>
-          </div>
-        </div>
-
-        {/* Content */}
-        <CardGallery<AlbumItem>
-          items={images}
-          renderItem={(image: AlbumItem, isSelected: boolean) => (
-            <ImageCard image={image} isSelected={isSelected} />
-          )}
-          groups={{
-            groupLabels: dates,
-            defaultGroupLabel: "Date Unknown",
-            groupFunc: (image: AlbumItem) => image.dateTaken.format("YYYY-MM-DD"),
-          }}
-        />
-      </div>
-    </div>
-  );
+const sortQueryOptions: Record<
+  AlbumPageSortOption,
+  FirestoreQueryOptions<AlbumItemDoc>
+> = {
+  "Newest → Oldest": {
+    orderBy: [{ fieldPath: "dateTaken", direction: "desc" }],
+  },
+  "Oldest → Newest": {
+    orderBy: [{ fieldPath: "dateTaken", direction: "asc" }],
+  },
+  "A → Z": { orderBy: [{ fieldPath: "name", direction: "asc" }] },
+  "Z → A": { orderBy: [{ fieldPath: "name", direction: "desc" }] },
 };
 
-export default AlbumPage;
+interface AlbumPageProps {
+  albumId: string;
+}
+
+export default function AlbumPage(props: AlbumPageProps) {
+  const { albumId } = props;
+
+  const albumQuery = useAlbum(albumId);
+  if (albumQuery.isPending) {
+    return <LoadingPage />;
+  } else if (albumQuery.isError) {
+    return <ErrorPage error={albumQuery.error} />;
+  } else {
+    return <AlbumPageContent album={albumQuery.data} />;
+  }
+}
+
+interface AlbumPageContentProps {
+  album: Album;
+}
+
+export function AlbumPageContent(props: AlbumPageContentProps) {
+  const { album } = props;
+
+  const [selectedTags, setSelectedTags] = useState<(typeof allTags)[0][]>([]);
+  const [sortOption, setSortOption] = useState<AlbumPageSortOption>(
+    AlbumPageSortOption.NEWEST_TO_OLDEST,
+  );
+
+  const albumItemsQuery = useAlbumItemsList(album.id, {
+    ...sortQueryOptions[sortOption],
+    limit: 10,
+    limitToLast: undefined,
+  });
+
+  const downloadAlbumMutation = useDownloadAlbum();
+
+  if (albumItemsQuery.isPending) {
+    return <LoadingPage />;
+  } else if (albumItemsQuery.isError) {
+    return <ErrorPage error={albumItemsQuery.error} />;
+  }
+
+  const albumItems =
+    albumItemsQuery.data.pages.flatMap((page) => page.docs) || [];
+
+  return (
+    <div className="flex flex-col w-6/7 grow mx-auto px-4 py-6 gap-6">
+      <div className="flex items-center justify-between">
+        <Breadcrumbs classNames={{ separator: "text-3xl" }} separator=">>">
+          {[
+            { title: "ALBUMS", href: "/albums" },
+            { title: album.name, href: `#` },
+          ].map((breadcrumb) => (
+            <Anchor href={breadcrumb.href} key={breadcrumb.title}>
+              <Title order={1}>{breadcrumb.title}</Title>
+            </Anchor>
+          ))}
+        </Breadcrumbs>
+        <div className="flex items-center gap-4 shrink-0">
+          {/* Tagging */}
+          <Tagging
+            items={allTags}
+            selectedItems={selectedTags}
+            onSelectionChange={setSelectedTags}
+            getOptionLabel={(tag) => tag.name}
+            getOptionValue={(tag) => tag.id}
+            placeholder="Search Tags..."
+            className="w-64 cursor-pointer"
+          />
+
+          <Menu>
+            <Tooltip label="Sort">
+              <Menu.Target>
+                <ActionIcon variant="transparent">
+                  <MdSort size={50} />
+                </ActionIcon>
+              </Menu.Target>
+            </Tooltip>
+            <Menu.Dropdown>
+              {[
+                AlbumPageSortOption.NEWEST_TO_OLDEST,
+                AlbumPageSortOption.OLDEST_TO_NEWEST,
+                AlbumPageSortOption.A_TO_Z,
+                AlbumPageSortOption.Z_TO_A,
+              ].map((option) => (
+                <Menu.Item key={option} onClick={() => setSortOption(option)}>
+                  {option}
+                </Menu.Item>
+              ))}
+            </Menu.Dropdown>
+          </Menu>
+          <Link href="/albums/pending">
+            <Tooltip label="Pending Items">
+              <Indicator color="error" offset={7}>
+                <ActionIcon variant="outline">
+                  <MdPendingActions size={30} />
+                </ActionIcon>
+              </Indicator>
+            </Tooltip>
+          </Link>
+          <Tooltip label="Upload Items">
+            <ActionIcon color="aqua" onClick={() => openUploadAlbumItemsModal(album.id)}>
+              <MdOutlineFileUpload size={40} />
+            </ActionIcon>
+          </Tooltip>
+          <Tooltip label="Download Album">
+            <ActionIcon
+              color="aqua"
+              onClick={() =>
+                downloadAlbumMutation.mutate({
+                  albumId: album.id,
+                  queryOptions: sortQueryOptions[sortOption],
+                })
+              }
+            >
+              <MdOutlineFileDownload size={40} />
+            </ActionIcon>
+          </Tooltip>
+        </div>
+      </div>
+
+      <CardGallery<AlbumItem>
+        items={albumItems}
+        renderItem={(image: AlbumItem, isSelected: boolean) => (
+          <AlbumItemCard
+            albumId={album.id}
+            albumItemId={image.id}
+            isSelected={isSelected}
+          />
+        )}
+        groups={{
+          groupLabels: [
+            ...new Set(
+              albumItems.map((item) => item.dateTaken.format("YYYY-MM-DD")),
+            ),
+          ],
+          defaultGroupLabel: "Date Unknown",
+          groupFunc: (image: AlbumItem) => image.dateTaken.format("YYYY-MM-DD"),
+        }}
+      />
+    </div>
+  );
+}
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,7 @@
 import { MantineThemeOverride } from "@mantine/core";
 import globalTheme from "./globalTheme";
 import ActionIconThemeExtension from "./components/ActionIconThemeExtension";
+import AnchorThemeExtension from "./components/AnchorThemeExtension";
 import ButtonThemeExtension from "./components/ButtonThemeExtension";
 import DatePickerInputThemeExtension from "./components/DatePickerInputThemeExtension";
 import DatePickerThemeExtension from "./components/DatePickerThemeExtension";
@@ -13,14 +14,17 @@ import TextInputThemeExtension from "./components/TextInputThemeExtension";
 import TextThemeExtension from "./components/TextThemeExtension";
 import TitleThemeExtension from "./components/TitleThemeExtension";
 import TooltipThemeExtension from "./components/TooltipThemeExtension";
+import DropzoneThemeExtension from "./components/DropzoneThemeExtension";
 
 const theme: MantineThemeOverride = {
   ...globalTheme,
   components: {
     ActionIcon: ActionIconThemeExtension,
+    Anchor: AnchorThemeExtension,
     Button: ButtonThemeExtension,
     DatePickerInput: DatePickerInputThemeExtension,
     DatePicker: DatePickerThemeExtension,
+    Dropzone: DropzoneThemeExtension,
     Image: ImageThemeExtension,
     Menu: MenuThemeExtension,
     Modal: ModalThemeExtension,
