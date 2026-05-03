c187c32aba6db7644e9d95fc7e5c0f249a42ed8c
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-03 15:10:45 -0400

    fix wrong import name

--- src/hooks/users/useUserBatch.ts
+++ src/hooks/users/useUserBatch.ts
@@ -1,11 +1,11 @@
-import { getUserBatch } from "@/data/firestore/users";
+import { batchGetUserDocs } from "@/data/firestore/users";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useUserBatch(userIds: number[]) {
   return useQuery({
     queryKey: ['users', 'batch', userIds],
     queryFn: async ({ client }) => {
-      const users = await getUserBatch(userIds);
+      const users = await batchGetUserDocs(userIds);
       users.forEach(user => client.setQueryData(['users', user.id], user));
     },
   })
