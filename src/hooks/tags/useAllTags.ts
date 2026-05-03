aad5f509a65bd87a50d714d6179554e95f4c0550
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-03 19:47:11 -0400

    combine all tag directory pages into a single object

--- src/hooks/tags/useAllTags.ts
+++ src/hooks/tags/useAllTags.ts
@@ -1,8 +1,14 @@
 import { executeTagDirectoryQuery } from "@/data/firestore/tagDirectory";
+import { TagDirectory } from "@/types/albums/albumTypes";
 import { useQuery } from "@tanstack/react-query";
 
-export async function getAllTags() {
-  return await executeTagDirectoryQuery();
+export async function getAllTags(): Promise<TagDirectory> {
+  const pages = await executeTagDirectoryQuery();
+  const combinedTags = pages.reduce((acc, page) => ({ ...acc, ...page }), {});
+  return {
+    ...combinedTags,
+    page: -1
+  }
 }
 
 export default function useAllTags() {
