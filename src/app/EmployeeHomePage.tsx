72f438bb752b4a321bdf59527177b75d3fa1b601
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-10 17:27:46 -0400

    fix references to /programs endpoint

--- src/app/EmployeeHomePage.tsx
+++ src/app/EmployeeHomePage.tsx
@@ -14,21 +14,17 @@ export default function EmployeeHomePage() {
         </h1>
       </div>
 
-      {/* Content Section */}
       <div className="flex flex-wrap justify-center items-center gap-[84px]">
-        {/* Albums */}
         <GalleryCardOne 
           title="ALBUMS" 
           href="/albums" 
-          description="Manage photos from past and ongoing programs"
+          description="Manage photos from past and ongoing sessions"
         />
-        {/* Programs */}
         <GalleryCardOne 
-          title="PROGRAMS" 
-          href="/programs" 
+          title="SESSIONS" 
+          href="/sessions" 
           description="Use the activity scheduler to organize campers and staff"
         />
-        {/* Campers */}
         <GalleryCardOne 
           title="CAMPERS" 
           href="/campers" 
