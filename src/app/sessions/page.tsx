6ee4b0c9f57cda45fa2cb37522aed77d7e89a412
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-11 19:14:48 -0400

    lint

--- src/app/sessions/page.tsx
+++ src/app/sessions/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import SessionsPage from "@/components/SessionsPage";
-import useSessionList from "@/hooks/sessions/useSessionList";
-import LoadingPage from "../loading";
 import RequireAuth from "@/auth/RequireAuth";
 import { useAuth } from "@/auth/useAuth";
-import ErrorPage from "@/app/error";
 
 export default function Page() {
   const { token } = useAuth();
