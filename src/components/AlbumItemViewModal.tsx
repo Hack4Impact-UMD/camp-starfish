7556e8aba0551bcb4de7a7dfd5db666064023bf1
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-14 21:50:59 -0400

    fix component in auth case

--- src/components/AlbumItemViewModal.tsx
+++ src/components/AlbumItemViewModal.tsx
@@ -111,7 +111,7 @@ export function AlbumItemViewModal(props: ImageViewProps) {
         authCases={[
           {
             authFn: () => role === "PARENT",
-            component: <AlbumItemViewModalTagSection albumId={albumId} albumItemId={albumItemId} />,
+            component: <AlbumItemViewModalReportSection albumId={albumId} albumItemId={albumItemId} />,
           },
           {
             authFn: () => !!role && (["ADMIN", "PHOTOGRAPHER", "STAFF"] as Role[]).includes(role),
