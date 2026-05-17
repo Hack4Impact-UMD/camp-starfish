2cabce544acb2b90336f2ac12ffdcb47be5bcbf6
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-17 16:13:35 -0400

    add PhotoReportingModal to AlbumItemViewModal

--- src/components/AlbumItemViewModal/AlbumItemViewModalReportSection.tsx
+++ src/components/AlbumItemViewModal/AlbumItemViewModalReportSection.tsx
@@ -1,14 +1,21 @@
 import { Button, Text } from "@mantine/core";
 import { MdFlag } from "react-icons/md";
+import openPhotoReportingModal from "../PhotoReporting";
 
-export default function AlbumItemViewModalReportSection() {
+interface AlbumItemViewModalReportSectionProps {
+  albumId: string;
+  albumItemId: string;
+}
+
+export default function AlbumItemViewModalReportSection(props: AlbumItemViewModalReportSectionProps) {
+  const { albumId, albumItemId } = props;
   return (
     <div className="w-full bg-white rounded-t-2xl flex justify-center items-center px-lg py-md gap-4">
       <Text>
         Something wrong with this image? Report your issue to the Camp Starfish
         team!
       </Text>
-      <Button aria-label="Report" rightSection={<MdFlag size={20} />}>
+      <Button aria-label="Report" rightSection={<MdFlag size={20} />} onClick={() => openPhotoReportingModal(albumId, albumItemId)}>
         Report
       </Button>
     </div>
