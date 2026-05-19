4667b39128e9e3e4c2c193b07ac39ffcc5d4f7db
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-18 21:01:20 -0400

    update tagging Cloud Functions to use new type

--- functions/src/features/tagging.ts
+++ functions/src/features/tagging.ts
@@ -1,43 +1,53 @@
 import { RootLevelCollection } from "@/data/firestore/types/collections";
-import { Name } from "@/types/users/userTypes";
+import { Name, Role } from "@/types/users/userTypes";
 import { onDocumentCreated, onDocumentUpdated } from "firebase-functions/firestore";
 import { adminDb } from "../config/firebaseAdminConfig";
 import { aggregateUserDirectoryDocs, createUserDirectoryDoc, executeUserDirectoryQuery, updateUserDirectoryDoc } from "../data/firestore/userDirectory";
 import { getFullName } from "@/types/users/userUtils";
-import { FieldValue } from "firebase-admin/firestore";
+import { FieldValue, UpdateData } from "firebase-admin/firestore";
+import { UserDirectoryDoc, UserDoc } from "@/data/firestore/types/documents";
+import { diff } from "@/utils/data/diff";
+import { UserDirectoryItem } from "@/types/albums/albumTypes";
 
 const onUserCreated = onDocumentCreated(`/${RootLevelCollection.USERS}/{userId}`, async (event) => {
   const { userId } = event.params;
-  const name: Name = event.data?.data()?.name;
-  const fullName: string = getFullName(name);
+  const userDoc = event.data?.data() as UserDoc;
+  const directoryData = {
+    [Number(userId)]: {
+      name: userDoc.name,
+      role: userDoc.role
+    }
+  }
+  
   adminDb.runTransaction(async (transaction) => {
     const { docCount } = await aggregateUserDirectoryDocs({
       transaction,
       aggregationQueryOptions: { aggregations: [{ aggregateFieldName: 'docCount', operation: 'count' }] }
     }) as { docCount: number };
     if (docCount === 0) {
-      await createUserDirectoryDoc(0, { [Number(userId)]: fullName }, transaction);
+      await createUserDirectoryDoc(0, directoryData, transaction);
       return;
     }
 
     try {
-      await updateUserDirectoryDoc(docCount - 1, { [userId]: fullName }, transaction);
+      await updateUserDirectoryDoc(docCount - 1, directoryData, transaction);
     } catch {
-      await createUserDirectoryDoc(docCount, { [userId]: fullName }, transaction);
+      await createUserDirectoryDoc(docCount, directoryData, transaction);
     }
   })
 });
 
 const onUserUpdated = onDocumentUpdated(`/${RootLevelCollection.USERS}/{userId}`, async (event) => {
   const { userId } = event.params;
-  const beforeData = event.data?.before.data().name;
   const afterData = event.data?.after.data().name;
 
-  const beforeFullName = getFullName(beforeData);
-  const afterFullName = getFullName(afterData);
-  if (beforeFullName === afterFullName) {
-    return;
-  }
+  const directoryDataUpdates = {
+    [Number(userId)]: {
+      name: afterData.name,
+      role: afterData.role
+    }
+  };
+
   await adminDb.runTransaction(async (transaction) => {
     const docs = await executeUserDirectoryQuery({
       transaction,
@@ -53,16 +63,15 @@ const onUserUpdated = onDocumentUpdated(`/${RootLevelCollection.USERS}/{userId}`
     
     const doc = docs[0];
     try {
-      await updateUserDirectoryDoc(doc.page, { [userId]: afterFullName }, transaction);
+      await updateUserDirectoryDoc(doc.page, directoryDataUpdates, transaction);
     } catch {
       await updateUserDirectoryDoc(doc.page, { [userId]: FieldValue.delete() }, transaction);
       const { docCount } = await aggregateUserDirectoryDocs({
         transaction,
         aggregationQueryOptions: { aggregations: [{ aggregateFieldName: 'docCount', operation: 'count' }] }
       }) as { docCount: number };
-      await createUserDirectoryDoc(docCount, { [userId]: afterFullName }, transaction);
+      await createUserDirectoryDoc(docCount, directoryDataUpdates, transaction);
     }
-    
   })
 });
 
