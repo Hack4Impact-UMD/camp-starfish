47af91ab22cc446bf183a330e07d7ffe82dcdeb5
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-22 21:06:56 -0400

    fix icon padding in TextInput

--- src/styles/components/TextInputThemeExtension.ts
+++ src/styles/components/TextInputThemeExtension.ts
@@ -3,7 +3,7 @@ import { TextInput } from "@mantine/core";
 const TextInputThemeExtension = TextInput.extend({
   classNames: {
     label: 'text-neutral-5 text-xl font-medium',
-    input: "bg-blue-0 rounded-md border-none hover:border-neutral-8 px-3 hover:border-2 py-2 text-sm text-blue-9 placeholder:text-neutral-5",
+    input: "bg-blue-0 rounded-md border-none hover:border-neutral-8 pl-xl hover:border-2 py-2 text-sm text-blue-9 placeholder:text-neutral-5",
   },
 })
 
