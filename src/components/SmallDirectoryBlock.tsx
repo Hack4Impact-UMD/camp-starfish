8c2234f12d1b06c78cec53eb38a18f2b78f5784f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-24 15:14:29 -0400

    add warning for when bunk data fails to load

--- src/components/SmallDirectoryBlock.tsx
+++ src/components/SmallDirectoryBlock.tsx
@@ -10,10 +10,19 @@ import {
   ScrollArea,
   Text,
 } from "@mantine/core";
-import { MdSearch, MdErrorOutline, MdFullscreen } from "react-icons/md";
+import {
+  MdSearch,
+  MdErrorOutline,
+  MdFullscreen,
+  MdWarningAmber,
+} from "react-icons/md";
 import { MdAccountCircle } from "react-icons/md";
 import useUserDirectory from "@/hooks/users/useUserDirectory";
-import { getFullName } from "@/types/users/userUtils";
+import {
+  attendeeRoles,
+  getFullName,
+  getPluralRole,
+} from "@/types/users/userUtils";
 import useBunkList from "@/hooks/bunks/useBunkList";
 import useSession from "@/hooks/sessions/useSession";
 
@@ -66,8 +75,8 @@ export function SmallDirectoryBlock({ sessionId }: SmallDirectoryBlockProps) {
   let smallDirectoryBlockContent = <></>;
   if (userDirectoryQuery.isError || sessionQuery.isError) {
     smallDirectoryBlockContent = (
-      <div className="flex items-center gap-3 p-3 bg-error-0 border border-error-5 rounded-md">
-        <MdErrorOutline size={16} className="text-error" />
+      <div className="flex items-center gap-sm p-sm bg-error-0 border border-error rounded-md">
+        <MdErrorOutline size={32} className="text-error" />
         <Text className="text-error">Unable to load directory</Text>
       </div>
     );
@@ -78,6 +87,12 @@ export function SmallDirectoryBlock({ sessionId }: SmallDirectoryBlockProps) {
   } else {
     smallDirectoryBlockContent = (
       <>
+        {true && (
+          <div className="flex items-center gap-sm p-sm bg-warning-0 border border-warning rounded-md">
+            <MdWarningAmber size={20} className="text-warning" />
+            <Text className="text-warning">Unable to load bunk data</Text>
+          </div>
+        )}
         <TextInput
           placeholder="Search directory..."
           leftSection={<MdSearch size={16} />}
@@ -91,18 +106,22 @@ export function SmallDirectoryBlock({ sessionId }: SmallDirectoryBlockProps) {
           }
         >
           <div className="flex gap-4 mt-4">
-            {attendeeRoles.map(attendeeRole => <Radio value={attendeeRole} label={getPluralRole(attendeeRole)} />)}
+            {attendeeRoles.map((attendeeRole) => (
+              <Radio value={attendeeRole} label={getPluralRole(attendeeRole)} />
+            ))}
           </div>
         </RadioGroup>
         {attendeesToDisplay.length > 0 ? (
           <ScrollArea.Autosize mah={500}>
             {attendeesToDisplay.map((attendee) => (
-              <div className="flex items-center gap-xs py-sm border-b last:border-b-0" key={attendee.id}>
+              <div
+                className="flex items-center gap-xs py-sm border-b last:border-b-0"
+                key={attendee.id}
+              >
                 <MdAccountCircle />
                 <Text className="font-bold">
                   {getFullName(attendee.name)}
-                  {usersToBunk[attendee.id] &&
-                    ` (${usersToBunk[attendee.id]})`}
+                  {usersToBunk[attendee.id] && ` (${usersToBunk[attendee.id]})`}
                 </Text>
               </div>
             ))}
@@ -117,7 +136,7 @@ export function SmallDirectoryBlock({ sessionId }: SmallDirectoryBlockProps) {
   }
 
   return (
-    <div className="flex flex-col h-full border border-black p-4 bg-neutral-2">
+    <div className="flex flex-col h-full border border-black p-4 bg-neutral-2 gap-xs">
       <div className="flex justify-between items-center mb-4">
         <Title order={2}>Directory</Title>
         <Anchor href={`/sessions/${sessionId}/directory`}>
