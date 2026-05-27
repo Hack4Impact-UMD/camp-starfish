7398a8eb053de83a984f98aa096bf7a7df3155be
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-26 21:30:06 -0400

    refactor PendingAlbumItemCard with Mantine components

--- src/components/PendingAlbumItemCard.tsx
+++ src/components/PendingAlbumItemCard.tsx
@@ -1,55 +1,56 @@
-import React, { useState } from "react";
-import checkIcon from "@/assets/icons/checkIcon.svg";
-import crossIcon from "@/assets/icons/crossIcon.svg";
+import React, { JSX, useState } from "react";
 import useAlbumItem from "@/hooks/albumItems/useAlbumItem";
 import useRejectPendingAlbumItems from "@/hooks/albumItems/pendingItems/useRejectPendingAlbumItems";
 import useApprovePendingAlbumItems from "@/hooks/albumItems/pendingItems/useApprovePendingAlbumItems";
 import useAlbumItemBlob from "@/hooks/albumItems/useAlbumItemBlob";
+import { BackgroundImage, Card, Image, Overlay } from "@mantine/core";
+import classNames from "classnames";
+import useAlbumItemSrc from "@/hooks/albumItems/useAlbumItemSrc";
+import LoadingAnimation from "./LoadingAnimation";
+import { MdError } from "react-icons/md";
 
 interface PendingAlbumItemCardProps {
     albumId: string;
     albumItemId: string;
+    isSelected: boolean;
 }
 
 export default function PendingAlbumItemCard(props: PendingAlbumItemCardProps) {
-    const { albumId, albumItemId } = props;
+    const { albumId, albumItemId, isSelected } = props;
 
     const albumItemQuery = useAlbumItem({ albumId, albumItemId });
-    const albumItemBlobQuery = useAlbumItemBlob(albumId, albumItemId);
+    const albumItemSrcQuery = useAlbumItemSrc(albumId, albumItemId);
 
     const approvePendingAlbumItemsMutation = useApprovePendingAlbumItems();
     const rejectPendingAlbumItemsMutation = useRejectPendingAlbumItems();
 
-    return (
-        <div className="relative group w-full rounded overflow-hidden shadow-md">
-            <img
-                src={src}
-                alt={alt}
-                className="w-full h-auto object-cover"
-            />
+    console.log(albumItemSrcQuery.data)
 
-            <div
-                className={`absolute top-4 right-4 flex gap-2 px-3 py-1 rounded bg-white/80 transition-opacity ${
-                    status === "none" ? "opacity-50 group-hover:opacity-100" : "opacity-100"
-                }`}
+    let cardContent: JSX.Element;
+    switch (albumItemSrcQuery.status) {
+        case "error":
+            cardContent = <MdError className="text-error" size={60} />
+            break;
+        case "pending":
+            cardContent = <LoadingAnimation />
+            break;
+        case "success":
+            cardContent = (
+                <Image src={albumItemSrcQuery.data} alt={albumItemQuery.data?.name ?? albumItemId} width={100} height={100} className="object-contain w-full h-full" unoptimized/>
+            )
+    }
+
+    return (
+            <Card
+              classNames={{
+                root: classNames("rounded-none p-0 aspect-3/2 cursor-pointer group", {
+                  "border-neutral-8": isSelected,
+                  "border-transparent": !isSelected,
+                  "border-4": albumItemSrcQuery.isSuccess,
+                }),
+              }}
             >
-                <button
-                    onClick={handleApprove}
-                    className={`w-6 h-6 rounded-full flex items-center justify-center ${
-                        status === "approved" ? "bg-camp-buttons-success" : "bg-white"
-                    }`}
-                >
-                    <img src={checkIcon.src} alt="Approve" className="w-3 h-3" />
-                </button>
-                <button
-                    onClick={handleReject}
-                    className={`w-6 h-6 rounded-full flex items-center justify-center ${
-                        status === "rejected" ? "bg-camp-primaryScale-p300" : "bg-white"
-                    }`}
-                >
-                    <img src={crossIcon.src} alt="Reject" className="w-3 h-3" />
-                </button>
-            </div>
-        </div>
-    );
+              {cardContent}
+            </Card>
+    )
 };
