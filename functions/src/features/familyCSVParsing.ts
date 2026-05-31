2ba5f7a4bd9335a70d89d206d39be8ce240ea4f0
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-05-31 14:07:55 -0400

    fix comparison bug

--- functions/src/features/familyCSVParsing.ts
+++ functions/src/features/familyCSVParsing.ts
@@ -1,4 +1,4 @@
-import { Role } from "@/types/users/userTypes";
+import { Camper, Parent, Role, UnregisteredCamper, UnregisteredParent } from "@/types/users/userTypes";
 import { HttpsError, onCall } from "firebase-functions/https"
 import { ParseFamilyCSVResponse } from "@/data/storage/parseCampersCSV";
 import { adminDb } from "../config/firebaseAdminConfig";
@@ -6,7 +6,7 @@ import { batchGetUserDocs, createUserDoc, updateUserDoc } from "../data/firestor
 import partition from "@/utils/data/partition";
 import { FieldValue } from "firebase-admin/firestore";
 
-export const handleFamilyCSVUpload = onCall({ memory:}, async (req) => {
+export const handleFamilyCSVUpload = onCall(async (req) => {
   const role: Role | undefined = req.auth?.token.role;
   if (!role || role !== "ADMIN") {
     throw new HttpsError("permission-denied", "User does not have permission to create new users.");
@@ -18,8 +18,8 @@ export const handleFamilyCSVUpload = onCall({ memory:}, async (req) => {
     const allIds: number[] = [...Object.keys(campers), ...Object.keys(parents)].map(id => parseInt(id));
     const existingUsers = await batchGetUserDocs(allIds);
 
-    const { trueGroup: existingCampers, falseGroup: newCampers } = partition(Object.values(campers), (camper) => existingUsers.some(user => user.id === camper.id));
-    const { trueGroup: existingParents, falseGroup: newParents } = partition(Object.values(parents), (parent) => existingUsers.some(user => user.id === parent.id));
+    const { trueGroup: existingCamperUpdates, falseGroup: newCampers } = partition(Object.values(campers), (camper) => existingUsers.some(user => user.id === camper.id));
+    const { trueGroup: existingParentUpdates, falseGroup: newParents } = partition(Object.values(parents), (parent) => existingUsers.some(user => user.id === parent.id));
 
     const promises = [
       newCampers.map((camper) => {
@@ -30,18 +30,18 @@ export const handleFamilyCSVUpload = onCall({ memory:}, async (req) => {
         const { id, ...parentDoc } = parent;
         return createUserDoc(id, { ...parentDoc, role: "PARENT" }, transaction);
       }),
-      existingCampers.map((camper) => {
-        const { id, ...docUpdates } = camper;
-        const existingCamper = existingCampers.find(camper => camper.id === id)!;
+      existingCamperUpdates.map((camperUpdates) => {
+        const { id, ...docUpdates } = camperUpdates;
+        const existingCamper = existingUsers.find(camper => camper.id === id) as Camper | UnregisteredCamper;
         if (existingCamper.name.firstName === docUpdates.name.firstName && existingCamper.name.middleName === docUpdates.name.middleName && existingCamper.name.lastName === docUpdates.name.lastName && docUpdates.parentIds.every(parentId => existingCamper.parentIds.includes(parentId))) return;
         return updateUserDoc(id, {
           name: docUpdates.name,
-          parentIds: FieldValue.arrayUnion(...camper.parentIds),
+          parentIds: FieldValue.arrayUnion(...camperUpdates.parentIds),
         }, transaction);
       }),
-      existingParents.map((parent) => {
-        const { id, ...docUpdates } = parent;
-        const existingParent = existingParents.find(parent => parent.id === id)!;
+      existingParentUpdates.map((parentUpdates) => {
+        const { id, ...docUpdates } = parentUpdates;
+        const existingParent = existingUsers.find(parent => parent.id === id) as Parent | UnregisteredParent;
         if (existingParent.name.firstName === docUpdates.name.firstName && existingParent.name.middleName === docUpdates.name.middleName && existingParent.name.lastName === docUpdates.name.lastName && docUpdates.camperIds.every(camperId => existingParent.camperIds.includes(camperId))) return;
         return updateUserDoc(id, {
           name: docUpdates.name,
