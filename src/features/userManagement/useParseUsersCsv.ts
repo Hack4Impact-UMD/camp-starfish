162b8358c3884e3067b659c1afb6ebba816760a6
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-03 21:52:16 -0400

    delete composite hooks

--- src/features/userManagement/useParseUsersCsv.ts
+++
@@ -1,9 +0,0 @@
-import useParseFamilyCsv from "./useParseFamilyCsv";
-import useParseEmployeeCsv from "./useParseEmployeeCsv";
-import { UsersCsvType } from "./types";
-
-export default function useProcessUsersCsv(usersCsvType: UsersCsvType) {
-  const parseFamilyCsvMutation = useParseFamilyCsv();
-  const parseEmployeeCsvMutation = useParseEmployeeCsv();
-  return usersCsvType === "FAMILY" ? parseFamilyCsvMutation : parseEmployeeCsvMutation
-}
--- src/features/userManagement/useProcessUsersCsv.ts
+++
@@ -1,9 +0,0 @@
-import useProcessFamilyCSV from "./useProcessFamilyCSV";
-import useProcessEmployeeCSV from "./useProcessEmployeeCSV";
-import { UsersCsvType } from "./types";
-
-export default function useProcessUsersCsv(usersCsvType: UsersCsvType) {
-  const processFamilyCSVMutation = useProcessFamilyCSV();
-  const processEmployeeCSVMutation = useProcessEmployeeCSV();
-  return usersCsvType === "FAMILY" ? processFamilyCSVMutation : processEmployeeCSVMutation
-}
