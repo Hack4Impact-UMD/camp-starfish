12fda924e53d823a10a8548054cc144f329a8a0f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-04 21:33:24 -0400

    validate employee csv

--- src/features/userManagement/useParseEmployeeCsv.ts
+++ src/features/userManagement/useParseEmployeeCsv.ts
@@ -1,17 +1,31 @@
 import { useMutation } from "@tanstack/react-query";
-import { parse } from "csv-parse/sync";
+import { Info, parse } from "csv-parse/sync";
 import { ParsedEmployeeCsvData } from "./types";
+import { z } from "zod";
 
 interface ParseEmployeeCsvRequest {
   csvFile: File;
 }
 
-interface EmployeeCSVRecord {
+interface RawEmployeeCSVRecord {
   "First Name": string;
   "Last Name": string;
   PersonID: string;
   "Login/Email": string;
 }
+
+interface RawEmployeeCsvRecordWithInfo {
+  record: RawEmployeeCSVRecord;
+  info: Info;
+}
+
+const EmployeeCsvRecordSchema = z.object({
+  "First Name": z.string().min(1),
+  "Last Name": z.string().min(1),
+  PersonID: z.preprocess(value => value === "" ? NaN : value, z.coerce.number()),
+  "Login/Email": z.string().email(),
+})
+type ParsedEmployeeCsvRecord = z.infer<typeof EmployeeCsvRecordSchema>;
 
 const REQUIRED_COLUMNS = [
   "First Name",
@@ -23,7 +37,7 @@ const REQUIRED_COLUMNS = [
 export async function parseEmployeeCsv(req: ParseEmployeeCsvRequest): Promise<ParsedEmployeeCsvData> {
   const { csvFile } = req;
   const rawText = await csvFile.text();
-  const records = parse(rawText, {
+  const data = parse(rawText, {
     columns: (cols: string[]) => {
       const missingColumns = REQUIRED_COLUMNS.filter(col => !cols.includes(col));
       if (missingColumns.length > 0) {
@@ -31,16 +45,24 @@ export async function parseEmployeeCsv(req: ParseEmployeeCsvRequest): Promise<Pa
       }
       return cols;
     },
+    info: true,
     skip_empty_lines: true
-  }) as EmployeeCSVRecord[];
-  return records.filter(record => !Number.isNaN(parseInt(record.PersonID))).map(record => ({
-    id: parseInt(record.PersonID),
-    name: {
-      firstName: record["First Name"],
-      lastName: record["Last Name"]
-    },
-    email: record["Login/Email"]
-  }));
+  }) as RawEmployeeCsvRecordWithInfo[];
+  return data.map(({ record, info }) => {
+    const validationResult = EmployeeCsvRecordSchema.safeParse(record);
+    if (!validationResult.success) {
+      throw Error(`Invalid record on line ${info.lines}. Please fix before uploading.`);
+    }
+    const parsedRecord: ParsedEmployeeCsvRecord = validationResult.data;
+    return {
+      id: parsedRecord.PersonID,
+      name: {
+        firstName: record["First Name"],
+        lastName: record["Last Name"]
+      },
+      email: record["Login/Email"]
+    }
+  });
 }
 
 export default function useParseEmployeeCsv() {
