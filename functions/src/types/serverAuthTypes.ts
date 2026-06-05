cb0724303c9c30c712bba8c02a6234a559e55bd2
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-05 09:09:58 -0400

    make custom claims partial

--- functions/src/types/serverAuthTypes.ts
+++ functions/src/types/serverAuthTypes.ts
@@ -1,4 +1,4 @@
 import { CustomClaims } from "@/auth/types/clientAuthTypes";
 import { DecodedIdToken } from "firebase-admin/auth";
 
-export type DecodedIdTokenWithCustomClaims = DecodedIdToken & CustomClaims;
+export type DecodedIdTokenWithCustomClaims = DecodedIdToken & Partial<CustomClaims>;
