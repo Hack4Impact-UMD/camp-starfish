87dcd58d83a4ea90265bb02e03c3265cfac911db
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-05 20:24:05 -0400

    write function to get block ids

--- src/types/scheduling/schedulingUtils.ts
+++ src/types/scheduling/schedulingUtils.ts
@@ -4,3 +4,9 @@ import { Activity } from "@/types/scheduling/schedulingTypes";
 export function getActivityName(activity: Activity): string {
   return isBundleActivity(activity) ? `${activity.programAreaId}: ${activity.name}` : activity.name;
 }
+
+// will start returning non-letter characters if num is greater than or equal to 26 - fix later
+const CAPITAL_A_CHAR_CODE = 65;
+export function getBlockIdFromNum(num: number) {
+  return String.fromCharCode(num + CAPITAL_A_CHAR_CODE);
+}
