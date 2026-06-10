a30bbf7770fcab6b724e3377bac91a94e6c05ad2
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-10 18:57:28 -0400

    update auth types

--- src/auth/types/clientAuthTypes.ts
+++ src/auth/types/clientAuthTypes.ts
@@ -1,9 +1,17 @@
 import { ParsedToken } from "firebase/auth";
 import { Role } from "@/types/users/userTypes";
 
-export interface CustomClaims {
-  role: Role;
+export interface NonAdminCustomClaims {
+  role: Exclude<Role, "ADMIN">;
   campminderId: number;
 }
+
+export interface AdminCustomClaims {
+  role: "ADMIN";
+  camperminderId: number;
+  isSuperAdmin: boolean;
+}
+
+export type CustomClaims = NonAdminCustomClaims | AdminCustomClaims;
 
 export type ParsedTokenWithCustomClaims = ParsedToken & CustomClaims;
