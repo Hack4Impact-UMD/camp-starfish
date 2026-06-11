508a95e1a8f99196a570474158c60f51713e3509
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-10 21:41:34 -0400

    cap number of blocks per section at 10

--- src/types/scheduling/schedulingUtils.ts
+++ src/types/scheduling/schedulingUtils.ts
@@ -5,8 +5,8 @@ export function getActivityName(activity: Activity): string {
   return isBundleActivity(activity) ? `${activity.programAreaId}: ${activity.name}` : activity.name;
 }
 
-// will start returning non-letter characters if num is greater than or equal to 26 - fix later
 const CAPITAL_A_CHAR_CODE = 65;
 export function getBlockIdFromNum(num: number) {
+  if (num < 0 || num >= 10) throw Error("Invalid block number");
   return String.fromCharCode(num + CAPITAL_A_CHAR_CODE);
 }
