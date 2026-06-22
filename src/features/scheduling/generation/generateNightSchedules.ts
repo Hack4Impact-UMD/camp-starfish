a0ae531c80e68325e04fcc6c23171c486b4c1a02
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-22 23:01:49 +0530

    determine counselors with tomorrow off

--- src/features/scheduling/generation/generateNightSchedules.ts
+++ src/features/scheduling/generation/generateNightSchedules.ts
@@ -1,17 +1,17 @@
-import { DaysOffSchedule, NightSchedule, Session } from "@/types/sessions/sessionTypes";
+import { Bunk, DaysOffSchedule, NightSchedule, Session } from "@/types/sessions/sessionTypes";
 import { getObjectKeysAsNumbers } from "@/utils/stringUtils";
 
 interface GenerateNightSchedulesRequest {
   session: Session;
   daysOffSchedule: DaysOffSchedule;
-  bunkNums: number[];
+  bunks: Bunk[];
 }
 
 export default function generateNightSchedules(req: GenerateNightSchedulesRequest): NightSchedule[] {
-  const { session, daysOffSchedule, bunkNums } = req;
+  const { session, daysOffSchedule, bunks } = req;
   
-  for (const bunkNum of bunkNums) {
-    generateNightSchedulesForBunk(bunkNum, daysOffSchedule, session)
+  for (const bunk of bunks) {
+    generateNightSchedulesForBunk(bunk, daysOffSchedule, session)
   }
   // generate night schedules by bunk
   // merge into a single night schedule
@@ -19,16 +19,23 @@ export default function generateNightSchedules(req: GenerateNightSchedulesReques
   return [];
 }
 
-function generateNightSchedulesForBunk(bunkNum: number, daysOffSchedule: DaysOffSchedule, session: Session)  {
+function generateNightSchedulesForBunk(bunk: Bunk, daysOffSchedule: DaysOffSchedule, session: Session)  {
   for (let currDate = session.startDate.clone(); currDate.isBefore(session.endDate, "day"); currDate = currDate.clone().add(1, 'day')) {
     const counselorsWithDayOff: number[] = [];
-    
+    const counselorsWithTomorrowOff: number[] = [];
     if (daysOffSchedule.daysOffInSession.some(dayOff => dayOff.isSame(currDate, 'day'))) {
       for (const counselorId of getObjectKeysAsNumbers(daysOffSchedule.daysOffByCounselorId)) {
-        if (daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate, 'day'))) {
+        if (!bunk.counselorIds.includes(counselorId)) {
+          continue;
+        } else if (daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate, 'day'))) {
           counselorsWithDayOff.push(counselorId);
+        } else if (daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate.clone().add(1, 'day')))) {
+          counselorsWithTomorrowOff.push(counselorId);
         }
       }
     }
   }
+
+  // determine who can't be COD
+
 }
