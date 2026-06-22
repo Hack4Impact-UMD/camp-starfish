0cacdee7b3ad53ef3da3d8a903cfbfdd7e4bd9a3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-22 23:58:59 +0530

    process data

--- src/features/scheduling/generation/generateNightSchedules.ts
+++ src/features/scheduling/generation/generateNightSchedules.ts
@@ -1,5 +1,6 @@
-import { Bunk, DaysOffSchedule, NightSchedule, Session } from "@/types/sessions/sessionTypes";
+import { Bunk, DaysOffSchedule, NightSchedule, NightSchedulePosition, Session } from "@/types/sessions/sessionTypes";
 import { getObjectKeysAsNumbers } from "@/utils/stringUtils";
+import moment from "moment";
 
 interface GenerateNightSchedulesRequest {
   session: Session;
@@ -9,17 +10,31 @@ interface GenerateNightSchedulesRequest {
 
 export default function generateNightSchedules(req: GenerateNightSchedulesRequest): NightSchedule[] {
   const { session, daysOffSchedule, bunks } = req;
-  
-  for (const bunk of bunks) {
-    generateNightSchedulesForBunk(bunk, daysOffSchedule, session)
-  }
-  // generate night schedules by bunk
-  // merge into a single night schedule
 
-  return [];
+  const nightSchedules: { [date: string]: NightSchedule; } = {};
+  for (let currDate = session.startDate.clone(); currDate.isBefore(session.endDate, "day"); currDate = currDate.clone().add(1, 'day')) {
+    nightSchedules[currDate.format("YYYY-MM-DD")] = {
+      sessionId: session.id,
+      date: currDate.clone(),
+      bunks: {}
+    };
+  }
+  for (const bunk of bunks) {
+    const bunkSchedules = generateNightSchedulesForBunk(bunk, daysOffSchedule, session);
+    for (const dateStr in bunkSchedules) {
+      nightSchedules[dateStr].bunks[bunk.bunkNum] = bunkSchedules[dateStr];
+    }
+  }
+  return Object.values(nightSchedules);
 }
 
-function generateNightSchedulesForBunk(bunk: Bunk, daysOffSchedule: DaysOffSchedule, session: Session)  {
+function generateNightSchedulesForBunk(bunk: Bunk, daysOffSchedule: DaysOffSchedule, session: Session): { [date: string]: Record<NightSchedulePosition, number[]>; } {
+  // calculate frequencies
+
+  // determine who days off and who can;t be CBD
+  // assign COD & NBD positions
+  // assign rover positions
+
   for (let currDate = session.startDate.clone(); currDate.isBefore(session.endDate, "day"); currDate = currDate.clone().add(1, 'day')) {
     const counselorsWithDayOff: number[] = [];
     const counselorsWithTomorrowOff: number[] = [];
@@ -38,4 +53,5 @@ function generateNightSchedulesForBunk(bunk: Bunk, daysOffSchedule: DaysOffSched
 
   // determine who can't be COD
 
+  return {};
 }
