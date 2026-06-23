2fce656dccfcc9ccd0de909a34dce302cadbaaf7
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-23 23:19:30 +0530

    fix bug in determining when counselors have days off

--- src/features/scheduling/generation/generateNightSchedules.ts
+++ src/features/scheduling/generation/generateNightSchedules.ts
@@ -77,17 +77,18 @@ function generateNightSchedulesForBunk(bunk: Bunk, daysOffSchedule: DaysOffSched
   for (let currDate = session.startDate.clone(); currDate.isBefore(session.endDate, "day"); currDate = currDate.clone().add(1, 'day')) {
     const counselorsWithDayOff: number[] = [];
     const counselorsWithTomorrowOff: number[] = [];
-    if (daysOffSchedule.daysOffInSession.some(dayOff => dayOff.isSame(currDate, 'day'))) {
-      for (const counselorId of getObjectKeysAsNumbers(daysOffSchedule.daysOffByCounselorId)) {
-        if (!bunk.counselorIds.includes(counselorId)) {
-          continue;
-        }
-        if (daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate, 'day'))) {
-          counselorsWithDayOff.push(counselorId);
-        }
-        if (daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate.clone().add(1, 'day')))) {
-          counselorsWithTomorrowOff.push(counselorId);
-        }
+
+    const isCurrDateADayOff = daysOffSchedule.daysOffInSession.some(dayOff => dayOff.isSame(currDate, 'day'));
+    const isTomorrowADayOff = daysOffSchedule.daysOffInSession.some(dayOff => dayOff.isSame(currDate.clone().add(1, 'day')));
+    for (const counselorId of getObjectKeysAsNumbers(daysOffSchedule.daysOffByCounselorId)) {
+      if (!bunk.counselorIds.includes(counselorId)) {
+        continue;
+      }
+      if (isCurrDateADayOff && daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate, 'day'))) {
+        counselorsWithDayOff.push(counselorId);
+      }
+      if (isTomorrowADayOff && daysOffSchedule.daysOffByCounselorId[counselorId].some(day => day.isSame(currDate.clone().add(1, 'day')))) {
+        counselorsWithTomorrowOff.push(counselorId);
       }
     }
 
