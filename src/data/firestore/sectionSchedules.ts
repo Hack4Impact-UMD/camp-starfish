4d8d46f48b2864daa8150e4bff781e7447e84393
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-27 13:11:59 +0530

    create sectionSchedules Firestore wrappers

--- src/data/firestore/sectionSchedules.ts
+++ src/data/firestore/sectionSchedules.ts
@@ -1,9 +1,10 @@
 import { SectionSchedule } from "@/types/scheduling/schedulingTypes";
-import { SectionScheduleDoc } from "./types/documents";
-import { doc, DocumentReference, DocumentSnapshot, QueryDocumentSnapshot, Transaction } from "firebase/firestore";
-import { getDoc } from "./firestoreClientOperations";
+import { SectionDoc, SectionScheduleDoc } from "./types/documents";
+import { collection, CollectionReference, doc, DocumentReference, DocumentSnapshot, QueryDocumentSnapshot, Transaction, UpdateData, WithFieldValue, WriteBatch } from "firebase/firestore";
+import { deleteDoc, executeQuery, getDoc, setDoc, updateDoc } from "./firestoreClientOperations";
 import { db } from "@/config/firebase";
 import { RootLevelCollection, SectionsSubcollection, SessionsSubcollection } from "./types/collections";
+import { FirestoreQueryOptions } from "./types/queries";
 
 function fromFirestore(snapshot: DocumentSnapshot<SectionScheduleDoc, SectionScheduleDoc> | QueryDocumentSnapshot<SectionScheduleDoc, SectionScheduleDoc>): SectionSchedule {
   if (!snapshot.exists()) { throw Error("Document not found"); };
@@ -14,7 +15,32 @@ function fromFirestore(snapshot: DocumentSnapshot<SectionScheduleDoc, SectionSch
   }
 }
 
-export async function getSectionSchedule(sessionId: string, sectionId: string, transaction?: Transaction): Promise<SectionSchedule> {
-  const snapshot = await getDoc<SectionScheduleDoc>(doc(db, RootLevelCollection.SESSIONS, sessionId, SessionsSubcollection.SECTIONS, sectionId, SectionsSubcollection.SCHEDULE, SectionsSubcollection.SCHEDULE) as DocumentReference<SectionScheduleDoc, SectionScheduleDoc>, transaction);
+function getSectionScheduleDocRef(sessionId: string, sectionId: string): DocumentReference<SectionScheduleDoc, SectionScheduleDoc> {
+  return doc(db, RootLevelCollection.SESSIONS, sessionId, SessionsSubcollection.SECTIONS, sectionId, SectionsSubcollection.SCHEDULE, SectionsSubcollection.SCHEDULE) as DocumentReference<SectionScheduleDoc, SectionScheduleDoc>;
+}
+
+function getSectionScheduleCollectionRef(sessionId: string, sectionId: string): CollectionReference<SectionScheduleDoc, SectionScheduleDoc> {
+  return collection(db, RootLevelCollection.SESSIONS, sessionId, SessionsSubcollection.SECTIONS, sectionId, SectionsSubcollection.SCHEDULE) as CollectionReference<SectionScheduleDoc, SectionScheduleDoc>;
+}
+
+export async function getSectionScheduleDoc(sessionId: string, sectionId: string, transaction?: Transaction): Promise<SectionSchedule> {
+  const snapshot = await getDoc<SectionScheduleDoc>(getSectionScheduleDocRef(sessionId, sectionId), transaction);
   return fromFirestore(snapshot);
+}
+
+export async function listSectionScheduleDocs(sessionId: string, sectionId: string, firestoreQueryOptions: FirestoreQueryOptions<SectionScheduleDoc> = {}): Promise<SectionSchedule[]> {
+  const snapshots = await executeQuery<SectionScheduleDoc>(getSectionScheduleCollectionRef(sessionId, sectionId), firestoreQueryOptions);
+  return snapshots.map(fromFirestore);
+}
+
+export async function setSectionScheduleDoc(sessionId: string, sectionId: string, sectionSchedule: WithFieldValue<SectionScheduleDoc>, instance?: Transaction | WriteBatch): Promise<void> {
+  await setDoc<SectionScheduleDoc>(getSectionScheduleDocRef(sessionId, sectionId), sectionSchedule, { instance });
+}
+
+export async function updateSectionScheduleDoc(sessionId: string, sectionId: string, updates: UpdateData<SectionScheduleDoc>, instance?: Transaction | WriteBatch): Promise<void> {
+  await updateDoc<SectionScheduleDoc>(getSectionScheduleDocRef(sessionId, sectionId), updates, instance);
+}
+
+export async function deleteSectionScheduleDoc(sessionId: string, sectionId: string, instance?: Transaction | WriteBatch): Promise<void> {
+  await deleteDoc<SectionScheduleDoc>(getSectionScheduleDocRef(sessionId, sectionId), instance);
 }
