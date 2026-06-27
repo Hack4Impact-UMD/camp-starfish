176d3db920b788282406311c8bd57c5d08ec40be
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-27 17:15:13 +0530

    create section schedule doc for scheduling sections

--- src/hooks/sections/useCreateSection.ts
+++ src/hooks/sections/useCreateSection.ts
@@ -1,7 +1,11 @@
+import { db } from "@/config/firebase";
 import { createSectionDoc } from "@/data/firestore/sections";
+import { setSectionScheduleDoc } from "@/data/firestore/sectionSchedules";
+import { SectionScheduleDoc } from "@/data/firestore/types/documents";
+import { getBlockIdFromNum } from "@/types/scheduling/schedulingUtils";
 import { SectionType } from "@/types/sessions/sessionTypes";
 import { useMutation } from "@tanstack/react-query";
-import { Timestamp } from "firebase/firestore";
+import { runTransaction, Timestamp, Transaction } from "firebase/firestore";
 import { Moment } from "moment";
 
 interface CreateSectionRequest {
@@ -12,14 +16,40 @@ interface CreateSectionRequest {
   type: SectionType;
 }
 
+const DEFAULT_NUMBER_BLOCKS = 5;
+
 async function createSection(req: CreateSectionRequest) {
   const { sessionId, ...rest } = req;
-  await createSectionDoc(sessionId, {
-    name: rest.name,
-    startDate: Timestamp.fromDate(rest.startDate.clone().startOf('day').toDate()),
-    endDate: Timestamp.fromDate(rest.endDate.clone().endOf('day').toDate()),
+  if (rest.type === "COMMON") {
+    await createSectionDoc(sessionId, {
+      name: rest.name,
+      startDate: Timestamp.fromDate(rest.startDate.clone().startOf('day').toDate()),
+      endDate: Timestamp.fromDate(rest.endDate.clone().endOf('day').toDate()),
+      type: rest.type,
+    });
+    return;
+  }
+
+  const sectionScheduleDoc: SectionScheduleDoc = {
     type: rest.type,
-    publishedAt: null,
+    blocks: {},
+    alternatePeriodsOff: {}
+  };
+  for (let i = 0; i < DEFAULT_NUMBER_BLOCKS; i++) {
+    sectionScheduleDoc.blocks[getBlockIdFromNum(i)] = {
+      activities: [],
+      periodsOff: []
+    }
+  }
+  await runTransaction(db, async (transaction: Transaction) => {
+    const sectionId = await createSectionDoc(sessionId, {
+      name: rest.name,
+      startDate: Timestamp.fromDate(rest.startDate.clone().startOf('day').toDate()),
+      endDate: Timestamp.fromDate(rest.endDate.clone().endOf('day').toDate()),
+      type: rest.type,
+      publishedAt: null,
+    }, transaction);
+    await setSectionScheduleDoc(sessionId, sectionId, sectionScheduleDoc);
   })
 }
 
