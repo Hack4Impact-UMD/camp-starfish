221a3d6d7dbd86387837c26e521454f6e28b08fa
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-06-27 17:33:04 +0530

    handle section type changes

--- src/hooks/sections/useUpdateSection.ts
+++ src/hooks/sections/useUpdateSection.ts
@@ -1,7 +1,11 @@
+import { db } from "@/config/firebase";
 import { updateSectionDoc } from "@/data/firestore/sections";
+import { deleteSectionScheduleDoc, setSectionScheduleDoc } from "@/data/firestore/sectionSchedules";
+import { SchedulingSectionDoc, SectionDoc } from "@/data/firestore/types/documents";
+import { DEFAULT_NUMBER_BLOCKS, getEmptySectionScheduleDoc } from "@/types/scheduling/schedulingUtils";
 import { SectionType } from "@/types/sessions/sessionTypes";
 import { useMutation } from "@tanstack/react-query";
-import { deleteField, Timestamp } from "firebase/firestore";
+import { deleteField, runTransaction, Timestamp, Transaction, UpdateData } from "firebase/firestore";
 import { Moment } from "moment";
 
 interface UpdateSectionRequest {
@@ -17,23 +21,35 @@ async function updateSection(req: UpdateSectionRequest) {
   const { sessionId, sectionId, ...updates } = req;
   switch (updates.type) {
     case "COMMON":
-      await updateSectionDoc(sessionId, sectionId, {
-        name: updates.name,
-        startDate: updates.startDate ? Timestamp.fromDate(updates.startDate.clone().startOf('day').toDate()) : undefined,
-        endDate: updates.endDate ? Timestamp.fromDate(updates.endDate.clone().endOf('day').toDate()) : undefined,
-        type: "COMMON",
-        publishedAt: deleteField()
+      await runTransaction(db, async (transaction: Transaction) => {
+        await Promise.all([
+          updateSectionDoc(sessionId, sectionId, {
+            name: updates.name,
+            startDate: updates.startDate ? Timestamp.fromDate(updates.startDate.clone().startOf('day').toDate()) : undefined,
+            endDate: updates.endDate ? Timestamp.fromDate(updates.endDate.clone().endOf('day').toDate()) : undefined,
+            type: "COMMON",
+            publishedAt: deleteField()
+          }, transaction),
+          deleteSectionScheduleDoc(sessionId, sectionId, transaction)
+        ]);
       });
       break;
     case "BUNDLE":
     case "BUNK-JAMBO":
     case "NON-BUNK-JAMBO":
-      await updateSectionDoc(sessionId, sectionId, {
+      const sectionUpdates: UpdateData<SchedulingSectionDoc> = {
         name: updates.name,
         startDate: updates.startDate ? Timestamp.fromDate(updates.startDate.clone().startOf('day').toDate()) : undefined,
         endDate: updates.endDate ? Timestamp.fromDate(updates.endDate.clone().endOf('day').toDate()) : undefined,
         type: updates.type,
         publishedAt: null
+      }
+      const sectionScheduleDoc = getEmptySectionScheduleDoc(updates.type, DEFAULT_NUMBER_BLOCKS);
+      await runTransaction(db, async (transaction: Transaction) => {
+        await Promise.all([
+          updateSectionDoc(sessionId, sectionId, sectionUpdates, transaction),
+          setSectionScheduleDoc(sessionId, sectionId, sectionScheduleDoc, transaction)
+        ])
       });
       break;
     default:
