21137f8f5d841701b443a79076b8eb37249fb200
Advik Duvvuri <123396162+aduvvuri007@users.noreply.github.com>
2026-06-28 10:09:40 -0400

    feat(scheduling): implement bundle scheduling algorithm (#34)
    - Implemented bundle scheduling algorithm
      - First, assign program counselors to their respective program areas
      - Next, assign all campers that need a swim activity to a swim activity
      - Next, assign all OCP campers to an OCP chat activity
      - Next, assign all campers to their preferred activities if possible, preferring older campers
      - Next, assign all counselors to their periods off
      - Next, ensure that each activity has at least 1 admin counselor supervising it
      - Finally, assign all remaining counselors to activities
      - Throughout scheduling, maintain as close to an even split of people between activities as possible to ensure a rough 1:1 split
      - Throughout scheduling, check for conflicts between attendees
    
    ---------
    
    Co-authored-by: Esha Vigneswaran <eshavigneswaran@gmail.com>
    Co-authored-by: dwang8180 <dwang818@terpmail.umd.edu>
    Co-authored-by: Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
    Co-authored-by: dewang47 <dephs28@gmail.com>
    Co-authored-by: Nitin Kanchinadam <53796958+nkanchinadam@users.noreply.github.com>

---
+++ src/features/scheduling/generation/generateBundleSchedule.ts
@@ -0,0 +1,191 @@
+import { StaffAttendee, CamperAttendee, AdminAttendee, Attendee, DaysOffSchedule, Section } from "@/types/sessions/sessionTypes";
+import { BundleSectionSchedule, SectionActivityPreferences } from "@/types/scheduling/schedulingTypes";
+import shuffle from "@/utils/data/shuffle";
+import { canBeAssignedToIndividualActivityAssignments, getYesYesListGroups } from "./schedulingUtils";
+import { getBlockIdFromNum } from "@/types/scheduling/schedulingUtils";
+import partition from "@/utils/data/partition";
+import { groupBy } from "@/utils/data/groupBy";
+import { isDayInRange } from "@/utils/timeUtils";
+
+interface GenerateBundleScheduleRequest {
+  attendees: Attendee[];
+  camperActivityPreferences: SectionActivityPreferences;
+  currentSchedule: BundleSectionSchedule;
+  daysOffSchedule: DaysOffSchedule;
+  section: Section;
+  isFirstBundleOfSession: boolean;
+}
+
+export default function generateBundleSchedule(req: GenerateBundleScheduleRequest): BundleSectionSchedule {
+  const { attendees, camperActivityPreferences, currentSchedule, daysOffSchedule, section, isFirstBundleOfSession } = req;
+
+  const campers: CamperAttendee[] = [];
+  const staff: StaffAttendee[] = [];
+  const admins: AdminAttendee[] = [];
+  for (const attendee of attendees) {
+    switch (attendee.role) {
+      case "CAMPER":
+        campers.push(attendee);
+        break;
+      case "STAFF":
+        staff.push(attendee);
+        break;
+      case "ADMIN":
+        admins.push(attendee);
+        break;
+      default: throw Error("Unknown attendee role");
+    }
+  }
+
+  const newSchedule: BundleSectionSchedule = {
+    sessionId: currentSchedule.sessionId,
+    sectionId: currentSchedule.sectionId,
+    type: "BUNDLE",
+    blocks: Object.entries(currentSchedule.blocks).reduce((prev, [blockId, block]) => {
+      prev[blockId] = {
+        activities: block.activities.map(activity => ({
+          ...activity,
+          camperIds: [],
+          staffIds: [],
+          adminIds: [],
+        })),
+        periodsOff: []
+      };
+      return prev;
+    }, {} as BundleSectionSchedule["blocks"]),
+    alternatePeriodsOff: Object.keys(currentSchedule.alternatePeriodsOff).reduce((prev, periodId) => {
+      prev[periodId] = [];
+      return prev;
+    }, {} as BundleSectionSchedule["alternatePeriodsOff"]),
+  }
+
+  const programCounselors = staff.filter(staffer => staffer.programCounselorFor !== undefined);
+  const programCounselorsByProgramArea = groupBy(programCounselors, programCounselor => programCounselor.programCounselorFor!);
+  for (const block of Object.values(newSchedule.blocks)) {
+    for (const activity of block.activities) {
+      const eligibleProgramCounselors = programCounselorsByProgramArea[activity.programAreaId].filter(programCounselor => daysOffSchedule.daysOffByCounselorId[programCounselor.attendeeId].every(dayOff => !isDayInRange(dayOff, [section.startDate, section.endDate])));
+      if (!eligibleProgramCounselors) {
+        continue;
+      }
+      activity.adminIds.push(...eligibleProgramCounselors.map(counselor => counselor.attendeeId));
+    }
+  }
+
+  const { trueGroup: navCampers, falseGroup: ocpCampers } = partition(campers, camper => camper.ageGroup === "NAV");
+
+  const navSwimActivities = Object.values(newSchedule.blocks).map((block) => block.activities.find(activity => activity.programAreaId === "WF" && activity.ageGroup === "NAV")).filter(activity => !!activity);
+  if (navSwimActivities.length !== 0) {
+    const maxNavCampersPerSwimActivity = Math.ceil(navCampers.length / navSwimActivities.length);
+    for (const camper of shuffle(navCampers)) {
+      let eligibleSwimActivities = navSwimActivities.filter(swimActivity => swimActivity.camperIds.length < maxNavCampersPerSwimActivity && canBeAssignedToIndividualActivityAssignments(camper, swimActivity));
+      if (eligibleSwimActivities.length === 0) {
+        eligibleSwimActivities = navSwimActivities.filter(swimActivity => canBeAssignedToIndividualActivityAssignments(camper, swimActivity));
+        if (eligibleSwimActivities.length === 0) {
+          eligibleSwimActivities = navSwimActivities;
+        }
+      }
+      const chosenSwimActivity = shuffle(eligibleSwimActivities)[0];
+      chosenSwimActivity.camperIds.push(camper.attendeeId);
+    }
+  }
+
+
+  const ocpSwimActivities = Object.values(newSchedule.blocks).map((block) => block.activities.find(activity => activity.programAreaId === "WF" && activity.ageGroup === "OCP")).filter(activity => !!activity);
+  if (ocpSwimActivities.length !== 0) {
+    const ocpCampersNeedingSwimActivities = ocpCampers.filter(camper => isFirstBundleOfSession || (camper.level >= 4 && camper.isOptedOutFromSwim));
+    const maxOcpCampersPerSwimActivity = Math.ceil(ocpCampersNeedingSwimActivities.length / navSwimActivities.length);
+    for (const camper of shuffle(ocpCampersNeedingSwimActivities)) {
+      let eligibleSwimActivities = ocpSwimActivities.filter(swimActivity => swimActivity.camperIds.length < maxOcpCampersPerSwimActivity && canBeAssignedToIndividualActivityAssignments(camper, swimActivity));
+      if (eligibleSwimActivities.length === 0) {
+        eligibleSwimActivities = ocpSwimActivities.filter(swimActivity => canBeAssignedToIndividualActivityAssignments(camper, swimActivity));
+        if (eligibleSwimActivities.length === 0) {
+          eligibleSwimActivities = ocpSwimActivities;
+        }
+      }
+      const chosenSwimActivity = shuffle(eligibleSwimActivities)[0];
+      chosenSwimActivity.camperIds.push(camper.attendeeId);
+    }
+  }
+
+
+  const ocpChatActivities = Object.values(newSchedule.blocks).map((block) => block.activities.find(activity => activity.programAreaId === "OCP")).filter(activity => !!activity);
+  if (ocpChatActivities.length !== 0) {
+    const maxOcpCampersPerChatActivity = Math.ceil(ocpCampers.length / ocpChatActivities.length);
+    for (const camper of shuffle(ocpCampers)) {
+      let eligibleOcpChatActivities = ocpChatActivities.filter(ocpChatActivity => ocpChatActivity.camperIds.length < maxOcpCampersPerChatActivity && canBeAssignedToIndividualActivityAssignments(camper, ocpChatActivity));
+      if (eligibleOcpChatActivities.length === 0) {
+        eligibleOcpChatActivities = ocpChatActivities.filter(ocpChatActivity => canBeAssignedToIndividualActivityAssignments(camper, ocpChatActivity));
+        if (eligibleOcpChatActivities.length === 0) {
+          eligibleOcpChatActivities = ocpChatActivities;
+        }
+      }
+      const chosenOcpChatActivity = shuffle(eligibleOcpChatActivities)[0];
+      chosenOcpChatActivity.camperIds.push(camper.attendeeId);
+    }
+  }
+
+  for (const [blockId, block] of Object.entries(newSchedule.blocks)) {
+    const sortedCampers = shuffle(campers.filter(camper => block.activities.every(activity => !activity.camperIds.includes(camper.attendeeId))).sort((a, b) => b.snapshot.dateOfBirth.diff(a.snapshot.dateOfBirth, "years")));
+    const maxCampersPerActivity = Math.ceil(sortedCampers.length / block.activities.length);
+    for (const camper of sortedCampers) {
+      const camperPrefs = camperActivityPreferences.blocks[blockId][camper.attendeeId];
+      let eligibleActivities = block.activities.filter((activity) => activity.camperIds.length < maxCampersPerActivity && canBeAssignedToIndividualActivityAssignments(camper, activity));
+      if (eligibleActivities.length === 0) {
+        eligibleActivities = block.activities;
+      }
+      const chosenActivity = eligibleActivities.sort((a, b) => camperPrefs[a.programAreaId] - camperPrefs[b.programAreaId])[0];
+      chosenActivity.camperIds.push(camper.attendeeId);
+    }
+  }
+
+  const yesyesListGroups = getYesYesListGroups([...shuffle(admins), ...shuffle(staff)]);
+  const numBlocks = Object.keys(newSchedule.blocks).length;
+  let currBlockNum = 0;
+  for (const yesyesListGroup of yesyesListGroups) {
+    const blockId = getBlockIdFromNum(currBlockNum);
+    if (yesyesListGroup.some(counselorId => newSchedule.blocks[blockId].activities.some(activity => activity.staffIds.includes(counselorId)))) {
+      const alternatePeriodsOff = Object.keys(newSchedule.alternatePeriodsOff);
+      const chosenAlternatePeriodOff = shuffle(alternatePeriodsOff)[0];
+      newSchedule.alternatePeriodsOff[chosenAlternatePeriodOff].push(...yesyesListGroup);
+    } else {
+      newSchedule.blocks[blockId].periodsOff.push(...yesyesListGroup);
+    }
+    currBlockNum = (currBlockNum + 1) % numBlocks;
+  }
+
+  for (const block of Object.values(newSchedule.blocks)) {
+    const adminsToAssign = shuffle(admins.filter((admin) => !block.periodsOff.includes(admin.attendeeId)));
+    const staffToAssign = shuffle(staff.filter((staffMember) => !block.periodsOff.includes(staffMember.attendeeId) && block.activities.every(activity => !activity.staffIds.includes(staffMember.attendeeId))));
+
+    const maxCounselorsPerActivity = Math.ceil((adminsToAssign.length + staffToAssign.length) / block.activities.length);
+
+    const numAdminsAssigned = Math.min(admins.length, block.activities.length);
+    const shuffledActivities = shuffle(block.activities);
+    for (let i = 0; i < numAdminsAssigned; i++) {
+      shuffledActivities[i].adminIds.push(adminsToAssign[i].attendeeId);
+    }
+
+    if (numAdminsAssigned !== adminsToAssign.length) {
+      const remainingAdminsToAssign = adminsToAssign.slice(numAdminsAssigned);
+      for (const admin of remainingAdminsToAssign) {
+        let eligibleActivities = block.activities.filter((activity) => activity.adminIds.length + activity.staffIds.length < maxCounselorsPerActivity && canBeAssignedToIndividualActivityAssignments(admin, activity));
+        if (eligibleActivities.length === 0) {
+          eligibleActivities = block.activities;
+        }
+        const chosenActivity = shuffle(eligibleActivities)[0];
+        chosenActivity.adminIds.push(admin.attendeeId);
+      }
+    }
+
+    for (const staffMember of staffToAssign) {
+      let eligibleActivities = block.activities.filter((activity) => activity.adminIds.length + activity.staffIds.length < maxCounselorsPerActivity && canBeAssignedToIndividualActivityAssignments(staffMember, activity));
+      if (eligibleActivities.length === 0) {
+        eligibleActivities = block.activities;
+      }
+      const chosenActivity = shuffle(eligibleActivities)[0];
+      chosenActivity.staffIds.push(staffMember.attendeeId);
+    }
+  }
+
+  return newSchedule;
+}
