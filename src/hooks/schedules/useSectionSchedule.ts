e2f051b73ecfc0772a08b2375e27c0a280527862
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-04 19:49:23 +0530

    create hooks for section schedules & activity preferences

--- src/hooks/schedules/useSectionSchedule.ts
+++ src/hooks/schedules/useSectionSchedule.ts
@@ -1,9 +1,13 @@
 import { getSectionScheduleDoc } from "@/data/firestore/sectionSchedules";
-import { skipToken, useQuery } from "@tanstack/react-query";
+import { queryOptions, skipToken, useQuery } from "@tanstack/react-query";
 
-export default function useSectionSchedule(sessionId: string | undefined, sectionId: string | undefined, enabled: boolean = true) {
-  return useQuery({
+export function getUseSectionScheduleOptions(sessionId: string | undefined, sectionId: string | undefined, enabled: boolean = true) {
+  return queryOptions({
     queryKey: ['sessions', sessionId, 'sections', sectionId, 'schedule'],
     queryFn: sessionId && sectionId && enabled ? (() => getSectionScheduleDoc(sessionId, sectionId)) : skipToken
   });
+}
+
+export default function useSectionSchedule(sessionId: string | undefined, sectionId: string | undefined, enabled: boolean = true) {
+  return useQuery(getUseSectionScheduleOptions(sessionId, sectionId, enabled));
 }
