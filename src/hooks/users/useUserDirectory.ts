afbe55d69d72d02ef768adfb9dec2028d0d2b797
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-07 08:47:14 +0530

    rename function

--- src/hooks/users/useUserDirectory.ts
+++ src/hooks/users/useUserDirectory.ts
@@ -2,7 +2,7 @@ import { executeUserDirectoryQuery } from "@/data/firestore/userDirectory";
 import { UserDirectory } from "@/types/albums/albumTypes";
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
-export async function getUserDirectory(): Promise<Omit<UserDirectory, 'page'>> {
+export async function getFullUserDirectory(): Promise<Omit<UserDirectory, 'page'>> {
   const pages = await executeUserDirectoryQuery();
   const fullDirectory = pages.reduce((acc, page) => ({ ...acc, ...page }), {});
   // @ts-expect-error - Typescript doesn't recognize arbitrary keys
@@ -10,10 +10,10 @@ export async function getUserDirectory(): Promise<Omit<UserDirectory, 'page'>> {
   return fullDirectory;
 }
 
-export default function useUserDirectory(tanstackQueryOptions: Omit<UseQueryOptions<Awaited<ReturnType<typeof getUserDirectory>>>, "queryKey" | "queryFn">) {
+export default function useUserDirectory(tanstackQueryOptions: Omit<UseQueryOptions<Awaited<ReturnType<typeof getFullUserDirectory>>>, "queryKey" | "queryFn">) {
   return useQuery({
     queryKey: ['user-directory'],
-    queryFn: getUserDirectory,
+    queryFn: getFullUserDirectory,
     ...tanstackQueryOptions,
   })
 }
