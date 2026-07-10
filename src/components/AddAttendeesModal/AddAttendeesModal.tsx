d99832a8cc8f4c1d7bde2c1c15ef1eddd37f47b5
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-10 15:27:13 +0530

    auto format

--- src/components/AddAttendeesModal/AddAttendeesModal.tsx
+++ src/components/AddAttendeesModal/AddAttendeesModal.tsx
@@ -3,7 +3,9 @@ import LoadingPage from "@/app/loading";
 import useSession from "@/hooks/sessions/useSession";
 import useUserDirectory from "@/hooks/users/useUserDirectory";
 import { getFullName, isAttendeeRole } from "@/types/users/userUtils";
-import { getObjectEntriesWithNumberKeys, getObjectKeysAsNumbers } from "@/utils/stringUtils";
+import {
+  getObjectEntriesWithNumberKeys,
+} from "@/utils/stringUtils";
 import { Select } from "@mantine/core";
 import { modals } from "@mantine/modals";
 import { useMemo } from "react";
@@ -20,21 +22,34 @@ export function AddAttendeesModal(props: AddAttendeesModalProps) {
 
   const potentialSessionAttendees = useMemo(() => {
     if (!userDirectoryQuery.data || !sessionQuery.data) return {};
-    return Object.fromEntries(getObjectEntriesWithNumberKeys(userDirectoryQuery.data || {}).filter(([userId, user]) => isAttendeeRole(user.role) && !sessionQuery.data.attendeeIds.includes(userId)));
+    return Object.fromEntries(
+      getObjectEntriesWithNumberKeys(userDirectoryQuery.data || {}).filter(
+        ([userId, user]) =>
+          isAttendeeRole(user.role) &&
+          !sessionQuery.data.attendeeIds.includes(userId),
+      ),
+    );
   }, [sessionQuery.data?.attendeeIds, userDirectoryQuery.data]);
 
   if (sessionQuery.isPending || userDirectoryQuery.isPending)
     return <LoadingPage />;
   else if (sessionQuery.isError)
-    return <ErrorPage error={sessionQuery.error} />
+    return <ErrorPage error={sessionQuery.error} />;
   else if (userDirectoryQuery.isError)
-    return <ErrorPage error={userDirectoryQuery.error} />
+    return <ErrorPage error={userDirectoryQuery.error} />;
 
-  return <div>
-    <Select data={Object.entries(potentialSessionAttendees).map(([userId, user]) => ({ value: userId, label: getFullName(user.name) }))}>
-    
-    </Select>
-  </div>;
+  return (
+    <div>
+      <Select
+        data={Object.entries(potentialSessionAttendees).map(
+          ([userId, user]) => ({
+            value: userId,
+            label: getFullName(user.name),
+          }),
+        )}
+      ></Select>
+    </div>
+  );
 }
 
 export default function openAddAttendeesModal(sessionId: string) {
