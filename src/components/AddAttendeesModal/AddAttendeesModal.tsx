f033e228c7b45669684a720ce407e87dac00f3e8
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-10 15:26:26 +0530

    only show attendees not currently in session

--- src/components/AddAttendeesModal/AddAttendeesModal.tsx
+++ src/components/AddAttendeesModal/AddAttendeesModal.tsx
@@ -2,9 +2,11 @@ import ErrorPage from "@/app/error";
 import LoadingPage from "@/app/loading";
 import useSession from "@/hooks/sessions/useSession";
 import useUserDirectory from "@/hooks/users/useUserDirectory";
-import { getFullName } from "@/types/users/userUtils";
+import { getFullName, isAttendeeRole } from "@/types/users/userUtils";
+import { getObjectEntriesWithNumberKeys, getObjectKeysAsNumbers } from "@/utils/stringUtils";
 import { Select } from "@mantine/core";
 import { modals } from "@mantine/modals";
+import { useMemo } from "react";
 
 interface AddAttendeesModalProps {
   sessionId: string;
@@ -16,14 +18,19 @@ export function AddAttendeesModal(props: AddAttendeesModalProps) {
   const sessionQuery = useSession(sessionId);
   const userDirectoryQuery = useUserDirectory();
 
+  const potentialSessionAttendees = useMemo(() => {
+    if (!userDirectoryQuery.data || !sessionQuery.data) return {};
+    return Object.fromEntries(getObjectEntriesWithNumberKeys(userDirectoryQuery.data || {}).filter(([userId, user]) => isAttendeeRole(user.role) && !sessionQuery.data.attendeeIds.includes(userId)));
+  }, [sessionQuery.data?.attendeeIds, userDirectoryQuery.data]);
+
   if (sessionQuery.isPending || userDirectoryQuery.isPending)
     return <LoadingPage />;
   else if (sessionQuery.isError || userDirectoryQuery.isError)
     return <ErrorPage error={sessionQuery.error || userDirectoryQuery.error} />;
 
   return <div>
-    <Select data={Object.entries(userDirectoryQuery.data || {}).map(([userId, user]) => ({ value: userId, label: getFullName(user.name) }))}>
-
+    <Select data={Object.entries(potentialSessionAttendees).map(([userId, user]) => ({ value: userId, label: getFullName(user.name) }))}>
+    
     </Select>
   </div>;
 }
--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -3,6 +3,10 @@ export function toNormalCase(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-export function getObjectKeysAsNumbers(obj: object): number[] {
+export function getObjectKeysAsNumbers(obj: Record<number, unknown>): number[] {
   return Object.keys(obj).map(key => Number(key))
+}
+
+export function getObjectEntriesWithNumberKeys<T>(obj: Record<number, T>): [number, T][] {
+  return Object.entries(obj).map(([key, value]) => [Number(key), value])
 }
