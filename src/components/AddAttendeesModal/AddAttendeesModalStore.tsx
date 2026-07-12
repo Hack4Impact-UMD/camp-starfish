ad165281ceb86c5150ef9d0d4c3640107d36fe74
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-12 16:47:29 +0530

    add & remove attendee IDs from additional data structures upon selection/deselection

--- src/components/AddAttendeesModal/AddAttendeesModalStore.tsx
+++ src/components/AddAttendeesModal/AddAttendeesModalStore.tsx
@@ -1,25 +1,27 @@
 import {
   AgeGroup,
+  AttendeeRole,
   CamperAttendee,
   StaffAttendee,
 } from "@/types/sessions/sessionTypes";
+import { getObjectEntriesWithNumberKeys } from "@/utils/stringUtils";
 import { create } from "zustand";
 
 interface AddAttendeesModalStoreState {
   selectedAttendeeIds: number[];
   additionalCamperData: Record<
     number,
-    Pick<CamperAttendee, "ageGroup" | "bunk">
+    Partial<Pick<CamperAttendee, "ageGroup" | "bunk">>
   >;
   additionalStaffData: Record<
     number,
-    Pick<StaffAttendee, "bunk" | "isLeadBunkCounselor">
+    Partial<Pick<StaffAttendee, "bunk" | "isLeadBunkCounselor">>
   >;
 }
 
 interface AddAttendeesModalStoreActions {
-  selectAttendeeId: (attendeeId: number) => void;
-  deselectAttendeeId: (attendeeId: number) => void;
+  selectAttendeeId: (attendeeId: number, role: AttendeeRole) => void;
+  deselectAttendeeId: (attendeeId: number, role: AttendeeRole) => void;
   setBunk: (attendeeId: number, bunkNum: number) => void;
   setAgeGroup: (attendeeId: number, ageGroup: AgeGroup) => void;
   setIsBunkCounselor: (
@@ -32,48 +34,131 @@ type AddAttendeesModalStore = AddAttendeesModalStoreState & {
   actions: AddAttendeesModalStoreActions;
 };
 
-const useAddAttendeesModalStore = create<AddAttendeesModalStore>(
-  (set) => ({
-    selectedAttendeeIds: [],
-    additionalCamperData: {},
-    additionalStaffData: {},
-    actions: {
-      selectAttendeeId: (attendeeId: number) =>
-        set((state) => ({
-          selectedAttendeeIds: [...state.selectedAttendeeIds, attendeeId],
-        })),
-      deselectAttendeeId: (attendeeId: number) =>
-        set((state) => ({
-          selectedAttendeeIds: state.selectedAttendeeIds.filter(
-            (a) => a !== attendeeId,
-          ),
-        })),
-      setBunk: (attendeeId: number, bunkNum: number) =>
-        set((state) => {
-          if (attendeeId in state.additionalCamperData) {
-            return { additionalCamperData: { ...state.additionalCamperData, [attendeeId]: { ...state.additionalCamperData[attendeeId], bunk: bunkNum } } };
-          } else if (attendeeId in state.additionalStaffData) {
-            return { additionalStaffData: { ...state.additionalStaffData, [attendeeId]: { ...state.additionalStaffData[attendeeId], bunk: bunkNum } } };
-          }
-          return {};
-        }),
-      setAgeGroup: (attendeeId: number, ageGroup: AgeGroup) => set((state) => {
+const useAddAttendeesModalStore = create<AddAttendeesModalStore>((set) => ({
+  selectedAttendeeIds: [],
+  additionalCamperData: {},
+  additionalStaffData: {},
+  actions: {
+    selectAttendeeId: (attendeeId: number, role: AttendeeRole) => {
+      switch (role) {
+        case "CAMPER":
+          return set((state) => ({
+            selectedAttendeeIds: [...state.selectedAttendeeIds, attendeeId],
+            additionalCamperData: {
+              ...state.additionalCamperData,
+              [attendeeId]: {},
+            },
+          }));
+        case "STAFF":
+          return set((state) => ({
+            selectedAttendeeIds: [...state.selectedAttendeeIds, attendeeId],
+            additionalStaffData: {
+              ...state.additionalStaffData,
+              [attendeeId]: {},
+            },
+          }));
+        case "ADMIN":
+          return set((state) => ({
+            selectedAttendeeIds: [...state.selectedAttendeeIds, attendeeId],
+          }));
+      }
+    },
+    deselectAttendeeId: (attendeeId: number, role: AttendeeRole) => {
+      switch (role) {
+        case "CAMPER":
+          return set((state) => ({
+            selectedAttendeeIds: state.selectedAttendeeIds.filter(
+              (a) => a !== attendeeId,
+            ),
+            additionalCamperData: Object.fromEntries(
+              getObjectEntriesWithNumberKeys(state.additionalCamperData).filter(
+                ([camperId, _camper]) => camperId !== attendeeId,
+              ),
+            ),
+          }));
+        case "STAFF":
+          return set((state) => ({
+            selectedAttendeeIds: state.selectedAttendeeIds.filter(
+              (a) => a !== attendeeId,
+            ),
+            additionalStaffData: Object.fromEntries(
+              getObjectEntriesWithNumberKeys(state.additionalStaffData).filter(
+                ([stafferId, _staffer]) => stafferId !== attendeeId,
+              ),
+            ),
+          }));
+        case "ADMIN":
+          return set((state) => ({
+            selectedAttendeeIds: state.selectedAttendeeIds.filter(
+              (a) => a !== attendeeId,
+            ),
+          }));
+      }
+    },
+
+    setBunk: (attendeeId: number, bunkNum: number) =>
+      set((state) => {
         if (attendeeId in state.additionalCamperData) {
-          return { additionalCamperData: { ...state.additionalCamperData, [attendeeId]: { ...state.additionalCamperData[attendeeId], ageGroup } } };
+          return {
+            additionalCamperData: {
+              ...state.additionalCamperData,
+              [attendeeId]: {
+                ...state.additionalCamperData[attendeeId],
+                bunk: bunkNum,
+              },
+            },
+          };
+        } else if (attendeeId in state.additionalStaffData) {
+          return {
+            additionalStaffData: {
+              ...state.additionalStaffData,
+              [attendeeId]: {
+                ...state.additionalStaffData[attendeeId],
+                bunk: bunkNum,
+              },
+            },
+          };
         }
         return {};
       }),
-      setIsBunkCounselor: (attendeeId: number, isLeadBunkCounselor: boolean) => set((state) => {
-        if (attendeeId in state.additionalStaffData) {
-          return { additionalStaffData: { ...state.additionalStaffData, [attendeeId]: { ...state.additionalStaffData[attendeeId], isLeadBunkCounselor } } };
+    setAgeGroup: (attendeeId: number, ageGroup: AgeGroup) =>
+      set((state) => {
+        if (attendeeId in state.additionalCamperData) {
+          return {
+            additionalCamperData: {
+              ...state.additionalCamperData,
+              [attendeeId]: {
+                ...state.additionalCamperData[attendeeId],
+                ageGroup,
+              },
+            },
+          };
         }
         return {};
-      })
-    },
-  }),
-);
+      }),
+    setIsBunkCounselor: (attendeeId: number, isLeadBunkCounselor: boolean) =>
+      set((state) => {
+        if (attendeeId in state.additionalStaffData) {
+          return {
+            additionalStaffData: {
+              ...state.additionalStaffData,
+              [attendeeId]: {
+                ...state.additionalStaffData[attendeeId],
+                isLeadBunkCounselor,
+              },
+            },
+          };
+        }
+        return {};
+      }),
+  },
+}));
 
-export const useSelectedAttendeeIds = () => useAddAttendeesModalStore((state) => state.selectedAttendeeIds);
-export const useAdditionalCamperData = () => useAddAttendeesModalStore((state) => state.additionalCamperData);
-export const useadditionalStaffData = () => useAddAttendeesModalStore((state) => state.additionalStaffData);
-export const useAddAttendeesModalStoreActions = () => useAddAttendeesModalStore((state) => state.actions);
+export const useSelectedAttendeeIds = () =>
+  useAddAttendeesModalStore((state) => state.selectedAttendeeIds);
+export const useAdditionalCamperData = () =>
+  useAddAttendeesModalStore((state) => state.additionalCamperData);
+export const useadditionalStaffData = () =>
+  useAddAttendeesModalStore((state) => state.additionalStaffData);
+export const useAddAttendeesModalStoreActions = () =>
+  useAddAttendeesModalStore((state) => state.actions);
