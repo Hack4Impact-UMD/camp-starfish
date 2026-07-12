11a70e68e070c910ad0c2e830f56d73a453270b3
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-12 19:34:41 +0530

    make MultiSelect take up full width of container by default

--- src/styles/components/MultiSelectThemeExtension.tsx
+++ src/styles/components/MultiSelectThemeExtension.tsx
@@ -2,7 +2,7 @@ import { MultiSelect } from "@mantine/core";
 
 const MultiSelectThemeExtension = MultiSelect.extend({
   classNames: {
-    root: 'max-w-full',
+    root: 'w-full',
     input: 'border-blue border-2 px-0 py-2.5 rounded-sm',
     dropdown: "rounded-sm border-neutral border p-0",
     option: "hover:bg-neutral-3 active:bg-neutral-4 rounded-none nth-2:rounded-t-sm last:rounded-b-sm not-last:border-b not-last:border-b-neutral-4",
