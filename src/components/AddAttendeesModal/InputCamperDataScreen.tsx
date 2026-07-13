1ada7e338579cbbf89232cc07f99d7ca7e7a833f
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-13 11:55:02 +0530

    add buttons to camper data input screen

--- src/components/AddAttendeesModal/InputCamperDataScreen.tsx
+++ src/components/AddAttendeesModal/InputCamperDataScreen.tsx
@@ -11,42 +11,69 @@ import {
 import { AGE_GROUPS } from "@/types/sessions/sessionTypes";
 import useUserDirectory from "@/hooks/users/useUserDirectory";
 import { getFullName } from "@/types/users/userUtils";
-import { NumberInput, Select, Table } from "@mantine/core";
+import { Button, NumberInput, Select, Table } from "@mantine/core";
 import { getObjectKeysAsNumbers } from "@/utils/stringUtils";
 import { useMemo } from "react";
 
 export default function InputCamperDataScreen() {
   const additionalCamperData = useAdditionalCamperData();
-  const { setAgeGroup, setBunk } = useAddAttendeesModalStoreActions();
-  console.log(additionalCamperData)
+  const { prevStep, nextStep, setAgeGroup, setBunk } = useAddAttendeesModalStoreActions();
 
-  const camperIds = useMemo(() => getObjectKeysAsNumbers(additionalCamperData), [additionalCamperData]);
+  const camperIds = useMemo(
+    () => getObjectKeysAsNumbers(additionalCamperData),
+    [additionalCamperData],
+  );
 
   const userDirectoryQuery = useUserDirectory();
 
   if (!userDirectoryQuery.data) return <div>Bruh</div>;
 
+  const isAllAdditionalCamperDataInputted = camperIds.every(camperId => additionalCamperData[camperId].ageGroup && additionalCamperData[camperId].bunk);
+
   const columnHelper = createColumnHelper<number>();
-  const columns = useMemo(() => [
-    columnHelper.accessor((camperId) => camperId, { header: "ID" }),
-    columnHelper.accessor(
-      (camperId) => getFullName(userDirectoryQuery.data?.[camperId].name),
-      { header: "Name" },
-    ),
-    columnHelper.accessor(
-      (camperId) => additionalCamperData[camperId]?.ageGroup,
-      {
-        header: "Age Group",
-        cell: ({ cell }) => (
-          <Select value={cell.getValue()} data={AGE_GROUPS} onChange={(val) => setAgeGroup(cell.row.original, val ?? undefined)} />
-        ),
-      },
-    ),
-    columnHelper.accessor((camperId) => additionalCamperData[camperId]?.bunk, {
-      header: "Bunk",
-      cell: ({ cell }) => <NumberInput value={cell.getValue()} onChange={(val) => setBunk(cell.row.original, Number(val))} />,
-    }),
-  ], [userDirectoryQuery.data, additionalCamperData, columnHelper, setAgeGroup, setBunk]);
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor((camperId) => camperId, { header: "ID" }),
+      columnHelper.accessor(
+        (camperId) => getFullName(userDirectoryQuery.data?.[camperId].name),
+        { header: "Name" },
+      ),
+      columnHelper.accessor(
+        (camperId) => additionalCamperData[camperId]?.ageGroup,
+        {
+          header: "Age Group",
+          cell: ({ cell }) => (
+            <Select
+              value={cell.getValue()}
+              data={AGE_GROUPS}
+              onChange={(val) =>
+                setAgeGroup(cell.row.original, val ?? undefined)
+              }
+            />
+          ),
+        },
+      ),
+      columnHelper.accessor(
+        (camperId) => additionalCamperData[camperId]?.bunk,
+        {
+          header: "Bunk",
+          cell: ({ cell }) => (
+            <NumberInput
+              value={cell.getValue()}
+              onChange={(val) => setBunk(cell.row.original, Number(val))}
+            />
+          ),
+        },
+      ),
+    ],
+    [
+      userDirectoryQuery.data,
+      additionalCamperData,
+      columnHelper,
+      setAgeGroup,
+      setBunk,
+    ],
+  );
 
   const table = useReactTable({
     data: camperIds,
@@ -54,26 +81,34 @@ export default function InputCamperDataScreen() {
     getCoreRowModel: getCoreRowModel(),
   });
 
-  return <Table>
-    <Table.Thead>
-      {table.getHeaderGroups().map((headerGroup) => (
-        <Table.Tr key={headerGroup.id}>
-          {headerGroup.headers.map((header) => (
-            <Table.Th key={header.id}>{header.id}</Table.Th>
+  return (
+    <div className="flex flex-col gap-sm">
+      <Table>
+        <Table.Thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <Table.Tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <Table.Th key={header.id}>{header.id}</Table.Th>
+              ))}
+            </Table.Tr>
           ))}
-        </Table.Tr>
-      ))}
-    </Table.Thead>
-    <Table.Tbody>
-      {table.getRowModel().rows.map((row) => (
-        <Table.Tr key={row.id}>
-          {row
-            .getAllCells()
-            .map((cell) =>
-              <Table.Td>{flexRender(cell.column.columnDef.cell, cell.getContext())}</Table.Td>
-            )}
-        </Table.Tr>
-      ))}
-    </Table.Tbody>
-  </Table>;
+        </Table.Thead>
+        <Table.Tbody>
+          {table.getRowModel().rows.map((row) => (
+            <Table.Tr key={row.id}>
+              {row.getAllCells().map((cell) => (
+                <Table.Td>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Table.Td>
+              ))}
+            </Table.Tr>
+          ))}
+        </Table.Tbody>
+      </Table>
+      <div className="flex flex-row justify-around w-full">
+        <Button onClick={prevStep}>Previous</Button>
+        <Button onClick={nextStep} disabled={() => !isAllAdditionalCamperDataInputted()}>Next</Button>
+      </div>
+    </div>
+  );
 }
