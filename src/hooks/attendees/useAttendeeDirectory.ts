d05d4ef4b49991c63db486075afe7ae616126064
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-27 19:51:45 -0400

    use useQueries hook

--- src/hooks/attendees/useAttendeeDirectory.ts
+++ src/hooks/attendees/useAttendeeDirectory.ts
@@ -1,12 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQueries, UseQueryResult } from "@tanstack/react-query";
 import { getUseUserDirectoryOptions } from "../users/useUserDirectory";
-import useSession from "../sessions/useSession";
+import { getUseSessionOptions } from "../sessions/useSession";
 import { getObjectEntriesWithNumberKeys } from "@/utils/stringUtils";
+import { UserDirectory } from "@/types/albums/albumTypes";
+import { Session } from "@/types/sessions/sessionTypes";
+
+function combine(queries: [UseQueryResult<Omit<UserDirectory, "page">>, UseQueryResult<Session>]) {
+  const [userDirectoryQuery, sessionQuery] = queries;
+  return {
+    isPending: userDirectoryQuery.isPending || sessionQuery.isPending,
+    isError: userDirectoryQuery.isError || sessionQuery.isError,
+    isSuccess: userDirectoryQuery.isSuccess && sessionQuery.isSuccess,
+    data: userDirectoryQuery.isSuccess && sessionQuery.isSuccess ? Object.fromEntries(getObjectEntriesWithNumberKeys(userDirectoryQuery.data).filter(([userId, _user]) => sessionQuery.data?.attendeeIds.includes(userId))) : undefined
+  }
+}
 
 export default function useAttendeeDirectory(sessionId: string) {
-  const sessionQuery = useSession(sessionId);
-  return useQuery({
-    ...getUseUserDirectoryOptions(),
-    select: (data) => Object.fromEntries(getObjectEntriesWithNumberKeys(data).filter(([userId, _user]) => sessionQuery.data?.attendeeIds.includes(userId)))
-  });
+  return useQueries({
+    queries: [getUseUserDirectoryOptions(), getUseSessionOptions(sessionId)],
+    combine
+  })
 }
