bc71e070acc6978b7c3b924ac60440974fc9067b
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-07-28 23:00:24 -0400

    extract query options

--- src/hooks/sections/useSection.ts
+++ src/hooks/sections/useSection.ts
@@ -1,14 +1,13 @@
 import { getSectionDoc } from "@/data/firestore/sections";
 import { queryOptions, skipToken, useQuery } from "@tanstack/react-query";
 
-export function getUseSectionOptions(sessionId: string | undefined, sectionId: string | undefined) {
+export function sectionQueryOptions(sessionId: string | undefined, sectionId: string | undefined) {
   return queryOptions({
     queryKey: ['sessions', sessionId, 'sections', sectionId],
     queryFn: sectionId && sessionId ? (() => getSectionDoc(sessionId, sectionId)) : skipToken,
-
   })
 }
 
 export default function useSection(sessionId: string | undefined, sectionId: string | undefined) {
-  return useQuery(getUseSectionOptions(sessionId, sectionId));
+  return useQuery(sectionQueryOptions(sessionId, sectionId));
 }
