d5ba16bd90f04961607d6922db67b7052e455bcf
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-08-02 16:49:31 -0400

    memoize list

--- src/app/sessions/[sessionId]/directory/DirectoryTableView.tsx
+++ src/app/sessions/[sessionId]/directory/DirectoryTableView.tsx
@@ -44,7 +44,6 @@ export default function DirectoryTableView({
   sessionId,
 }: LargeDirectoryBlockProps) {
   const { data: attendeeData, isLoading, isError } = useAttendeeList(sessionId);
-  const attendeeList = attendeeData?.pages.flatMap(page => page.docs);
   const daysOffScheduleQuery = useDaysOffSchedule(sessionId);
   const sessionQuery = useSession(sessionId);
 
@@ -54,6 +53,11 @@ export default function DirectoryTableView({
   // table filter/pagination options
   const [globalFilter, setGlobalFilter] = useState("");
   const [pagination, setPagination] = useState({ pageIndex: 0, pageSize: 10 });
+
+  const attendeeList = useMemo(
+    () => attendeeData?.pages.flatMap((page) => page.docs),
+    [attendeeData],
+  );
 
   const data: Attendee[] = useMemo(() => {
     if (!attendeeList) return [];
@@ -87,7 +91,7 @@ export default function DirectoryTableView({
     }
 
     return attendeeArr;
-  }, [attendeeData, selectedRole, sortNameOption]);
+  }, [attendeeList, selectedRole, sortNameOption]);
 
   const getNameFromId = useCallback(
     (id: number) => {
@@ -95,7 +99,7 @@ export default function DirectoryTableView({
       if (!person) return null;
       return `${person.snapshot.name.firstName} ${person.snapshot.name.lastName[0]}.`;
     },
-    [attendeeData],
+    [attendeeList],
   );
 
   const columns = useMemo<ColumnDef<Attendee>[]>(() => {
@@ -321,10 +325,11 @@ export default function DirectoryTableView({
     <div className="border border-black bg-[#F7F7F7] w-[80%] mx-auto py-[20px]">
       <div className="flex flex-row justify-between px-sm">
         <div></div>
-        <Title order={3}>
-          DIRECTORY
-        </Title>
-        <ActionIcon color="aqua" onClick={() => openAddAttendeesModal(sessionId)}>
+        <Title order={3}>DIRECTORY</Title>
+        <ActionIcon
+          color="aqua"
+          onClick={() => openAddAttendeesModal(sessionId)}
+        >
           <Tooltip label="Add Attendees">
             <MdPersonAdd size={30} />
           </Tooltip>
