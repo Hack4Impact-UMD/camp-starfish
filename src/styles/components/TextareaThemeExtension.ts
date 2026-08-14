add1628af1572047a36e85848a2e791aee45079b
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-08-13 21:59:03 -0400

    standardize label styling

--- src/styles/components/TextareaThemeExtension.ts
+++ src/styles/components/TextareaThemeExtension.ts
@@ -2,7 +2,8 @@ import { Textarea } from "@mantine/core";
 
 const TextareaThemeExtension = Textarea.extend({
   classNames: {
-        input: "bg-blue-0 rounded-md border-none hover:border-neutral-8 px-3 hover:border-2 py-2 text-sm text-blue-9 placeholder:text-neutral-5",
+    label: 'text-neutral-5 text-xl font-medium',
+    input: "bg-blue-0 rounded-md border-none hover:border-neutral-8 px-3 hover:border-2 py-2 text-sm text-blue-9 placeholder:text-neutral-5",
   }
 });
 
