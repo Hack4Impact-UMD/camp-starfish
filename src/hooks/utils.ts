4e9ea8852ba2baec19a1213cd9b3d0b39f9450fb
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-08-18 00:28:28 -0400

    stabilize function reference for flattening infinite query responses from Firestore

---
+++ src/hooks/utils.ts
@@ -0,0 +1,8 @@
+import { PaginatedQueryResponse } from "@/data/firestore/types/queries"
+import { InfiniteData } from "@tanstack/react-query"
+import { TanstackQueryFirestorePageParam } from "./types/tanstackQueryTypes"
+import { DocumentData } from "firebase/firestore"
+
+export function flattenFirestoreInfiniteQuery<AppModelType, DbModelType extends DocumentData>(data: InfiniteData<PaginatedQueryResponse<AppModelType, DbModelType>, TanstackQueryFirestorePageParam<DbModelType> | undefined): AppModelType[] {
+  return data.pages.flatMap(page => page.docs);
+}
