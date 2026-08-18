3d2dc5d6fcfbe9aeb1f54dc666d8af0fd68a06ed
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-08-17 23:18:56 -0400

    add select parameter to flatten docs

--- src/hooks/posts/usePostList.ts
+++ src/hooks/posts/usePostList.ts
@@ -26,6 +26,7 @@ export function postListQueryOptions(firestoreQueryOptions: FirestoreQueryOption
     initialPageParam: undefined as TanstackQueryFirestorePageParam<PostDoc> | undefined,
     getPreviousPageParam: (firstPage) => firstPage.firstSnapshot ? ({ direction: 'previous' as const, snapshot: firstPage.firstSnapshot }) : undefined,
     getNextPageParam: (lastPage) => lastPage.lastSnapshot ? ({ direction: 'next' as const, snapshot: lastPage.lastSnapshot }) : undefined,
+    select: (data) => data.pages.flatMap(page => page.docs),
   });
 }
 
