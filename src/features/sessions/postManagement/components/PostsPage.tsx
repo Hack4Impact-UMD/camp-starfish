36408f725c4176c9ab9224b72ee37b3b0204dc24
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2026-08-20 20:29:30 -0400

    delete accidentally committed file

--- src/features/sessions/postManagement/components/PostsPage.tsx
+++
@@ -1,8 +0,0 @@
-import { postListQueryOptions } from "@/hooks/posts/usePostList";
-import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
-
-export default function PostsPage() {
-  const postsQuery = useSuspenseInfiniteQuery(postListQueryOptions());
-
-  return <div>{postsQuery.data.map(x => x.id)}</div>
-}
